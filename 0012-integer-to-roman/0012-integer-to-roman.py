12171b9c0b09beb50190cea3a3fe14df0d3f9330
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-03 18:23:04 -0700

    Time: 40 ms (88.33%), Space: 16.8 MB (15.26%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,7 +1,16 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
+        '''
+        Roman integers are from big to small
+        So, when converting the integer to roman symbol, divide the num by larger roman num 
+        '''
         
-        #create a list of list for each symbol and value
+        # create a list of list for each symbol and value
+        # iterate over the reversed list
+        #   check if the number of roman integers that need to be added to res
+        #   add the roman integers to the res
+        #   update the num by modding the num by the val
+        
         
         symbols = [ ['I', 1], ['IV', 4], ['V', 5], ['IX',9], ['X', 10], ['XL', 40],
                    ['L',50], ['XC', 90], ['C',100], ['CD', 400], ['D',500],
