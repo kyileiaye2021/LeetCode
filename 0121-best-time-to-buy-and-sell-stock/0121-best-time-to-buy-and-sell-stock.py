c8a884eff2b9c3bd0813a1fbd880773226d5a5f2
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-08-29 12:21:10 -0700

    Time: 771 ms (29.87%), Space: 27.5 MB (36.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,54 +1,40 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        '''
-        #Hashmap Approach
-        #create a hashmap {val: profit}
-        #iterate over the price
-        #max value from i+1 to len of the list
-        #subtract the current val from max value which is the profit
-        #store the profit in the map
-
-        #max_profit = 0
-        #after the loop, iterate over the hashmap to find the largest profit
-        #return the max_profit
-
-        hash_map = {}
-        for i, n in enumerate(prices):
-            price_sublst = prices[i:len(prices)]
-            max_value = max(price_sublst)
-            profit = max_value - n
-            if profit <= 0:
-                hash_map[n] = 0
-            else:
-                hash_map[n] = profit
-    
+        # happy cases 
+        # input - [7,1,5,3,6,4]
+        # output - 5
+        
+        # edge cases
+        # input - [7,6,4,3,1]
+        # output - 0
+        
+        # input - [2,2,2,2,2]
+        # output - 0
+        
+        # Two pointer approach
+        # create a var max_profit to trace the max difference between prices
+        # low and high pointing to 0 and 1
+        # go over the list until high passes the length of the list
+        #   check if the right pointer ele is greater than left pointer ele
+        #       find the difference of the two pointer ele 
+        #       update the max_profit if difference is greater than curr max_profit
+        #       increment right pointer by 1
+        #   assign left pointer to right 
+        #   increment right pointer by 1
+        
         max_profit = 0
-        for profit in hash_map.values():
-            if profit > max_profit:
-                max_profit = profit
+        low, high = 0, 1
         
-        return max_profit
-        '''
-        
-        #Two pointer approach
-        #create two vars (l, r) represeenting index 0 and 1
-        #create a var max_profit to keep track of the max profit
-        #iterate over the lst
-        #if lst[r] > lst[l]:
-        #find the profit and update the max
-        #if not, l = r
-
-        l, r = 0, 1
-        max_profit = 0
-
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(profit, max_profit)
+        while high < len(prices):
+            if prices[high] > prices[low]:
+                difference = prices[high] - prices[low]
+                max_profit = max(max_profit, difference)
+                
             else:
-                l = r
-            r += 1
-
+                low = high
+                
+            high += 1
+            
         return max_profit
-        
+                
