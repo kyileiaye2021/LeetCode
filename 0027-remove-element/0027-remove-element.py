5bdc60052166a5018b57810161581affd8e78cc2
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-08-31 12:32:37 -0700

    Time: 41 ms (27.50%), Space: 16.5 MB (23.94%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,13 +1,34 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        #create a variable to keep track of index which is not equal to val
-        #iterate over the nums
-        #check the curr item == val
-        #if not, update the var
-
-        k = 0
-        for item in nums:
-            if item != val:
-                nums[k] = item
-                k += 1
-        return k
+        # Happy case
+        # input - nums=[3,2,2,3], and val = 3
+        # output - 2 , [2,2,_,_]
+        
+        # Edge case
+        # input - nums=[], and val = 2
+        # output - 0, []
+        
+        # input - nums=[3,4,5], val = 2
+        # output - 3, [3,4,5]
+        
+        
+        # Two pointer approach
+        # create two pointer p and q
+        # iterate until p reaches the end of the nums list
+        #   check if p ele is not the same as val
+        #       assign p ele to the position where q is curr pointing to
+        #       increment q by 1
+        #   increment p by 1
+        # return q
+        
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        
+        p, q = 0, 0
+        while p < len(nums):
+            if nums[p] != val:
+                nums[q] = nums[p]
+                q += 1
+            p += 1
+            
+        return q
