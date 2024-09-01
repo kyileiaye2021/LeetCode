fc2de5b9b871652df8478cc83263b318641b9953
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-09-01 15:13:50 -0700

    Time: 524 ms (59.52%), Space: 31 MB (42.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,12 +1,36 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        maxSub = nums[0]
-        currSum = 0
-
-        for n in nums:
-            if currSum < 0:
-                currSum = 0
-
-            currSum += n
-            maxSub = max(maxSub, currSum)
-        return maxSub
+        # Happy cases
+        # input - nums = [-2,1,2]
+        # output - 3
+        
+        # Edge cases
+        # input - nums = [-1,1]
+        # output - 1
+        # input - nums = [2]
+        # output - 2
+        
+        # Brute Force approach (O(n^2))
+        # Sliding Window approach (O(n))
+        
+        # create a max sub_total to trace the max sub array total value to first ele
+        # create a curr_sub_total to trace the total of curr sub range to 0
+        # create a var called n to 0 and iterate over the list
+        #   update the curr_sub_total by adding n 
+        #   update the max_sub_total if curr_sub_total is greater than max_sub_total
+        #   check if curr_sub_total < 0:
+        #       set curr_sub_total to 0 again
+        
+        
+        max_sub_total = nums[0]
+        curr_sub_total = 0
+        
+        n = 0
+        for num in nums:
+            curr_sub_total += num
+            max_sub_total = max(max_sub_total, curr_sub_total)
+            if curr_sub_total < 0:
+                curr_sub_total = 0
+                
+        return max_sub_total
+    
