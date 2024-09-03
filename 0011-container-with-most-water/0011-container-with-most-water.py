2c905538adda087da18f04afc190243254121a2e
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-09-02 23:09:22 -0700

    Time: 528 ms (42.42%), Space: 29.6 MB (44.80%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,16 +1,54 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l = 0 #first
-        r = len(height) - 1 #last 
         
-        maxArea = 0
-        while l != r:
-            currArea = (r - l) * min(height[l], height[r])
-            maxArea = max(maxArea, currArea)
-
-            if height[l] < height[r]:
-                l += 1
+        # Happy cases
+        # input - height = [1,2,3]
+        # output - 2
+        
+        # Edge cases
+        # input - height = [1, 1]
+        # output - 1
+        # input - height = [0, 0]
+        # output - 0
+        # input - height = [1]
+        # output - 0
+        
+        # Edge cases
+        # find the area of the container
+        # area = height * width
+        # width --difference between ele 
+        
+        # Two pointer approach
+        # create 2 pointers p, q
+        # create a var to trace the max area
+        # iterate over the list until p passes q    
+        #   find the diff between p and q 
+        #   find the min height between p ele and q ele
+        #   find the curr area 
+        #   update the max area if curr area is greater than max area
+        #   check if the p ele is greater than q ele
+        #       decrement q by 1
+        #   else:
+        #       increment p by 1
+        # return the max area
+        
+        # Time complexity - O(n)
+        # Space complexity - O(1)
+        
+        p = 0
+        q = len(height) - 1
+        
+        max_area = 0
+        
+        while p < q:
+            diff = q - p
+            ht = min(height[p], height[q])
+            curr_area = diff * ht
+            max_area = max(max_area, curr_area)
+            
+            if height[p] > height[q]:
+                q -= 1
             else:
-                r -= 1
-
-        return maxArea
+                p += 1
+                
+        return max_area
