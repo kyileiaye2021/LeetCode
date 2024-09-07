658ee03abffd9131b40d57b2faeea880cab20c33
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-09-07 10:13:43 -0700

    Time: 41 ms (27.95%), Space: 16.6 MB (6.41%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,36 +1,57 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         
+        # Happy case
+        # n = 19
+        # output - True
         
-        #Assumption
-        # Num is not null
+        # Edge case
+        # n = 0
+        # output - False
+        # n - 2
+        # output - False
+        # n - 1
+        # output - True
         
-        # * Iterative Appraoch
-        # * create a set to check the n is already visited or not
-        # * iterate until the n is not found in the set
-        #   * add n to the set
-        #.  * iterate until n is 0
-        #       * find n % 10 to get digit
-        #       * square that digit and add it to the sum
-        #       * update orig n with curr sum
-        #   * if n is equal to 1, return True
-        #   * return False 
+        # if the number is already in the visited hashset, return False
         
-        visited_set = set()
-        while n not in visited_set:
-            visited_set.add(n)
+        # create a hashset to store the sum of each time
+        # check until the n is already in the hashset
+        #   get the sum of the square of the digits of n 
+        #   check if it is 1
+        #       return True
+        # return False
+        
+        # Sumofsquare
+        # sum = 0
+        # iterate until n becomes 0
+        #   n % 10 
+        #   sum += n
+        #   n = n // 10
+        
+        visited = set() # memory O(n)
+        
+        while n not in visited:
+            visited.add(n)
             
             n = self.sumOfSquare(n)
             if n == 1:
                 return True
             
         return False
-                
+        
     def sumOfSquare(self, n):
         sum = 0
+        
         while n:
             digit = n % 10
             sum += (digit ** 2)
             n = n // 10
+            
         return sum
+            
+            
         
+        
+        
+     
