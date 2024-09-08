039cc19bb2561f740a903f42fa1456b4c276a145
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-09-08 11:23:46 -0700

    Time: 56 ms (28.51%), Space: 16.6 MB (87.87%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,35 +1,40 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        # Can there be like that there is no common str in the words? No
-        # Can the words list be empty or null? no
-        # Can there be repetitive word in the str?
-        # Can there be repetitive char in each str?
         
-        # High Level Plan
-        # * Iterative Approach
-        #   * iterate over the list. in each iteration, iterate again to check the 
-        #       curr ele with other ele in the list
-        # * Dictionary Approach
+      
+        # Happy cases
+        # input: words = ["bella", "label", "roller"]
+        # output: ["e", "l", "l"]
+
+        # Edge cases
+        # input: words = ["a"]
+        # output: []
+        # input: words = ["a", "b"]
+        # output: []
+        # input: words = ["a", "a"]
+        # output: ["a"]
+
+        # Hashmap
+        # create a res list
+        # create a comm hashmap for the 1st ele with Counter
+        # iterate over the list from index 1
+        #   create a hashmap for each curr ele
+        #   iterate over the 1st ele hashmap 
+        #       update curr ele with the min of the curr ele in the comm hashmap and that in the curr hashmap
+        # iterate over the comm hashmap 
+        #   append the curr key with its frequence in the res list
         
-        # Low Level Plan
-        # * create a frequency map for the first word using Counter()
-        # * iterate over the lst
-        #   * create the curr frequency map for the curr word using Counter()
-        #   * compare the curr frequency map with the min frequency map
-        # * iterate over the min frequency
-        #   * iterate the amount of value of each key
-        
-        min_common = Counter(words[0])
-        
-        for word in words:
-            curr_cnt = Counter(word)
-            for c in min_common:
-                min_common[c] = min(min_common[c], curr_cnt[c])
-                
-        common_list = []    
-        for key in min_common:
-            for i in range(min_common[key]):
-                common_list.append(key)
-        return common_list
-                
-                
+        res_lst = []
+        comm_map = collections.Counter(words[0])
+
+        for i in range(1, len(words)):
+            curr_map = collections.Counter(words[i])
+
+            for key in comm_map:
+                comm_map[key] = min(curr_map[key], comm_map[key])
+
+        for key, value in comm_map.items():
+            for _ in range(value):
+                res_lst.append(key)
+
+        return res_lst
