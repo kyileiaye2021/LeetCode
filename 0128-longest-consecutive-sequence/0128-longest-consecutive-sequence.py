0a9241594a99878eca707308b43f964f6847a6da
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-09-09 09:29:40 -0700

    Time: 5543 ms (5.67%), Space: 31.7 MB (82.40%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,26 +1,40 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # creating a set to store the ele in order
-        # create longest seq length to 0
+        # Happy case:
+        # input - nums = [100,200,1,2,3]
+        # output - 3
         
-        # iterate over the nums
-        #   * check if the num-1 is not in set
-        #   * curr num will be start num
-        #   * if there are right neighbor (num + 1) 
-        #       * update the curr length
-        #   * update the longest seq length
+        # Edge cases:
+        # input - nums = []
+        # output - 0
+        # input - nums = [1]
+        # output - 1
+        # input - nums = [1, 0, 0]
+        # output - 2
+        
+        # O(n)
+        # create a set for the nums
+        # create a var longest to trace the longest seq
+        # iterate over the ele in the nums
+        #   check if ele - 1 is not in the set
+        #       y = ele + 1
+        #       check while y is in the set
+        #           increment y by 1
+        #       update the longest
+        # return longest
         
         numSet = set(nums)
         longest = 0
         
-        for n in nums:
-            
-            if (n - 1) not in numSet: #if there is no left neighbor, n is the start num
-                length = 0 #to keep track of the length of curr seq
-                
-                while (n + length) in numSet: #calculate the length of seq
-                    length += 1
-            
-                longest = max(longest, length)
-                
+        for ele in nums:
+            if ele - 1 not in numSet:
+                y = 1
+                while ele+1 in numSet:
+                    y += 1
+                    ele = ele + 1
+                    
+                longest = max(y, longest)
         return longest
+                
+        
+        
