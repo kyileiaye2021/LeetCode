af3fc65c64a7fc5ac63fef34f3d5417e4c06c9a3
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-09-13 00:28:41 -0700

    Time: 97 ms (28.35%), Space: 22 MB (26.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,22 +1,41 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Happy cases
+        # input - ["eat", "tea", "tan"]
+        # output - [["eat", "tea"], ["tan"]]
         
-        # create a dict to store { count of the num of ele : [list of anagrams]}
-        #iterate over the list
-            # iterate over the curr str
-            #   count the num of each curr char 
-            # append the str to the value of the dict key
-            
-        res = defaultdict(list) #values are gonna be list
+        # Edge cases:
+        # input - [""]
+        # output - [[""]]
+        
+        # input - ["a"]
+        # output - [["a"]]
+        
+        # create a hashmap {countChar : [list of anagram]}
+        # iterate over the list
+        #   create a list of 0s representing the counter of a to z
+        #   iterate over the curr char
+        #       increment the index where the char exists by subtracting the ascii value of a from ascii value of the curr char
+        #   append the curr str to the value list of corresponding list
+        
+        res = {}
         
         for s in strs:
             
-            count = [0] * 26
+            lst = [0] * 26
             
-            for char in s:
-                count[ord(char) - ord('a')] += 1
-            
-            res[tuple(count)].append(s) #list cannot be key in python so make it tuple
-            
-        return res.values()
+            for c in s:
                 
+                lst[ord(c) - ord('a')] += 1 # using ascii value to increment the ele indexed position
+                
+            lst = tuple(lst) # list cannot be the key of hashmap
+            if lst not in res:
+                res[lst] = [s]
+            
+            else:
+                res[lst].append(s)
+                
+        return res.values()
+            
+        
+    
