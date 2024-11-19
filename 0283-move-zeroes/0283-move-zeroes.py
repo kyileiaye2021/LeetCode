1373bc390f7db086b1d2b4198838136134a9fae1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-11-19 01:18:38 -0800

    Time: 3 ms (82.39%), Space: 17.7 MB (70.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,39 +3,42 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Time complexity - O(n)
-        # Space complexity - O(1) 
         
-        # Happy cases
-        # input - [0,1,0,3,12]
-        # output - [1,3,12, 0,0]
+        # happy case
+        # input: [0, 1, 0]
+        # output: [1, 0, 0]
+
+        # edge cases
+        # input: [0, 0]
+        # output: [0, 0]
+
+        # input: [1]
+        # output: [1]
+
+        # Brute force (O(n))
+        # Dictionary (O(n))
+        # Two pointer (O(n))
+
+        # low level steps 
+        # initialize two pointers i and j
+        # i, j = 0, 0
+        # iterate over the list until i reaches the end of the lst
+        #   check if the i ele is not 0
+        #       assign the i ele in the j position 
+        #       increment both i and j by 1
+        #   increment i
+        # iterate over the list from jth position to the end of the list
+        #   assign the value of cur ele to 0
+
+        i, j = 0, 0
+
+        while i < len(nums):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+            i += 1
         
-        #Edge cases
-        # input - []
-        # output - []
-        #input - [0]
-        # output - [0]
-        
-        # Two pointer approach
-        # p, q pointing to 0, 0
-        # iterate over the list until p exceeds the end
-        #   check if the curr p pointer ele is not 0
-        #       assign the p pointer ele in the pos where q is currently pointing to
-        #       increment p and q by 1
-        #   else:
-        #       increment p by 1
-        # replace the ele with 0s starting from q pointer index to the end
-        # return the list
-        
-        p, q = 0, 0
-        while p < len(nums):
-            if nums[p] != 0:
-                nums[q] = nums[p]
-                q += 1
-            p += 1
-        
-        while q < len(nums):
-            nums[q] = 0
-            q += 1
-            
-        return nums
+        while j < len(nums):
+            nums[j] = 0
+            j += 1
+
