5cb0e4f19d324100056b9a26c93f1d1f1e222a98
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-11-19 00:42:19 -0800

    Time: 95 ms (52.33%), Space: 25.9 MB (82.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,40 +1,51 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        # happy cases 
-        # input - [7,1,5,3,6,4]
+        # happy cases
+        # input - [1,2,3,4]
+        # output - 3
+        
+        # input - [7,3,5,1,6]
         # output - 5
         
+        # input - [3,1,2,5]
+        # output - 4
+        
         # edge cases
-        # input - [7,6,4,3,1]
+        # input - [4,3,2,1]
         # output - 0
         
-        # input - [2,2,2,2,2]
+        # input - [3,3,3]
         # output - 0
         
-        # Two pointer approach
-        # create a var max_profit to trace the max difference between prices
-        # low and high pointing to 0 and 1
-        # go over the list until high passes the length of the list
-        #   check if the right pointer ele is greater than left pointer ele
-        #       find the difference of the two pointer ele 
-        #       update the max_profit if difference is greater than curr max_profit
-        #       increment right pointer by 1
-        #   assign left pointer to right 
-        #   increment right pointer by 1
+        # Brute force approach (O(n^2))
+        # Two pointer approach (O(n))
         
+        # low level steps
+        # initialize two pointers 
+        # i will point to second ele in nums
+        # j will point to first ele in nums
+        # One pointer i will iterate over the list
+        # Another pointer j will be pointing to the current smallest num in the list
+        # while i is less than the length of the lst
+        #   check if the curr j ele is not less than i ele
+        #       point j to i
+        #   increment i
+        
+        i, j = 1, 0
         max_profit = 0
-        low, high = 0, 1
         
-        while high < len(prices):
-            if prices[high] > prices[low]:
-                difference = prices[high] - prices[low]
-                max_profit = max(max_profit, difference)
-                
+        while i < len(prices):
+            if prices[j] >= prices[i]:
+                j = i
+               
             else:
-                low = high
+                profit = prices[i] - prices[j]
+                max_profit = max(max_profit,profit)
                 
-            high += 1
-            
+            i += 1
+                
         return max_profit
+            
                 
+        
+        
