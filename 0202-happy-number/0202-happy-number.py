4b5a08404d2f279dfee01c12076dfabaae95bba1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-11-23 15:39:54 -0800

    Time: 0 ms (100%), Space: 16.8 MB (9.41%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,57 +1,61 @@
 class Solution:
+    
     def isHappy(self, n: int) -> bool:
-        
-        # Happy case
-        # n = 19
-        # output - True
-        
-        # Edge case
-        # n = 0
-        # output - False
-        # n - 2
-        # output - False
-        # n - 1
-        # output - True
-        
-        # if the number is already in the visited hashset, return False
-        
-        # create a hashset to store the sum of each time
-        # check until the n is already in the hashset
-        #   get the sum of the square of the digits of n 
-        #   check if it is 1
-        #       return True
-        # return False
-        
-        # Sumofsquare
-        # sum = 0
-        # iterate until n becomes 0
-        #   n % 10 
-        #   sum += n
-        #   n = n // 10
-        
-        visited = set() # memory O(n)
-        
-        while n not in visited:
-            visited.add(n)
-            
-            n = self.sumOfSquare(n)
-            if n == 1:
-                return True
-            
-        return False
-        
-    def sumOfSquare(self, n):
+
+        # happy cases
+        # input: n = 10
+        # output: true
+
+        # input: n = 12
+        # output: false
+
+        # edge cases
+        # input: n = 1
+        # output: true
+
+        # input: n = 5
+        # output: false
+
+        # plan
+        # check if the number is 1
+        #   return true
+        # check if the number is less than 10
+        #   return false
+        # sum = 0 
+        # iterate until num becomes 1
+        #   iterate until num % 10 becomes 0
+        #       digit = num % 10
+        #       square = digit **2
+        #       accumulate the sum with the digits
+        #   if sum is 1
+        #       return true
+        #   else if sum is less than 10: return false
+        #   else: update num with sum
+
+        visited = []
+
+        if n == 1:
+            return True
+
         sum = 0
+        while n != 1:
+            if n not in visited:
+                visited.append(n)
+            else:
+                return False
+            while n != 0:
+                digit = n % 10
+                n = n // 10
+                sum += (digit**2)
+
+            if sum == 1:
+                return True
+
+            else:
+                n = sum
+                sum = 0
+
+
+    
+
         
-        while n:
-            digit = n % 10
-            sum += (digit ** 2)
-            n = n // 10
-            
-        return sum
-            
-            
-        
-        
-        
-     
