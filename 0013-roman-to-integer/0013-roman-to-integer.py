f4d28d6afa8ecc739809d78c0bb5000710ff4790
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-11-29 02:15:35 -0800

    Time: 4 ms (66.01%), Space: 17.5 MB (18%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,28 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        #create a map {roman ele : numerical val}
-        #create a var to store the total numerical val
-        #iterate over the str s
-            #check the next index still within the range of bound and the curr char val < next char val
-                #subtract the curr char val from the total numerical val
-            #add the curr char val to the total numerical val
-        #return total numerical val
+
+        symbols = {'I': 1,
+                    'V': 5,
+                    'X': 10,
+                    'L': 50,
+                    'C': 100,
+                    'D': 500,
+                    'M': 1000}
         
-        roman = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000   
-        }
-        total_numerical_val = 0
-        
-        for i in range(len(s)):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
-                total_numerical_val -= roman[s[i]]
+        total = 0
+
+        i = 0
+
+        while i < len(s)-1:
+            if symbols[s[i]] < symbols[s[i+1]]:
+                total -= symbols[s[i]]
             else:
-                total_numerical_val += roman[s[i]]
-                
-        return total_numerical_val
+                total += symbols[s[i]]
+            i += 1
+
+        return total + symbols[s[i]]
+
+        
