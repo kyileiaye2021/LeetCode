ea9139ce100f5b72f57bf10424954ddcfc29fa4d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-11-29 03:31:37 -0800

    Time: 0 ms (100%), Space: 18.3 MB (8.28%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,37 +5,13 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Happy case
-        # input - [1,2,3,4]
-        # output - [4,3,2,1]
-        
-        # input - [1,2]
-        # output - [2,1]
-        
-        # Edge case
-        # input - []
-        # output - []
-        
-        # input - [2]
-        # output - [2]
-        
-        # Traversal approach
-        # create a prev pointer initialized to null
-        # traverse the linked list
-        #   track the next node of the curr node
-        #   assign the prev node pointer to the next of curr node
-        #   move prev pointer to curr node
-        #   move curr pointer to the next of the curr node
-        # return prev pointer
-        
-        if not head:
-            return None
-        curr = head
         prev = None
+        curr = head
+
         while curr:
-            next_node = curr.next
+            next = curr.next
             curr.next = prev
             prev = curr
-            curr = next_node
-        return prev 
-        
+            curr = next
+           
+        return prev
