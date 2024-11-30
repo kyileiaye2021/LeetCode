e1bf7712c2d53c097342ac88de79501fd53c3e53
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-11-30 02:39:16 -0800

    Time: 41 ms (79.58%), Space: 19.7 MB (8.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,14 +6,25 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        st = set()
+        # Using hashset
+        # st = set() # Space O(n)
 
-        while head:
-            if head in st:
+        # while head: # Time O(n)
+        #     if head in st:
+        #         return True
+
+        #     st.add(head)
+        #     head = head.next
+
+        # return False
+       
+       # Floyd cycle algorithm
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
                 return True
 
-            st.add(head)
-            head = head.next
-
         return False
-       
