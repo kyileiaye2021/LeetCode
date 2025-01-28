2f2669b458310884a195f43c313e7e1817b2fa91
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-01-28 01:10:27 -0800

    Time: 0 ms (100%), Space: 18.8 MB (37.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,33 +1,29 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # happy cases
-        # input: nums = [2,7,9] , target = 9
-        # output: [0,1]
+        # Happy cases
+        # input - [2,7,11,15], target = 9
+        # output = [0,1]
 
-        # input : nums = [1,2,4], target = 5
-        # output: [0,2]
+        # Edge cases
+        # input - [3,2], target = 5
+        # output = [0, 1]
 
-        # edge cases
-        # input: nums = [1, 1], target = 2
-        # output: [0, 1]
-
-        # Brute Force
-        # Two pointer with the hashmap
-
-        # low level steps
-        # create a hashmap [ele, index]
-        # iterate over the list
-        #   diff between target and the curr ele
-        #   check if the diff is already in hashmap
-        #       return a pair of curr ele index and the diff ele index
-        #   if not in the hashmap
-        #       add the curr ele with its index in the hashmap
-
-        map = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in map:
-                return [i, map[diff]]
+        # dictionary approach
+        # {ele : index}
+        # iterate over the ele in the list
+        #   do a diff between the target and curr ele
+        #       check if the diff is in dict
+        #           return the curr ele index and diff ele index as a list
+        #       else: add the curr ele with its index in the dict
+        
+        idx_map = {}
+        
+        for i, ele in enumerate(nums):
+            diff = target - ele
+            if diff in idx_map:
+                return [i, idx_map[diff]]
 
             else:
-                map[nums[i]] = i
+                idx_map[ele] = i
+
+        
