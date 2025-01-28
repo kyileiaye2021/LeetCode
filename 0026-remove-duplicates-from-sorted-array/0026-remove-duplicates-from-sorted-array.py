a031d25b0b7d2ee9e629ff91208a2ddb37380499
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-01-28 00:53:22 -0800

    Time: 4 ms (28.92%), Space: 19 MB (22.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,45 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
-    # Happy cases
-    # input - [1,1,2]
-    # output - 2
-    
-    # Edge cases
-    # input - []
-    # output - 0
-    
-    # input - [1]
-    # output - 1
-    
-    # Two pointer approach
-    # if len(nums) == 0:
-    #   return []
-    # p and q initialize to 1,1
-    # p pointing to the ele and iterate over the ele in the list
-    # q point to index of the unique ele 
-    # while p reaches the end of the list
-    #   check if the curr p element is not the same as the prev ele
-    #       assign p ele into the positon that q pointer currently points to
-    #       increment q by 1
-    #   increment p by 1
-    # return q
-    
-    # Time complexity - O(n)
-    # Space complexity - O(1)
-    
-        if len(nums) == 0:
-            return []
+        i, j = 1, 1
+        while i < len(nums):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
 
-        p, q = 1 , 1
+            i += 1
 
-        while p < len(nums):
-
-            if nums[p] != nums[p - 1]:
-                nums[q] = nums[p]
-                q += 1
-
-            p += 1
-
-        return q
-
+        return j
