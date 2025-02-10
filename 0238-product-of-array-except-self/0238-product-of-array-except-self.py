17bb1c01f8c1d3b3e92b19c41979dcb53a6b60f1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-10 13:27:13 -0800

    Time: 28 ms (40.88%), Space: 26.4 MB (25.67%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,68 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        # prefix and postfix 
+        prefix = [1] * len(nums)
+        postfix = [1] * len(nums)
 
-        # given array, create prefix and postfix technique
-        # iterate two passes of the array from left to right and right to left
-        # keep track of prefix and postfix product vals
-        # update the curr ele of nums with its product of prefix and postfix vals
 
-        # Time - O(n)
-        # Space - O(n)
-
-        prefix = 1
-        postfix = 1
-
-        # creating prefix array
-        prefix_arr = [0] * len(nums) 
+        # [1,1,2,6]
+        # [1,-1,-1,0,0,0]
+        pre_product = 1
         for i in range(len(nums)):
-            prefix_arr[i] = prefix * nums[i]
-            prefix = prefix_arr[i]
+            prefix[i] = pre_product
+            pre_product *= nums[i]
 
-        # creating postfix array
-        postfix_arr = [0] * len(nums)
-        for i in range(len(nums) - 1, 0, -1):
-            postfix_arr[i] = postfix * nums[i]
-            postfix = postfix_arr[i]
+        # [24,12,4,1]
+        # [0,0,0,-9,3,1]
+        post_product = 1
+        for i in range(len(nums)-1, -1, -1):
+            postfix[i] = post_product
+            post_product *= nums[i]
 
-        # updating the nums array with the product of values
-        for i in range(len(nums)):
-
-            if i != 0 and i != len(nums) - 1:
-                nums[i] = prefix_arr[i - 1] * postfix_arr[i + 1]
-
-            elif i == 0:
-                nums[i] = postfix_arr[i + 1]
-
-            else:
-                nums[i] = prefix_arr[i - 1]
+        # [24, 12,8,6]
+        for i in range(len(prefix)):
+            nums[i] = prefix[i] * postfix[i]
 
         return nums
 
-        # using division operation (just try this but not allowed to use in code)
-        # Time - O(n)
-        # Space - O(n)
-        total = 1
-        count_zero = 0
-        for ele in nums: 
-            if ele != 0:
-                total *= ele
-            else:
-                count_zero += 1
 
-        res = []
-        for ele in nums:
-            if count_zero > 1:
-                res.append(0)
-
-            elif count_zero == 1:
-                if ele != 0:
-                    res.append(0)
-                else:
-                    res.append(total)
-            
-            else:
-                res.append(total // ele)
-
-        return res
