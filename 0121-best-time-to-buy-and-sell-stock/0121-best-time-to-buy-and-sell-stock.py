d10e5aea29c6f6e3c5753fcb7accf01c4eb7907f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-10 07:30:00 -0800

    Time: 120 ms (35.34%), Space: 26.8 MB (71.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,51 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # happy cases
-        # input - [1,2,3,4]
-        # output - 3
         
-        # input - [7,3,5,1,6]
-        # output - 5
-        
-        # input - [3,1,2,5]
-        # output - 4
-        
-        # edge cases
-        # input - [4,3,2,1]
-        # output - 0
-        
-        # input - [3,3,3]
-        # output - 0
-        
-        # Brute force approach (O(n^2))
-        # Two pointer approach (O(n))
-        
-        # low level steps
-        # initialize two pointers 
-        # i will point to second ele in nums
-        # j will point to first ele in nums
-        # One pointer i will iterate over the list
-        # Another pointer j will be pointing to the current smallest num in the list
-        # while i is less than the length of the lst
-        #   check if the curr j ele is not less than i ele
-        #       point j to i
-        #   increment i
-        
-        i, j = 1, 0
+        # if current jth ele is > than the ith ele
+        #   move i to j and j by 1
+        # else:
+        #   calculate profiit and keep track of the max profit
+
+        i, j = 0, 1
         max_profit = 0
-        
-        while i < len(prices):
-            if prices[j] >= prices[i]:
-                j = i
-               
+        while j < len(prices):
+            if prices[j] < prices[i]:
+                i = j
+                j += 1
+
             else:
-                profit = prices[i] - prices[j]
-                max_profit = max(max_profit,profit)
-                
-            i += 1
-                
+                profit = prices[j] - prices[i]
+                max_profit = max(profit, max_profit)
+                j += 1
+
         return max_profit
-            
-                
-        
-        
