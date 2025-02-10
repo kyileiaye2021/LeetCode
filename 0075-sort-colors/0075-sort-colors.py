8e5eebd89281f78873ece2b67e243d2394f74d0c
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-10 12:36:20 -0800

    Time: 0 ms (100%), Space: 17.9 MB (31.34%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,48 +3,43 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # happy cases
-        # input - [2,0,2,1,1,0]
-        # output - [0,0,1,1,2,2]
-        
+
+        # happy case
+        # input = [2,2,1,0]
+        # output = [0, 1, 2, 2]
+
         # edge cases
-        # input - [0]
-        # output - [0]
-        # input - [1]
-        # output - [1]
-        # input - [2]
-        # output - [2]
-        
-        # counting ele approach
-        # partition approach
-        
-        # partition approach
-        # create two pointers l, r
-        # start i from 0
-        # iterate over the list until i passes r
-        #   check if the curr ele is 0
-        #       swap the curr ele with the ele that is curr pointed by l pointer
+        # input = [0,0]
+        # output = [0,0]
+
+        # input = [2]
+        # output = [2]
+
+        # forward backword two pointer 
+        # iterate thru the list
+        #   check if the curr ele is 0 
+        #       swap that ele with the ele curr pointed by l
         #       increment l by 1
-        #   else if the curr ele is 2
-        #       swap the curr ele with the ele that is curr pointed by r pointer
+        #   check if the curr ele is 2
+        #       swap that ele with the ele curr pointed by r
         #       decrement r by 1
-        #       decrement i by 1
-        #   increment i by 1
-        
+
         l, r = 0, len(nums) - 1
         i = 0
         while i <= r:
-            if nums[i] == 0:
-                temp = nums[i]
-                nums[i] = nums[l]
-                nums[l] = temp
-                l += 1
-            elif nums[i] == 2:
+            if nums[i] == 2:
                 temp = nums[i]
                 nums[i] = nums[r]
                 nums[r] = temp
                 r -= 1
                 i -= 1
-                
+
+            elif nums[i] == 0:
+                temp = nums[i]
+                nums[i] = nums[l]
+                nums[l] = temp
+                l += 1
             i += 1
             
+        return nums
+        
