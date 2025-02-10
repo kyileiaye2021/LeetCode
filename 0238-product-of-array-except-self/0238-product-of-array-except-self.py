81ddb52be88a0e250e2fe8aa0c1dbbb00dfef415
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-10 13:26:28 -0800

    Time: 39 ms (18.33%), Space: 26.4 MB (24.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,36 +3,46 @@ class Solution:
         
         # prefix and postfix 
 
-        # given array, create prefix and postfix array
+        # given array, create prefix and postfix technique
+        # iterate two passes of the array from left to right and right to left
+        # keep track of prefix and postfix product vals
+        # update the curr ele of nums with its product of prefix and postfix vals
 
-        # prefix = 1
-        # postfix = 1
+        # Time - O(n)
+        # Space - O(n)
 
-        # prefix_arr = [0] * len(nums) 
-        # postfix_arr = [0] * len(nums)
+        prefix = 1
+        postfix = 1
 
-        # for i in range(len(nums)):
-        #     prefix_arr[i] = prefix * nums[i]
-        #     prefix = prefix_arr[i]
+        # creating prefix array
+        prefix_arr = [0] * len(nums) 
+        for i in range(len(nums)):
+            prefix_arr[i] = prefix * nums[i]
+            prefix = prefix_arr[i]
 
-        
-        # for i in range(len(nums) - 1, 0, -1):
-        #     postfix_arr[i] = postfix * nums[i]
-        #     postfix = postfix_arr[i]
+        # creating postfix array
+        postfix_arr = [0] * len(nums)
+        for i in range(len(nums) - 1, 0, -1):
+            postfix_arr[i] = postfix * nums[i]
+            postfix = postfix_arr[i]
 
-        # for i in range(len(nums)):
-        #     if i != 0 and i != len(nums) - 1:
-        #         nums[i] = prefix_arr[i - 1] * postfix_arr[i + 1]
+        # updating the nums array with the product of values
+        for i in range(len(nums)):
 
-        #     elif i == 0:
-        #         nums[i] = postfix_arr[i + 1]
+            if i != 0 and i != len(nums) - 1:
+                nums[i] = prefix_arr[i - 1] * postfix_arr[i + 1]
 
-        #     else:
-        #         nums[i] = prefix_arr[i - 1]
+            elif i == 0:
+                nums[i] = postfix_arr[i + 1]
 
-        # return nums
+            else:
+                nums[i] = prefix_arr[i - 1]
 
-        # using division operation
+        return nums
+
+        # using division operation (just try this but not allowed to use in code)
+        # Time - O(n)
+        # Space - O(n)
         total = 1
         count_zero = 0
         for ele in nums: 
@@ -54,5 +64,5 @@ class Solution:
             
             else:
                 res.append(total // ele)
-                
+
         return res
