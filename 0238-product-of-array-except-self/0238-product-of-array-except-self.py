59b68f913b62164590b9d4b408e492fbf3eeec6f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-10 16:42:01 -0800

    Time: 19 ms (87.37%), Space: 23.3 MB (54.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,28 +1,46 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        prefix = [1] * len(nums)
-        postfix = [1] * len(nums)
+        # space - O(n)
+        # time - O(3n)
+        # prefix = [1] * len(nums)
+        # postfix = [1] * len(nums)
 
+        # # [1,1,2,6]
+        # # [1,-1,-1,0,0]
+        # pre_product = 1
+        # for i in range(len(nums)):
+        #     prefix[i] = pre_product
+        #     pre_product *= nums[i]
 
-        # [1,1,2,6]
-        # [1,-1,-1,0,0,0]
-        pre_product = 1
+        # # [24,12,4,1]
+        # # [0,0,-9,3,1]
+        # post_product = 1
+        # for i in range(len(nums)-1, -1, -1):
+        #     postfix[i] = post_product
+        #     post_product *= nums[i]
+
+        # # [24, 12,8,6]
+        # for i in range(len(prefix)):
+        #     nums[i] = prefix[i] * postfix[i]
+
+        # return nums
+
+        # Space complexity O(1)
+
+        res = [1] * len(nums)
+        prefix = 1
+        postfix = 1
+
         for i in range(len(nums)):
-            prefix[i] = pre_product
-            pre_product *= nums[i]
+            res[i] = prefix
+            prefix *= nums[i]
 
-        # [24,12,4,1]
-        # [0,0,0,-9,3,1]
-        post_product = 1
         for i in range(len(nums)-1, -1, -1):
-            postfix[i] = post_product
-            post_product *= nums[i]
+            res[i] *= postfix
+            postfix *= nums[i]
 
-        # [24, 12,8,6]
-        for i in range(len(prefix)):
-            nums[i] = prefix[i] * postfix[i]
+        return res
+            
 
-        return nums
 
 
