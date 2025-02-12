aad2c2d3589aed3b057ba2616cc8bdbbb1bf1ed3
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-12 13:48:44 -0800

    Time: 0 ms (100%), Space: 17.6 MB (89.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,29 +3,32 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # two pointer
-        i, j = m - 1, n-1
-        index = m + n -1
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[index] = nums1[i]
-                i -= 1
+        # two pointer approach with simultaneous operation
+
+        l, r = m - 1, n - 1
+        assigned_index = m + n - 1
+
+        while l >= 0 and r >= 0:
+            if nums1[l] > nums2[r]:
+                nums1[assigned_index] = nums1[l]
+                l -= 1
+                assigned_index -= 1
 
             else:
-                nums1[index] = nums2[j]
-                j -= 1
+                nums1[assigned_index] = nums2[r]
+                r -= 1
+                assigned_index -= 1
 
-            index -= 1
+        while l >= 0:
+            nums1[assigned_index] = nums1[l]
+            l -= 1
+            assigned_index -= 1
 
-        while i >= 0:
-            nums1[index] = nums1[i]
-            index -= 1
-            i -=1
+        while r >= 0:
+            nums1[assigned_index] = nums2[r]
+            r -= 1
+            assigned_index -= 1
 
-        while j >= 0:
-            nums1[index] = nums2[j]
-            index -= 1
-            j -= 1
+        return nums1
 
 
-    
