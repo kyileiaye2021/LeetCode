3311e472f6bf8046959be0c91f524580631ad1c1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-12 11:35:22 -0800

    Time: 0 ms (100%), Space: 17.9 MB (30.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,56 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # happy cases
-        # input: s = "()"
-        # output: true
+        # using stack
+        # using dict
 
-        # input: s = "(}"
-        # output: false
-
-        # input: s = "({)}"
-        # output: false
-
-        # edge cases
-        # input: s = "("
-        # output: false
-
-        # input: s = "}"
-        # output: false
-
-        # input: "}{"
-        # output: false
-
-        # hashmap {'(':")"}
-        # create a stack
-        # iterate over the char in the str
-        #   if the cur char is open bracket
-        #       add it to the stack
-        #   else:
-        #       check if the stack is not empty and the current last char of stack matches the close bracket
-        #           pop that last ele 
-        #       else: return false
-
-
-        map = {'(': ')', '{': '}', '[':']'}
-        open_brackets = []
+        map = {'(':')', '{':'}', '[':']'}
+        open_stack = []
 
         for ele in s:
-            if ele in map: # open brackets
-                open_brackets.append(ele)
-            else: # close brackets
-                if len(open_brackets) > 0:
-                    last = open_brackets[-1]
-                    if ele == map[last]:
-                        open_brackets.pop()
-                    else: 
+            if ele in map:
+                open_stack.append(ele)
+
+            else:
+                if open_stack:
+                    last_ele = open_stack.pop()
+                    if map[last_ele] != ele:
                         return False
+
                 else:
                     return False
 
-        if len(open_brackets) > 0:
+        if not open_stack:
+            return True
+
+        else:
             return False
-        return True
-
-
-
-
