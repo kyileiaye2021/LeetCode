11730c3cdbe2996ae8605014f024d5ac20896b76
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-02-12 13:38:30 -0800

    Time: 67 ms (33.04%), Space: 18.1 MB (59.95%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,48 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        # happy case
+        # s = "aba"
+        # output = true
+
+        # edge case
+
+        # s = "ab"
+        # output = true
+
+        # s = "abcgdba"
+        # output = false
+
+         # s = "abca"
+        # output = true
+
+        # two pointer 
+
+        return_val = True
+        l, r = 0, len(s) - 1
+
+        while l < r:
+    
+            if s[l] != s[r] :
+
+                return_val_l = self.valid(s, l + 1,r)
+                return_val_r = self.valid(s, l, r-1)
+                if return_val_l or return_val_r:
+                    return True
+                else:
+                    return False
+
+            else:
+                l += 1
+                r -= 1
+
+        return True
+
+    def valid(self,s, l, r):
+
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
