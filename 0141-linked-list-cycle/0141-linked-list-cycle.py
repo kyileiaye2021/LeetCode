b6f87fb439e51480226c8a5026ba8709ddb033f0
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-03-31 09:13:31 -0700

    Time: 44 ms (81.3%), Space: 19.9 MB (57.56%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,25 +6,42 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # Using hashset
-        # st = set() # Space O(n)
+        # happy case
+        # input: head = [3,2,0,-4], pos = 1
+        # output: true
 
-        # while head: # Time O(n)
-        #     if head in st:
-        #         return True
+        # input: head = [2,3], pos = 0
+        # output: True
 
-        #     st.add(head)
-        #     head = head.next
+        # edge case
 
-        # return False
-       
-       # Floyd cycle algorithm
-        slow, fast = head, head
+        # input: head = [], pos = 0
+        # output: False
+
+        # input: head = [1], pos = 0
+        # output: True
+         
+        # input: head = [2], pos = -1
+        # output: False
+
+        # input: head = [2,4,3], post = 4
+        # output: False
+
+        # brute force - array: cannot work because we don't know when to break
+        # fast slow pointer approach 
+        # fast, slow both pointing to head
+        # traverse the list from the head until the fast pointer reaches none
+        #   move the slow pointer by 1 node
+        #   move the fast pointer by 2 nodes
+        #   check if the fast and slow pointer meets at some point:
+        #       return true
+        # return false
+
+        fast, slow = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
             if slow == fast:
                 return True
-
         return False
+
