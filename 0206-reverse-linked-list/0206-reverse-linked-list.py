29ccbaa1d3f2343df20724693f100b9129b04111
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-03-31 14:20:13 -0700

    Time: 0 ms (100%), Space: 18.8 MB (15.01%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,52 +6,45 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # happy cases
-        # head = [1,2,3]
-        # output: [3,2,1]
+        # input: head = [1,2,3,4]
+        # output: [4,3,2,1]
 
-        # head = ['a', 'b']
-        # output: ['b', 'a']
+        # input: head = [6,-1,3]
+        # output: [3,-1,6]
 
-        # head = ['a', 'b', 1, 2]
+        # edge cases:
+        # input: head = [3]
+        # output: [3]
+
+        # input: head = []
         # output: []
 
-        # head = [1]
-        # output: [1]
+        # Brute force - O(n) space using two arrays
+        # use an array to store the data of every node by traversing the linked list
+        # create an another array to store the elements in reverse order
 
-        # edge case
-        # head = []
-        # output: []
+        # Bruter force - using one array and two pointers - O(n) space
+        # use an array to store the data of every node by traversing the linked list
+        # create two pointers at the edges of the array and swap the two pointer elements and shrink the size between two pointers
 
-        # Three pointers
-        # curr pointer, prev pointer, next pointer
-        # temp = head
-        # traverse the linked list
-        #   curr next = prev
-        # 1 --> 2 2 --> 1
-        # curr = head
-        # prev = None
-        # temp = curr.next
-        # while temp.next
+        # Two pointer
+        # create a prev pointer to None
+        # initialize the curr pointer to head
+        # traverse the list until curr pointer reaches None
+        #   next node = curr.next
         #   curr.next = prev
         #   prev = curr
-        #   curr = temp
-        #   temp = temp.next 
-        # return temp
+        #   curr = next node
+        # return prev
 
-        if not head: 
-            return head
-
-        curr = head
+        if not head:
+            return None
         prev = None
-
+        curr = head
         while curr:
-            temp = curr.next
+            next_node = curr.next
             curr.next = prev
             prev = curr
-            curr = temp
-            
+            curr = next_node
+
         return prev
-
-
-
-        
