0fa2947096d69f7d0d9509afb8796f3ca62fe261
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-04-01 12:41:02 -0700

    Time: 91 ms (26.18%), Space: 27.9 MB (32.68%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,55 +1,77 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        # find the length of the two nodes
-        # if list A len is greater, traverse the list A ahead by the difference  between list A and B
-        # if list B len is greater, traverse the list B ahead
-        # traverse both A and B to find the intersection node
-        
-        
-        # find the length of the two nodes
-        intersectVal = ListNode(0)
-        listA = headA
-        listB = headB
-        
-        listA_length = 0
-        while listA:
-            listA_length += 1
-            listA = listA.next
-        
-        listB_length = 0
-        while listB:
-            listB_length += 1
-            listB = listB.next
-        
-        skipA = 0
-        skipB = 0
-        listA = headA
-        listB = headB
-        
-# if list A len is greater, traverse the list A ahead by the difference  between list A and B
-        if listA_length > listB_length:
-            for _ in range(listA_length - listB_length):
-                listA = listA.next
-                skipA += 1
-              
-    # if list B len is greater, traverse the list B ahead
+        # Happy cases
+        # input: listA = [4,1,8,4,5], listB = [5,6,1,8,4,5]
+        # output: 8
+
+        # input: listA = [1,4,3,2], listB = [5,3,2]
+        # output: 3
+
+        # input: listA = [5,6,2,1], listB = [1,4,1]
+        # output: 1
+
+        # Edge cases
+        # input: listA = [7,5,1], listB = [4,2,6]
+        # output: None
+
+        # input: listA = [1], listB = [1]
+        # output: 1
+
+        # input: listA = [3,5,6], listB = [3,5,6]
+        # output: 3
+
+        # Brute force - traverse the list and in each iteration, traverse the second list and check if the curr node is equal to the outer curr node - O(n*m) time, O(1) space
+
+        # Hashset - traverse 1 list and store the nodes in hashset. Then, traverse another list and check if there are the same nodes - O(n) time, O(n) space
+
+        # Find length and first traverse the longer list and traverse both lists simultaneously later - O(n + m) time, and O(1) space - Two pass
+
+        # Two pointer
+        # p and q pointers pointing to heads of listA and listB
+        # traverse the list until p reaches the end
+        # if p reaches the end, point it to the head of listB
+        # traverse the list until q reaches the end
+        # if p reaches the end, point it to the head of listB
+        # do the above until 
+
+        curr1 = headA
+        curr2 = headB
+        len1 = 0
+        len2 = 0
+        while curr1:
+            len1 += 1
+            curr1 = curr1.next
+
+        while curr2:
+            len2 += 1
+            curr2 = curr2.next
+
+        curr1 = headA
+        curr2 = headB
+        diff = 0
+        if len1 > len2:
+            diff = len1 - len2
+            for _ in range(diff):
+                curr1 = curr1.next
         else:
-            for _ in range(listB_length - listA_length):
-                listB = listB.next
-                skipB += 1
-        
-        # traverse both A and B to find the intersection node
-        while listA and listB:
-            if listA == listB:
-                return listA
-            listA = listA.next
-            listB = listB.next
-        
-        return None # if no intersection is found
-                
+            diff = len2 - len1
+            for _ in range(diff):
+                curr2 = curr2.next
+
+        while curr1 and curr2:
+            if curr1 == curr2:
+                return curr1
+
+            curr1 = curr1.next
+            curr2 = curr2.next
+
+        return None
+
+        # move the longer list diff node ahead
+       
