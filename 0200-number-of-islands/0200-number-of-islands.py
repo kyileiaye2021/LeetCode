8be0044305da3c48c8e9ab8726ead580624f3a22
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-18 21:04:10 -0700

    Time: 220 ms (98.09%), Space: 20 MB (82.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,45 @@
 class Solution:
-
-    def dfs(self, grid, r, c):
-        # mark the curr entry as visited
-        # for each direction 
-        #   check if the entry within the bound
-        #       check if it's 1 and not visited
-        #           call dfs on the neighbor
-        directions = [[1,0], [-1,0], [0,1], [0,-1]]
-        grid[r][c] = 0
-        for dir_r, dir_c in directions:
-            new_r, new_c = r + dir_r, c + dir_c
-            if new_r in range(len(grid)) and new_c in range(len(grid[0])):
-                if grid[new_r][new_c] == '1':
-                    self.dfs(grid, new_r, new_c)
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        # dfs 
-        # for each row in the grid
-        #   for each col in the grid
-        #       check if the curr entry is 1 and not visited
-        #           call dfs
-        #           increment island count
-        # return island count
-        num_island = 0
-        rows, cols = len(grid), len(grid[0])
-
+        # dfs
+        # dfs func  (grid, r, c)
+        #   mark the curr cell as visited
+        #   for each neighbor of the curr cell
+        #       check if they are within the boundary and they are not visited
+        #           check if they are 1
+        #               call dfs on those cells
+        # 
+        # count = 0
+        # num of r, c 
+        # visited set to check the visited vertices
+        # iterate thru each cell
+        #   check if the curr cell is not visited and the curr cell is 1
+        #       call dfs func
+        #       increment the count 
+        # return count 
+        
+        def dfs(r, c):
+            # base case
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0':
+                return
+                
+            grid[r][c] = '0'
+            
+            dfs(r, c+1)
+            dfs(r, c-1)
+            dfs(r+1, c)
+            dfs(r-1, c)
+    
+    
+        count = 0
+        rows = len(grid)
+        cols = len(grid[0])    
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1':
-                    self.dfs(grid, r, c)
-                    num_island += 1
-        return num_island
+                    dfs(r,c)
+                    count += 1
+                
+        return count
+        
+            
