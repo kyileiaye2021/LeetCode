3a624a5f5ad1a1c43740d622602ae6da3f307beb
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-18 21:20:26 -0700

    Time: 16 ms (78.41%), Space: 18.4 MB (38.5%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,34 +1,30 @@
 class Solution:
-    def dfs(self, grid, r, c):
-
-        grid[r][c] = 0
-        count = 1
-
-        directions = [[1,0], [-1,0], [0,1],[0,-1]]
-        for dir_r, dir_c in directions:
-            new_r = r + dir_r
-            new_c = c + dir_c
-            if new_r in range(len(grid)) and new_c in range(len(grid[0])) and grid[new_r][new_c]== 1:
-                grid[new_r][new_c] = 0
-                count += self.dfs(grid, new_r, new_c)
-
-        return count
-
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        # dfs
-        # mark the curr cell as 0
-        # go to the neighbor in 4 directions and mark them as 0
-        #   count incremented
-
+        def dfs(r,c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
+                return
+            
+            nonlocal count
+            count += 1
+            grid[r][c] = 0
+            
+            dfs(r+1, c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        
+        
+        count = 0
         max_area = 0
         rows = len(grid)
         cols = len(grid[0])
-
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    area = self.dfs(grid,r, c)
-                    max_area = max(max_area, area)
-
+                    dfs(r, c)
+                    max_area = max(max_area, count)
+                    count = 0
+                
         return max_area
-
+                    
