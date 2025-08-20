cf3566f213b35a53d3401c894cc93a90f37a75ee
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-20 15:16:08 -0700

    Time: 11 ms (22.93%), Space: 24.4 MB (7.58%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,53 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # input - array of ints, an int
-        # output - array of ints
+        
+        # hashmap
 
-        # happy cases
-        # input - nums = [1,1,1], k = 1
-        # output - [1]
+        freq_map = {}
 
-        # input - nums = [1,1,2], k = 2
-        # output - [1,2] or [2,1]
+        for n in nums:
+            freq_map[n] = freq_map.get(n, 0) + 1
 
-        # edge cases
-        # input - nums = [1], k =1
-        # output - [1]
+        #. bucket sort
+        bucket = [[] for _ in range(len(nums) + 1)]
 
-        # dict {ele: freq count}
-        # for dict with freq count, we can only keep track of the 
-        # largest freq count ele so we can sort the dict based on the freq vals
-        # but it may take O(nlogn) time
-
-        # so we will use bucket sort here
-        # populate the dict with eles and their freq counts
-        # create a bucket sort of input array size [index: count, [] of eles in the bucket]
-        # insert the elements based on their freq count 
-        # (index of the bucket is equal to the freq count of ele)
-        # iterate over the bucket from the end and add the eles to the res list until the len of res list reaches to the k ele
-
-        # create a dict {ele : freq_count} and populate the dict
-        freq_dict = {}
-        for ele in nums: 
-            if ele not in freq_dict:
-                freq_dict[ele] = 1
-
-            else:
-                freq_dict[ele] += 1
-
-        # create a bucket (index will 
-        count_bucket = [[] for i in range(len(nums) + 1)]
-
-        # inserting the eles based on their freq count
-        for ele in freq_dict:
-            count_bucket[freq_dict[ele]].append(ele)
+        for ele in freq_map:
+            bucket[freq_map[ele]].append(ele)
 
         res = []
-        # iterate over the bucket in descending order to get the most k freq eles
-        for i in range(len(count_bucket) - 1, 0, -1):
-            for ele in count_bucket[i]:
+        for i in range(len(bucket) - 1, 0, -1):
+            for ele in bucket[i]:
                 res.append(ele)
-                if len(res) == k:
-                    return res
+                k -= 1
 
-        
+            if k == 0:
+                return res
