865a6ad9cbd46dc698b0c5d02c18bc38acf6cac7
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-20 13:45:45 -0700

    Time: 0 ms (100%), Space: 17.8 MB (53.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,42 +4,33 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        # happy case
-        # input = [2,2,1,0]
-        # output = [0, 1, 2, 2]
-
-        # edge cases
-        # input = [0,0]
-        # output = [0,0]
-
-        # input = [2]
-        # output = [2]
-
-        # forward backword two pointer 
+        # l, r = 0, len(nums) - 1
         # iterate thru the list
-        #   check if the curr ele is 0 
-        #       swap that ele with the ele curr pointed by l
-        #       increment l by 1
-        #   check if the curr ele is 2
-        #       swap that ele with the ele curr pointed by r
-        #       decrement r by 1
+        #   when we meet 2, replace with r pointer ele 
+        #       decrement r pointer
+        #   when we meet 0, replace with l pointer ele
+        #       increment l pointer
 
-        l, r = 0, len(nums) - 1
         i = 0
+        l = 0
+        r = len(nums) - 1
         while i <= r:
             if nums[i] == 2:
                 temp = nums[i]
                 nums[i] = nums[r]
                 nums[r] = temp
                 r -= 1
-                i -= 1
 
             elif nums[i] == 0:
                 temp = nums[i]
                 nums[i] = nums[l]
                 nums[l] = temp
                 l += 1
-            i += 1
-            
-        return nums
+                i += 1
+
+            else:
+                i += 1
+
+        
+
         
