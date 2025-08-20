1387c4b9822568b963c55dd969b5566848d74edf
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-20 11:20:43 -0700

    Time: 56 ms (63.88%), Space: 32 MB (82.5%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,22 +1,15 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # if the curr sum becomes lesser than the max, then we have to move left pointer and subtract the left pointer ele from the arr
 
-        # sliding window approach
-        # if curr sum becomes negative, reset to 0
-        # update the max sum with current sum
-        # return the max sum
+        # until the right pointer reaches the end
+        maxSum = float('-inf')
+        currSum = 0
 
-        max_sum = nums[0]
-        curr_sum = 0
+        for ele in nums:
+            currSum += ele
+            maxSum = max(maxSum, currSum)
+            if currSum < 0:
+                currSum = 0
 
-        i, j = 0, 0
-
-        while j < len(nums):
-            if curr_sum < 0:
-                curr_sum = 0
-            curr_sum += nums[j]
-
-            max_sum = max(max_sum, curr_sum)
-            j += 1
-        
-        return max_sum
+        return maxSum
