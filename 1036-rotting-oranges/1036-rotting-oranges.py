de15b5ab7fe377b271718130c2d3793f05a80c63
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-23 21:22:37 -0700

    Time: 7 ms (25.13%), Space: 18 MB (26.28%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,38 +1,46 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        
-        queue = collections.deque()
-        dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        
+        # bfs
+        # go thru the cells
+        #   if the fruit is rotten
+        #       append it to the queue
+        #   if the fruit is fresh
+        #       increment the fresh
+
+        queue = deque()
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        minute = 0
         fresh = 0
+
         rows = len(grid)
         cols = len(grid[0])
-        
-        minute = 0
-        
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
-                    queue.append((r,c))
-                elif grid[r][c] == 1:
+                    queue.append((r, c, minute))
+
+                if grid[r][c] == 1:
                     fresh += 1
-        
-        if not fresh:
-            return 0
-        
+
         while queue:
-            queue_len = len(queue)
-            
-            for _ in range(queue_len):
-                r,c = queue.popleft()
-                
-                for dx,dy in dir:
-                    nr = r+dx
-                    nc = c+dy
-                    if nr >= 0 and nr < rows and nc >= 0 and nc < cols and grid[nr][nc] == 1:
-                        grid[nr][nc] = 0
-                        fresh -= 1
-                        queue.append((nr,nc))
-            minute += 1
+            curr_r, curr_c, curr_min = queue.popleft()
+            minute = curr_min
+
+            for dx, dy in dirs:
+                new_r, new_c, new_min = curr_r + dx, curr_c + dy, curr_min + 1
+                if new_r in range(rows) and new_c in range(cols) and grid[new_r][new_c] == 1:
+                    grid[new_r][new_c] = 2
+                    fresh -= 1
+                    queue.append((new_r, new_c, new_min))
+
+        return -1 if fresh > 0 else minute
+
         
-        return -1 if fresh > 0 else minute-1
+
+
+
+
+
+        
+
