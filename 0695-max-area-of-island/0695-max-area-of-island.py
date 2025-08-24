4d49899233b6ea2b059facc469e859bd79d6da56
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-23 21:06:12 -0700

    Time: 35 ms (21.92%), Space: 18.2 MB (78.34%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,30 +1,29 @@
 class Solution:
+    def dfs(self, r, c, grid, count, dirs):
+        grid[r][c] = 0
+        count[0] += 1
+
+        for dx, dy in dirs:
+            new_r, new_c = r + dx, c + dy
+            if new_r in range(len(grid)) and new_c in range(len(grid[0])) and grid[new_r][new_c] == 1:
+                self.dfs(new_r, new_c, grid, count, dirs)
+
+
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        def dfs(r,c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
-                return
-            
-            nonlocal count
-            count += 1
-            grid[r][c] = 0
-            
-            dfs(r+1, c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-        
-        
-        count = 0
-        max_area = 0
+        # dfs
         rows = len(grid)
         cols = len(grid[0])
-        
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        max_count = 0
+
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    dfs(r, c)
-                    max_area = max(max_area, count)
-                    count = 0
-                
-        return max_area
+                    count = [0]
+                    self.dfs(r, c, grid, count, dirs)
+                    max_count = max(max_count, count[0])
+
+        return max_count
                     
+
+       
