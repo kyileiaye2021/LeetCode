c3385b138916cca9dc7bfab0d8b5a76f4f95a02a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-25 00:17:54 -0700

    Time: 5 ms (36.16%), Space: 19 MB (85.54%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,85 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        course_graph = defaultdict(list)
         indeg = [0] * numCourses
-        graph = {i: [] for i in range(numCourses)}
-        
-        for course, prereq in prerequisites:
-            graph[course].append(prereq)
-            indeg[prereq] += 1
-        
-        q = deque([i for i in range(numCourses) if indeg[i] == 0])
-        order = []
-        
-        while q:
-            cur = q.popleft()
-            order.append(cur)
-            
-            for c in graph[cur]:
-                indeg[c] -= 1
-                if indeg[c] == 0:
-                    q.append(c)
-        
-        return order[::-1] if len(order) == numCourses else []
-            
-        
-        
-        
-        # bfs
-        
-        # # hashmap {preq: courses}
-        # indeg = [0] * numCourses
-        # course_graph = defaultdict(list)
-        # for (course, prereq) in prerequisites:
-        #     course_graph[prereq].append(course)
-        #     indeg[course] += 1
-        
-        # course_res = []
-        # queue = collections.deque()
-        # # store the course with no prereq in the queue first
-        # for i in range(numCourses):
-        #     if indeg[i] == 0:
-        #         queue.append(i)
-            
-        # while queue:
-            
-        #     curr_course = queue.popleft()
-        #     course_res.append(curr_course)
-            
-        #     for next_course in course_graph[curr_course]:
-        #         indeg[next_course] -= 1
-                
-        #         if indeg[next_course] == 0:
-        #             queue.append(next_course)
-        
-        # return course_res if len(course_res) == numCourses else []
-        
-        
-        
-        
-        # graph = {i: [] for i in range(numCourses)}
-        # dependences = defaultdict(list)
-        
-        # order = []
-        
-        # for course, prereq in prerequisites:
-        #     graph[course].append(prereq)
-        #     dependences[prereq].append(course)
-            
-        # independent = [i for i in graph if not graph[i]]
-        # queue = deque(independent)
-        
-        # while queue:
-        #     cur = queue.popleft()
-        #     order.append(cur)
-            
-        #     for nextLevelCourse in dependences[cur]:
-        #         graph[nextLevelCourse].remove(cur)
-        #         if not graph[nextLevelCourse]:
-        #             queue.append(nextLevelCourse)
-            
-        #     graph.pop(cur)
-        
-        # return order if not graph else []        
-        
-        
-        
+
+        for (course, prereq) in prerequisites:
+            course_graph[prereq].append(course)
+            indeg[course] += 1
+
+        queue = deque()
+        course_count = 0
+        res_list = []
+        for i in range(numCourses):
+            if indeg[i] == 0:
+                queue.append(i) 
+
+        while queue:
+            curr_course = queue.popleft()
+            res_list.append(curr_course)
+            course_count += 1
+
+            for next_course in course_graph[curr_course]:
+                indeg[next_course] -= 1
+
+                if indeg[next_course] == 0:
+                    queue.append(next_course)
+
+        return [] if course_count != numCourses else res_list
+
