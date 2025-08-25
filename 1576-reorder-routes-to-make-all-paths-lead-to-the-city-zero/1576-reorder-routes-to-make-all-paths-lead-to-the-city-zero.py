d9f98c0279a3a438ac3be27c2a1b0cad7ed869a1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-24 23:38:59 -0700

    Time: 329 ms (5.01%), Space: 53.6 MB (18.54%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,39 +1,29 @@
 class Solution:
+    def dfs(self, visited, graph, currcity, count):
+        visited.add(currcity)
+
+        for (neighbor, tag) in graph[currcity]:
+            if neighbor not in visited:
+                if tag == 1:
+                    count[0] += 1
+                self.dfs(visited, graph, neighbor, count)
+
+
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        # iterate thru each city
-        #   dfs() on eaach city
-        
-        
-        # in dfs
-        #   go to neighbors
-        #   backtrack and change edges
-        #   count the edges as well
-        
-        # creating a graph
-        # [[0, 1]
-        #   [0, 1]]
-        
-        def dfs(curcity):
-            visited.add(curcity)
-            nonlocal changes
-            
-            for (neighbor, tag) in adj[curcity]:
-                if neighbor not in visited:
-                    if tag == 1:
-                        changes += 1
-                    dfs(neighbor)
-            
-        
-        adj = [[] for _ in range(n)]
-        
-        for (u, v) in connections:
-            adj[u].append((v, 1)) # going away from city 0
-            adj[v].append((u, -1)) # going towards city 0
-            
-        print(adj)
-        
         visited = set()
-        changes = 0
-        dfs(0)
-        return changes
-        
+        graph = defaultdict(list)
+        count = [0]
+
+        for x, y in connections:
+            graph[x].append((y, 1))
+            graph[y].append((x, -1))
+
+        print(graph)
+
+        self.dfs(visited, graph, 0, count)
+        return count[0]
+
+
+
+
+    
