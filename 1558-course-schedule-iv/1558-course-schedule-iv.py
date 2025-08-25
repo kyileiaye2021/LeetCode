6a6f45832464326a326650f012c29baf2d076d5d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-25 01:19:55 -0700

    Time: 23 ms (93.05%), Space: 20.6 MB (44.57%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.py
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -1,75 +1,36 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        # course graph : {prereq: [next courses]}
-        # iterate thru the queries
-        #   [1, 0]
-        #   
-        # course_graph = defaultdict(list)
-        # for prereq, course in prerequisites:
-        #     course_graph[prereq].append(course)
-            
-        # ans = [False] * len(queries)
-        # for prereq, course in queries:
-        #     if course in course_graph[prereq]:
-        #         ans.append(True)
-        #     else:
-        #         ans.append(False)
-        
-        # return ans
-        
-        graph = defaultdict(list)# {i: set() for i in range(numCourses)}
-        # all courses that i depends on 
-        allPrereq = {i: set() for i in range(numCourses)}
-        # the number of courses that this course depends on
+        course_graph = defaultdict(list)
         indeg = [0] * numCourses
-        
         for prereq, course in prerequisites:
-            # graph[prereq].add(course)
-            graph[prereq].append(course)
+            course_graph[prereq].append(course)
             indeg[course] += 1
-        
-        independent = [i for i in range(numCourses) if not indeg[i]]
-        q = deque(independent)
-        
-        while q:
-            cur = q.popleft()
-            
-            for c in graph[cur]:
-                allPrereq[c].add(cur)
-                allPrereq[c].update(allPrereq[cur])
-                indeg[c] -= 1
-                
-                if not indeg[c]:
-                    q.append(c)
-        
-        return [u in allPrereq[v] for u,v in queries]
-        
-            
-            
-            
-        '''
-        class Solution:
-    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        adj = [set() for _ in range(numCourses)]
-        indegree = [0] * numCourses
-        isPrereq = [set() for _ in range(numCourses)]
 
-        for pre, crs in prerequisites:
-            adj[pre].add(crs)
-            indegree[crs] += 1
+        queue = deque()
+        for i in range(numCourses):
+            if indeg[i] == 0:
+                queue.append(i)
 
-        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+        prereq_set = {i: set() for i in range(numCourses)}
+        while queue:
+            curr_course = queue.popleft()
 
-        while q:
-            node = q.popleft()
-            for neighbor in adj[node]:
-                isPrereq[neighbor].add(node)
-                isPrereq[neighbor].update(isPrereq[node])
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
+            for neighbor in course_graph[curr_course]:
+                prereq_set[neighbor].add(curr_course)
+                prereq_set[neighbor].update(prereq_set[curr_course])
+                indeg[neighbor] -= 1
+                if indeg[neighbor] == 0:
+                    queue.append(neighbor)
 
-        return [u in isPrereq[v] for u, v in queries]
-        '''
-            
-            
+        res = []
+        for (u, v) in queries:
+            if u in prereq_set[v]:
+                res.append(True)
+            else:
+                res.append(False)
+
+        return res
+
+        
+
+
