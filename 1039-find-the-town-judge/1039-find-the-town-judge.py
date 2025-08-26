809707d0d3b7e0a8edae65c98098a9ec6d23239f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-25 22:20:53 -0700

    Time: 16 ms (44.6%), Space: 21.1 MB (15.29%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -1,24 +1,24 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        # incoming -> n-1
-        # outgoing --> 0
-
-        # two hashmaps {incoming: num}, {outgoing: num}
-        # iterate thru the trust
-        #   add the incoming num and outgoing num
-
-        # iterate thru the incoming
-        #   return the key that has n -1 incoming and 0 outgoing
-
-        incoming = defaultdict(int)
-        outgoing = defaultdict(int)
-
-        for src, des in trust:
-            incoming[des] += 1
-            outgoing[src] += 1
+        graph = defaultdict(set)
+        candidates = set([i for i in range(1, n+1)])
         
-        for v in range(1, n + 1):
-            if incoming[v] == n - 1 and outgoing[v] == 0:
-                return v
-
+        for a, b in trust:
+            graph[b].add(a)
+            if a in candidates:
+                candidates.remove(a)
+            
+        if (not candidates) or len(candidates) > 1:
+            return -1
+        
+        judge_candidate = candidates.pop()
+        
+        if len(graph[judge_candidate]) == n-1:
+            return judge_candidate
+            
         return -1
+            
+            
+            
+            
+            
