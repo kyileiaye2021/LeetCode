d239300304566f05d22011d0081c4f9307e9bd1c
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-08-27 21:09:14 -0700

    Time: 3 ms (46.43%), Space: 19 MB (29.32%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,27 +1,23 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # two ponters starting from 1
+        # f, s
+        # while s is less than the len of the list
+        #   if s ele is the same as s - 1 ele
+        #       move s by 1
+        #   else:
+        #       assign s ele to f 
+        #       move f by 1
+        #       move s by 1
+        # return f + 1
 
-        # happy case
-        # input - [1,2,2,4,5]
-        # output - [1,2,4,5,_] , 4
-
-        # edge case
-        # input - [1,1,1,1]
-        # output - [1,_,_,_], 1
-
-        # Brute Force
-        # Two pointer 
-
-        i, j = 1, 1
-
-        while j < len(nums):
-            if nums[j] != nums[j - 1]:
-                nums[i] = nums[j]
-                i += 1
-
-            j += 1
-
-        return i
-
-        
-        
+        if len(nums) == 1:
+            return 1
+        f, s = 1, 1
+        while s < len(nums):
+            if nums[s] != nums[s - 1]:
+                nums[f] = nums[s]
+                f += 1
+            s += 1
+        # nums = nums[:f]
+        return f
