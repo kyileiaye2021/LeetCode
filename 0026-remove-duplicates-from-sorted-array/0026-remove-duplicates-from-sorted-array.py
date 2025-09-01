6265ff6c459f7855731d5dba23be9065a8c6a35f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-01 09:22:38 -0700

    Time: 0 ms (100%), Space: 18.7 MB (99.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,23 +1,27 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # two ponters starting from 1
-        # f, s
-        # while s is less than the len of the list
-        #   if s ele is the same as s - 1 ele
-        #       move s by 1
-        #   else:
-        #       assign s ele to f 
-        #       move f by 1
-        #       move s by 1
-        # return f + 1
+        # nums = [1,1,2]
+        # output: 2, nums = [1,2,_]
 
-        if len(nums) == 1:
-            return 1
-        f, s = 1, 1
-        while s < len(nums):
-            if nums[s] != nums[s - 1]:
-                nums[f] = nums[s]
-                f += 1
-            s += 1
-        # nums = nums[:f]
-        return f
+        # nums = [0,0,1,1,1,1,2,]
+        # output: 3, [0,1,2,_]
+
+        # two pointer
+
+        # i, j index 0
+        # iterate thry the nums upto 2nd to last ele with j
+        #   if j ele is != j + 1 ele
+        #       move j ele to i
+        #       move i by 1
+        #   move j by 1
+
+        i, j = 1, 1
+        while j < len(nums):
+            if nums[j] != nums[j - 1]:
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+
+        return i
+
+        
