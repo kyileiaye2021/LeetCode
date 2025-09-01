be7d9863a6bdeb047711e180162d90ef2e142381
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-01 10:51:54 -0700

    Time: 0 ms (100%), Space: 19 MB (12.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,32 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # i, j
+        # twopointers
+        # i and j
         # total profit
-        # max profit
-        # iterate thru the list until j reaches the end
-        #   find the cur profit
-        #   if cur profit > max profit
-        #       update the max profit
-        #       move j pointer
+        # itereate thru the list
+        #   diff 
+        #   if diff > 0:
+        #       add the diff to total profit
+        #       move i to j
         #   else:
-        #       update total
-        #       move i to j pointer
-        #       reset max profit to 0
-        #       move j pointer
+        #       move i to j
+        #   move j by 1
+        # return total profit
 
         i, j = 0, 1
-        total = 0
-
+        total_profit = 0
         while j < len(prices):
-            # if we can make profit
-            #   sell the stock, make profit, and rebuy at the same day
-            # else:
-            #   buy the product
+            curr_profit = prices[j] - prices[i]
+            if curr_profit > 0:
+                total_profit += curr_profit
 
-            profit = prices[j] - prices[i]
-            if profit > 0:
-                total += profit
-            i = j # rebuy immediately after selling at the same day
+            i = j
             j += 1
-                  
-        return total
+        return total_profit
