ad56a158468387d1a3cb032118e20434812475a6
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-02 17:57:28 -0700

    Time: 0 ms (100%), Space: 18.1 MB (63.28%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -32,18 +32,27 @@ class Solution:
         # input: [1,3,1]
         # output: 1
 
-        # brute force --> nested loop
-        # sort the array / count the nums when iterating the array from end '
-        citations.sort()
-        n = len(citations)
+        # # brute force --> nested loop
+        # # sort the array / count the nums when iterating the array from end '
+        # citations.sort()
+        # n = len(citations)
         
-        for i in range(n):
+        # for i in range(n):
 
-            # num of books that have at least citations[i] in the arr => n-i
-            # check if citations at ith position is at least n-i
+        #     # num of books that have at least citations[i] in the arr => n-i
+        #     # check if citations at ith position is at least n-i
 
-            if citations[i] >= n - i:
-                return n - i
-        return citations[0]
+        #     if citations[i] >= n - i:
+        #         return n - i
+        # return citations[0]
         
+        # descending order and keep track of how many papers have at least ith ciations
+        citations.sort(reverse=True)
+        count = 1
 
+        for ct in citations:
+            if ct >= count:
+                count += 1
+            else:
+                return count - 1
+        return count - 1
