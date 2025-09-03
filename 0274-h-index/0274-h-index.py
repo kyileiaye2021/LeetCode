3e9e01eceb766c940d3f68dc8f70c194c4efa1a8
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-02 17:45:02 -0700

    Time: 0 ms (100%), Space: 18.2 MB (40.68%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,49 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        # input: [1,3,3,3,4]
+        # output: 3
+
+        # input: [3,1,2,6,7,1]
+        # output: 3
+
+        # [1,1,2,3,6,7]
+       
+        # input: [1,2,2,0,3]
+        # output: 2
+
+        # input: [1,2,3]
+        # output: 2
+
+        # input: [0,1,0,3]
+        # output: 1
+
+        # input: [0,0,0]
+        # output: 0
+
+        # input: [3, 100]
+        # output: 2
+
+        # input: [5]
+        # output: 1
+
+        # input: [1]
+        # output: 1
+
+        # input: [1,3,1]
+        # output: 1
+
+        # brute force --> nested loop
+        # sort the array / count the nums when iterating the array from end '
+        citations.sort()
+        n = len(citations)
+        
+        for i in range(n):
+
+            # num of books that have at least citations[i] in the arr => n-i
+            # check if citations at ith position is at least n-i
+
+            if citations[i] >= n - i:
+                return n - i
+        return citations[0]
+        
+
