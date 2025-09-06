b62c6c0ebca53ef5e01547c683f6dae24fd1493a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-06 03:01:57 -0700

    Time: 0 ms (100%), Space: 18 MB (43.02%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -3,9 +3,8 @@ class Solution:
         # split the words into the ele in the list
         # in the list, reverse the ele with 2 pointers
         # combine them back to the str
-        s = s.strip()
-        temp_lst = s.split(" ")
-        temp_lst2 = []
+        # s = s.strip()
+        temp_lst = s.split()
         i = 0
         j = len(temp_lst) - 1
         while i < j:
@@ -13,11 +12,21 @@ class Solution:
             i += 1
             j -= 1
         
-        for ele in temp_lst:
-            if ele != "":
-                temp_lst2.append(ele)
-        print("temp_lst: ", temp_lst)
-        print("temp_lst2: ", temp_lst2)
-        new_str = " ".join(temp_lst2)
+        # for ele in temp_lst:
+        #     if ele != "":
+        #         temp_lst2.append(ele)
+        # print("temp_lst: ", temp_lst)
+        # print("temp_lst2: ", temp_lst2)
+        new_str = " ".join(temp_lst)
         return new_str
+    
+        # words = s.split()
+        # res = []
+        # print(words)
+        # for i in range(len(words) - 1, -1, -1):
+        #     res.append(words[i])
+        #     if i != 0:
+        #         res.append(" ")
+
+        # return "".join(res)
         
