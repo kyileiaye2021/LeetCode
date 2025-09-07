1905849d28dc79f8b93b8b89ad452b5ce2f4744a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-06 21:36:48 -0700

    Time: 22 ms (73.12%), Space: 29.3 MB (39.18%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,37 +1,17 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        # happy cases
-        # input: s = "AAAAACCCCCAAAAACCCCCAAAAA"
-        # output: "AAAAACCCCC", "CCCCCAAAAA"
-
-        # input: s = "AAAAAAAAAAAAAA"
-        # output: "AAAAAAAAAA"
-
-        # edge cases
-        # input: s = "AAA"
-        # output: None
-
-        # input: s = "ACATGGATGCCCATA"
-        # output: None
-
-        # use set 
-        # iterate thru the ele in the str upto len - 10
-        #   make a window of size 10 starting from the curr ele
-        #   check if the substr is in the set
-        #       add it to the res list
-        # return the res list
-        unique_substr = set()
+        
+        visited = set()
         res = set()
-
-        n = len(s)
-        if n <= 10:
-            return list(res)
-
-        for i in range(n - 9):
-            curr_window = s[i: i + 10]
-            if curr_window in unique_substr:
+        
+        for i in range(len(s) - 9):
+            curr_window = s[i:i + 10]
+            if curr_window in visited:
                 res.add(curr_window)
             else:
-                unique_substr.add(curr_window)
-
+                visited.add(curr_window)
         return list(res)
+            
+        
+        
+     
