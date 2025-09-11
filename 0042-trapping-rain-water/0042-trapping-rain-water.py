9f346247597956a2c0838726826328f5de0b3391
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-11 15:42:07 -0700

    Time: 43 ms (5.1%), Space: 19.7 MB (5.33%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,71 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # happy case
+        # input: height = [1,0,1]
+        # output: 1
+
+        # input: height = [0, 2, 1, 0, 2]
+        # output: 3
+
+        # input: height = [0, 1, 0, 1, 0]
+        # output: 1
+
+        # input: height = [1, 0, 0, 0, 1]
+        # output: 3
+
+        # edge cases
+        # input: [0,0,0,0]
+        # output; 0
+
+        # input: [0,1,0]
+        # output: 0
+
+        # input: [1]
+        # output: 0
+
+        # on the left and right side, we need to keep track of the longest bar
+        # we have l and r pointers at the end
+        # we get the min of left and right pointers
+        # do min-l_ele and min - r_ele
+        # if min-l_ele > 0: add it to the total
+        # if min-r_ele > 0: add it to the total
+        # update the min
+
+        # first thing - we have to get the max left and max right
+        # second - we have to get the min of max_left and max_right
+        n = len(height)
+        max_left = [0] * n
+        max_right = [0] * n
+        min_num = [0] * n
+
+        curr_l = 0
+        for i, h in enumerate(height):
+            max_left[i] = curr_l
+            curr_l = max(curr_l, height[i])
+
+        print(max_left)
+
+        curr_r = 0
+        for i in range(n-1, -1, -1):
+            max_right[i] = curr_r
+            curr_r = max(curr_r, height[i])
+
+        print(max_right)
+
+        for i in range(n):
+            min_num[i] = min(max_left[i], max_right[i])
+
+        print(min_num)
+
+        res = 0
+        for i, h in enumerate(height):
+            if (min_num[i] - h) > 0:
+                res += min_num[i] - h
+
+        return res
+
+        
+
+
+
+
