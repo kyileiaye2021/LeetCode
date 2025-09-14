3f5204b49c27d5aaade3a5c53970e87245476790
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-09-14 10:59:42 -0700

    Time: 0 ms (100%), Space: 17.6 MB (83.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,33 +4,48 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        # l, r = 0, len(nums) - 1
-        # iterate thru the list
-        #   when we meet 2, replace with r pointer ele 
-        #       decrement r pointer
-        #   when we meet 0, replace with l pointer ele
-        #       increment l pointer
+        # happy cases
+        # input: nums = [1,2,0,2,1]
+        # output: [0,1,1,2,2]
+
+        # input: nums = [2,1,0,2,2]
+        # output: [0, 1,,2,2,2]
+
+        # edge cases
+        # input: [2,0,1]
+        # output: [0,1,2]
+
+        # input: []
+        # output: []
+
+        # brute force (nested for loop)
+        # two pointers - i, j
+        # iterate thru the list with k
+        #   if curr ele is 0
+        #       swap with ith position 
+        #       increment i by 1
+        #       decrement k by 1
+        #   elif curr ele is 2
+        #       swap with jth position 
+        #       decrement j by 1
+        #       decrement k by 1
+        #   increment k by 1
 
         i = 0
-        l = 0
-        r = len(nums) - 1
-        while i <= r:
-            if nums[i] == 2:
-                temp = nums[i]
-                nums[i] = nums[r]
-                nums[r] = temp
-                r -= 1
+        j = len(nums) - 1
+        k = 0
 
-            elif nums[i] == 0:
-                temp = nums[i]
-                nums[i] = nums[l]
-                nums[l] = temp
-                l += 1
+        while k <= j:
+            if nums[k] == 0:
+                nums[i], nums[k] = nums[k], nums[i]
                 i += 1
 
-            else:
-                i += 1
+            elif nums[k] == 2:
+                nums[j], nums[k] = nums[k], nums[j]
+                j -= 1
+                k -= 1
+            
+            k += 1
 
         
-
         
