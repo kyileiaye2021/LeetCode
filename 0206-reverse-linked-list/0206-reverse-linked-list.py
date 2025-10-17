4b4f6647a41c7ac6f7607516f18f2c770be2a01a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-10-17 15:57:11 -0700

    Time: 0 ms (100%), Space: 18.9 MB (5.1%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,47 +4,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def helper(self, head):
+        if not head or not head.next:
+            return head
+        revHead = self.helper(head.next)
+        head.next.next = head
+        head.next = None
+        return revHead
+
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # happy cases
-        # input: head = [1,2,3,4]
-        # output: [4,3,2,1]
-
-        # input: head = [6,-1,3]
-        # output: [3,-1,6]
-
-        # edge cases:
-        # input: head = [3]
-        # output: [3]
-
-        # input: head = []
-        # output: []
-
-        # Brute force - O(n) space using two arrays
-        # use an array to store the data of every node by traversing the linked list
-        # create an another array to store the elements in reverse order
-
-        # Bruter force - using one array and two pointers - O(n) space
-        # use an array to store the data of every node by traversing the linked list
-        # create two pointers at the edges of the array and swap the two pointer elements and shrink the size between two pointers
-
-        # Two pointer
-        # create a prev pointer to None
-        # initialize the curr pointer to head
-        # traverse the list until curr pointer reaches None
-        #   next node = curr.next
-        #   curr.next = prev
-        #   prev = curr
-        #   curr = next node
-        # return prev
-
-        if not head:
-            return None
-        prev = None
-        curr = head
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-
-        return prev
+        return self.helper(head)
+        
