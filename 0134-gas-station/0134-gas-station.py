30ef0b6e73126179954fa2cf281424bc54cf63fa
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-10-18 10:11:54 -0700

    Time: 25 ms (50.87%), Space: 23.4 MB (19.68%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,78 +1,28 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # detect the circle
+        # find the total of the gas amount and cost
+        # check if it cannnot cover the cost of whole journey
+        #   return -1
 
-        # define the start point first
-        # for start point
-        # go thru the 2 arrs at the same time
-        # compare if gas[i] > cost[i]
-        #   set that i pos as the start point
-        # if start point is still none --> return -1
+        # itereate thru the ele in the gas
+        #   check if the gas is greater than equal to the cost
+        #       return the pos
 
-        # curr tank = 0
-        # curr = start point
-        # until curr get back to the start point
-        #   add curr gas to curr tank
-        #   energy = curr tank - curr cost
-        #   if that becomes neg
-        #       return -1
-        # if curr < len(gas)
-        #   increment curr by 1
-        # else: reset it to 0
-
-        # i = 0
-        # start = None
-        # while i < len(gas):
-        #     if gas[i] > cost[i]:
-        #         start = i
-        #         break
-        #     i += 1
-        
-        # if not start:
-        #     return -1
-
-        # print(f"Start index: {start}")
-        # curr_tank = gas[start]
-        # curr = start + 1
-
-        # if curr >= len(gas):
-        #     curr = 0
-
-        # while curr != start:
-        #     if curr >= len(gas):
-        #         curr = 0
-
-        #     print(f"curr: {curr}")
-        #     energy = curr_tank - cost[curr - 1]
-        #     if energy < 0:
-        #         return -1
-
-        #     energy += gas[curr]
-        #     print(f"Energy in each step: {energy}")
-        #     curr += 1
-        # return curr
-
-        # first we need to find if the gas can cover the cost 
-        # check if the sum of all gas is >= the sum of all cost
-        # 
-        # start point
-        # get the energy
-        # tank += energy 
-        # if energy is neg --> go to next ele
-
-        # check if we can go thru the whole loop to get back to the start point
+        res = 0
+        total = 0
         if sum(gas) < sum(cost):
             return -1
 
-        # for finding start point
-        tank = 0
-        start = 0
         for i in range(len(gas)):
-            energy = gas[i] - cost[i]
-            tank += energy
+            
+            total += (gas[i] - cost[i])
 
-            if tank < 0:
-                tank = 0
-                start = i + 1
+            if total < 0:
+                total = 0
+                res = i + 1
 
-        return start
+        return res
+
+
+
+
