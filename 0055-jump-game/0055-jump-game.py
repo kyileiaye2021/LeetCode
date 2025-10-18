ffa61075dadde179590702bda15419f277322a2d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-10-18 00:53:55 -0700

    Time: 16 ms (73.14%), Space: 18.6 MB (69.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,46 +1,23 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # input: nums = [2,3,1,1,4]
-        # output: true
 
-        # input: nums = [1,2,0,0,1]
-        # output: false
-
-        # input: nums = [1,1,1,1]
-        # output: true
-
-        # input: nums = [3,2,1,0]
-        # output: true
-
-        # input: nums = [3,2,1,0,4]
-        # output: false
-
-
-        # input: nums = [0, 2, 1,2]
-        # output: false
-
-        # input: nums = [1,0,1,0]
-        # output: false
-
-        # input: nums = [3,3,1,0,4]
-        # output: true
-
-        # input: nums=[0]
-        # output: true
-
-        # input: nums = [1]
-        # output: true
-
-        # if index 0 ele is 0 --> return false
-        # two pointer
-        remaining = 0
-        for n in range(len(nums)):
-            print(f"n: {n}")
-            if remaining < 0:
+        # total = first ele in nums
+        # iterate thru the nums upto last index
+        #   decrement 1 step from total
+        #   if total < 0:
+        #       return False
+        #   if total == 0
+        #       add curr jump len to total
+        # if total >= 0 -> we can reach to the end
+        
+        total = nums[0]
+        for i in range(0, len(nums) - 1):
+            if total < 0:
                 return False
-            elif nums[n] > remaining:
-                remaining = nums[n]
-            remaining -= 1
-            print(f"remaining: {remaining}")
-        return True
+            if nums[i] > total:
+                total = nums[i]
+            total -= 1
 
+        return True if total >= 0 else False
+
+       
