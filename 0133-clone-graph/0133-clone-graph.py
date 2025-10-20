9aa87b22a1011bfc358b533a8e727a93b99fa3ab
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-10-20 12:32:49 -0700

    Time: 35 ms (93.69%), Space: 18.1 MB (56.78%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -8,65 +8,21 @@ class Node:
 
 from typing import Optional
 class Solution:
+    def dfs(self, node, hashmap):
+        # base case
+        if node in hashmap:
+            return hashmap[node]
+
+        copy = Node(node.val)
+        hashmap[node] = copy
+
+        for nei in node.neighbors:
+            copy.neighbors.append(self.dfs(nei, hashmap))
+        return copy
+
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        # happy case
-        # input: [[2,4],[1,3],[2,4],[1,3]]
-        # output: [[2,4],[1,3],[2,4],[1,3]]
-
-        # input: [[2,3], [1], [1]]
-
-        # edge case
-        # input: []
-        # output: []
-
-        # input: [[]]
-        # output: [[]]
-
-        # bfs
-        # queue 
-        # hashmap {old node : new node}
-
-        # bfs 
-        # create a queue
-        # hashmap  {old node: new node}
-        # add the first node into the queue
-        # add the first node into the hashmap
-        # until the queue is empty
-        #   pop out the node 
-        #   for each neighbor
-        #       check if the neighbor node is already not in the hashmap
-        #           create a new node
-        #           add the new node into the hashmap
-        #           add it to the queue
-        #           append that node to the curr neighbor's list
-        #        if the neighbor is already in the hashmap
-        #           append that node to the curr neighbor's list
-
-        queue = collections.deque()
-        oldToNew = {}
-
-        if not node:
-            return None
-
-        queue.append(node)
-        new_node = Node(node.val, [])
-        oldToNew[node] = new_node
-
-        while queue:
-            curr_node = queue.popleft()
-            
-            cloned_node = oldToNew[curr_node]
-
-            for nei in curr_node.neighbors:
-
-                if nei not in oldToNew:
-
-                    # make a new node and mark it as cloned
-                    new_nei = Node(nei.val, [])
-                    oldToNew[nei] = new_nei
-
-                    queue.append(nei)
-
-                cloned_node.neighbors.append(oldToNew[nei])
-
-        return oldToNew[node]
+        # hashmap to store the nodes {old node : new copy node}
+        # dfs
+        hashmap = {}
+        return self.dfs(node, hashmap) if node else None
+        
