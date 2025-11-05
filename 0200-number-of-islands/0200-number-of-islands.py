f1d6c7425e22fdfd63e649466d2e88a02dba152d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-11-04 20:33:52 -0800

    Time: 221 ms (97.91%), Space: 20.3 MB (42.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,57 +1,20 @@
 class Solution:
-    def dfs(self, r, c, grid):
-        grid[r][c] = '0'
-
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-        for dx, dy in dirs:
-            new_r = r + dx
-            new_c = c + dy
-
-            if new_r in range(len(grid)) and new_c in range(len(grid[0])) and grid[new_r][new_c] == '1':
-                self.dfs(new_r, new_c, grid)
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        # dfs
-        # mark the cell as 0
-        # go to neighbors
-        # for each neighbor 
-        #   check they are within boundary and they are 1
-        #       call dfs func on the cell
-
-        # iterate thru the ele in the 2d grid
-        #   if the cell is 1 
-        #       call dfs func on the cell
-        # happy cases
-
-
-        rows = len(grid)
-        cols = len(grid[0])
-        count = 0
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == '1':
-                    self.dfs(r, c, grid)
-                    count += 1
-
-        return count
-
-#         Input: grid = [
-#   ["1","1","1","1","0"],
-#   ["1","1","0","1","0"],
-#   ["1","1","0","0","0"],
-#   ["0","0","0","0","0"]
-# ]
-
-# output: 1
-
-
-        # edge cases
-#         Input: grid = [
-#   ["0","0","1","1","0"],
-#   ["1","1","0","1","0"],
-#   ["1","1","0","0","0"],
-#   ["0","0","0","0","0"]
-# ]
-
+        # [[1,1,0,1]]
+        m, n = len(grid), len(grid[0])
+        visited = [[False for j in range(n)] for i in range(m)]
+        islands = 0
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n: return
+            if grid[i][j] == '1' and not visited[i][j]:
+                visited[i][j] = True
+                dfs(i - 1, j)
+                dfs(i + 1, j)
+                dfs(i, j - 1)
+                dfs(i, j + 1)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1' and not visited[i][j]:
+                    islands += 1
+                    dfs(i, j)
+        return islands
