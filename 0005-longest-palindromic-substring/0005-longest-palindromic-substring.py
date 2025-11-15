dc74bc0fb490600c61afc03ffb0b275e4b695fd6
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-11-15 12:45:12 -0800

    Time: 254 ms (83.76%), Space: 17.8 MB (59.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,60 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        # aba 
-        #  |
-        # l, r will be expanded
 
+        # the word is not fully panlindrome
+        # we have to find the longest substr that is panlindrome 
+
+        # iterate thru each char in the word
+
+        #   # for odd len 
+        #   start from curr i (l, r pointers)
+        #   while l and r are the same
+        #       move l by 1 to left
+        #       move r by 1 to right
+        #   create the curr substr
+        #   check if the substr len is > the longest
+        #       update the longest substr with curr substr
+        #       update the longest len with curr len
+        
+        #   for even len
+        #   start l at curr
+        #   start r at i + 1
+        #   while l and r are the same
+        #       move l by 1 to left
+        #       move r by 1 to right
+        #   create the curr substr
+        #   check if the substr len is > the longest
+        #       update the longest substr with curr substr
+        #       update the longest len with curr len
         longest = 0
-        longest_pan = ''
+        longest_substr = ""
 
         for i in range(len(s)):
-            
-            l, r = i, i
+            # for odd len
+            l = i 
+            r = i
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                curr_len = r - l + 1
-                if curr_len > longest:
-                    longest = curr_len
-                    longest_pan = s[l:r+1]
-
                 l -= 1
                 r += 1
 
-            # even len of s
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                curr_len = r - l + 1
-                if curr_len > longest:
-                    longest = curr_len
-                    longest_pan = s[l:r+1]
+            curr_substr = s[l + 1: r]
+            if len(curr_substr) > longest:
+                longest_substr = curr_substr
+                longest = len(curr_substr)
 
+
+            # for even len
+            l = i
+            r = i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
-        return longest_pan
-            
-            
+
+            curr_substr = s[l + 1: r]
+            if len(curr_substr) > longest:
+                longest_substr = curr_substr
+                longest = len(curr_substr)
+        return longest_substr
+
+
+        
