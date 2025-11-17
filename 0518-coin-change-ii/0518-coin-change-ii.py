217cb0c1ac25f639b3e365dfe9014550c18ae9f3
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-11-16 16:57:24 -0800

    Time: 1851 ms (5.02%), Space: 149.9 MB (21.47%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def dp_helper(self, i, amount, coins, memo):
+        # base case
+        if i >= len(coins) or amount < 0:
+            return 0
+        
+        if amount == 0:
+            return 1
+
+        # retrieve from cache if the count is already stored
+        if (i, amount) in memo:
+            return memo[(i, amount)]
+
+        # include the curr ith ele
+        include = self.dp_helper(i, amount - coins[i], coins, memo)
+
+        # exclude the curr ith ele
+        exclude = self.dp_helper(i + 1, amount, coins,memo)
+
+        # num of counts from 2 paths
+        memo[(i, amount)] = include + exclude
+        return memo[(i, amount)]
+
+    def change(self, amount: int, coins: List[int]) -> int:
+        memo:Dict[Tuple(int, int): int] = {}
+        return self.dp_helper(0, amount, coins, memo)
+        
+        # recursion
+        # base case
+        # if the amount == 0
+        #   increment the count by 1
+        #   return 
+        # if amount < 0
+        #   return 0
+
+        # iterate thru the coins
+        #   diff = amount - curr coin
+        #   return call recursive on the diff
+        
+        
