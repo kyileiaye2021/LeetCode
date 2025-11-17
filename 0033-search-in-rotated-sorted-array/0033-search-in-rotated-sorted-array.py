2b5283c323f5732c8707cde2b77706fcf04831ad
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-11-16 19:19:05 -0800

    Time: 0 ms (100%), Space: 18.1 MB (66.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -59,15 +59,11 @@ class Solution:
                     r = mid - 1
 
             else: 
-                if target > nums[mid]:
-                    if target > nums[r]:
-                        r = mid - 1
-                    else:
-                        l = mid + 1
-                
-                else:
+                if target < nums[mid] or target > nums[r]:
                     r = mid - 1
-
+                else:
+                    l = mid + 1
+                
         return -1
 
         
