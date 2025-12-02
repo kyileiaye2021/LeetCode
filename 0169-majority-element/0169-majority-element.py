d9105fac2784a56b56e9aa57b8dee2b8b1b1a9a9
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-01 21:32:00 -0800

    Time: 15 ms (15.61%), Space: 19.4 MB (19.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,38 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # nums = [3,2,3]
-        # output: 3
-
-        # nums = [2, 2, 1, 1, 2, 3]
-        # output: 2
-
-        # nums = [1,1,1]
-        # output: 1
-
-        # nums = [3,2, 2, 3, 2, 3]
-        # nums = [1]
-        # output: 1
-
-        # # hashmap 
-        # freq_map = defaultdict(int)
-        # for n in nums:
-        #     freq_map[n] += 1
-        
-        # majority = len(nums) // 2
-        # for n, freq in freq_map.items():
-        #     if freq > majority:
-        #         return n
-
-        # moore voting algorithm
-        count = 0
-        candidate = 0
-
+        map = {}
         for n in nums:
-            if count == 0:
-                candidate = n
+            map[n] = map.get(n, 0) + 1
 
-            if n == candidate:
-                count += 1
-            else:
-                count -= 1
-        return candidate
+        for n, freq in map.items():
+            if freq > (len(nums) // 2):
+                return n
+
+       # hashmap to count freq of nums
+       # iterate thru the hashmap
+       #    check if the curr val is > n // 2
+       #        return that key
+
+    
