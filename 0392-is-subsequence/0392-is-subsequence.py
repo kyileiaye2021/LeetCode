5e96e917c58a7298e9a5dc2bb2c1fb1daf7bde8e
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-01 21:51:46 -0800

    Time: 0 ms (100%), Space: 17.8 MB (67.49%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,15 +1,36 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
 
-        # 2 pointers
-        if len(s) > len(t):
-            return False
+        # happy case
+        # s = "abc", t = "abcd"
+        # true
+
+        # s = "abc", t = "xacbd"
+        # false
+        
+        # edge case
+        # s = "abc", t = ""
+        # false
+
+        # s = "", t = "abc"
+        # true
+
+        # i, j = 0, 0
+        # while i < len(s) and j < len(t):
+        #   check if curr i ele == curr j ele
+        #       move i and j
+        #   else
+        #       move j
+        # return true if i >= len(s) else false
 
         i, j = 0, 0
-        while i < len(s) and j < len(t):
+        s_len = len(s)
+        t_len = len(t)
+
+        while i < s_len and j < t_len:
             if s[i] == t[j]:
                 i += 1
             j += 1
 
-        return True if i >= len(s) else False
-        
+        return True if i >= s_len else False
+            
