5974b474475740ade4cd86c939590373857d78a4
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-01 22:55:11 -0800

    Time: 36 ms (20.65%), Space: 27 MB (11.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,38 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # happy cases
-        # nums = [1,2,3,4]
-        # output  = [24, 12, 8, 6]
+        res = []
+        prefix_product = [1] * (len(nums) + 1)
+        postfix_product = [1] * (len(nums) + 1)
 
-        # edge cases
-        # nums = []
-        # output: []
-
-        # nums = [2, 0]
-        # output: [0, 2]
-
-        # nums = [-1, -1, -1]
-        # output: [1, 1, 1]
-
-        # nums = [-1, 0, -2]
-        # output: [0, 2, 0]
-
-        # brute force
-        # nested loop (O(n^2))
-
-        # prefix sum 2 arrays O(n) time , O(n) space
-        # prefix sum var left, and right
-
-        left = 1
-        right = 1
-        res = [1] * len(nums)
         for i in range(len(nums)):
-            res[i] = left
-            left = left * nums[i]
+            prefix_product[i + 1] = prefix_product[i] * nums[i]
 
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= right
-            right = right * nums[i]
+        for i in range(len(nums) -1, -1, -1):
+            postfix_product[i] = postfix_product[i + 1] * nums[i]
+
+        for i in range(len(nums)):
+            res.append(prefix_product[i] * postfix_product[i + 1])
 
         return res
-    
+        
