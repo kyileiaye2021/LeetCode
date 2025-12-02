ed2653828a56b3cfdac7ae94ea80f723a457b9f4
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-01 20:55:00 -0800

    Time: 99 ms (47.35%), Space: 26.9 MB (32.77%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,28 +1,32 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        cheapest = float('inf')
 
-        for i in range(len(prices)):
-            if cheapest != float('inf'):
-                profit = max(profit, prices[i] - cheapest)
+        # i, j - 0, 1
+        # iterate thru the arr until j reaches the end
+        #   if j ele < i ele
+        #       move i to j
+        #       move j by 1
+        #   else
+        #       curr profit = j ele - i ele
+        #       update max profit if curr profit > max profit
+        # return max profit
+
+        i = 0
+        j = 1
+        max_profit = 0
+        while j < len(prices):
+            if prices[j] <= prices[i]:
+                i = j
+
+            else:
+                curr_profit = prices[j] - prices[i]
+                max_profit = max(max_profit, curr_profit)
+
+            j += 1
+
+        return max_profit
+
+                
+
+
         
-            cheapest = min(prices[i], cheapest)
-
-        return profit
-
-       # store the max profit for each day so far
-
-       # profit = 0
-       # cheapest price = inf
-       # temp list to store the max profit for each day
-
-       # iterate thru the list
-       #    profit[i] = ith price - cheapest price
-       #    update cheapest price with the curr price if curr price is smaller
-       # iterate thru temp list and return the max profit
-
-
-
-
-
