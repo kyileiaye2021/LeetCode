2f29a9b8d0e81c74a82dd68076ee3a8841d294b9
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-01 23:06:55 -0800

    Time: 6 ms (47.04%), Space: 18.8 MB (79.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,42 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        # we maintain the index i at positions 0
+        # j will go to next elements 
+        # if j ele is not 0
+        #   put j ele to i
+        #   i += 1
+        #   j += 1
+        # else: j += 1
+        # make all ele 0 from i to j
         
-        # happy case
-        # input: [0, 1, 0]
-        # output: [1, 0, 0]
-
-        # edge cases
-        # input: [0, 0]
-        # output: [0, 0]
-
-        # input: [1]
-        # output: [1]
-
-        # Brute force (O(n))
-        # Dictionary (O(n))
-        # Two pointer (O(n))
-
-        # low level steps 
-        # initialize two pointers i and j
-        # i, j = 0, 0
-        # iterate over the list until i reaches the end of the lst
-        #   check if the i ele is not 0
-        #       assign the i ele in the j position 
-        #       increment both i and j by 1
-        #   increment i
-        # iterate over the list from jth position to the end of the list
-        #   assign the value of cur ele to 0
-
-        i, j = 0, 0
-
-        while i < len(nums):
-            if nums[i] != 0:
-                nums[j] = nums[i]
-                j += 1
-            i += 1
-        
+        i = 0
+        j = 0
         while j < len(nums):
-            nums[j] = 0
-            j += 1
+            if nums[j] != 0:
+                nums[i] = nums[j]
+                i += 1
 
+            j += 1
+        
+        for k in range(i, len(nums)):
+            nums[k] = 0
+
+        
+
+
+        
