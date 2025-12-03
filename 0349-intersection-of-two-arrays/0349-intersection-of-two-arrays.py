f20082e220e07ea2fd03fa5f3a2991237f178da5
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-02 23:59:09 -0800

    Time: 0 ms (100%), Space: 17.8 MB (92.88%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,26 +1,22 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # nums 1 = [1,2,2,1], nums 2 = [2,2]
+        # [2]
+
+        # [4,9,5], [9,4,9,8]
+        # [4, 9]
+
         # set
-        # regular list iteration
+        # iterate thru nums1
+        #   check if the curr ele is in nums2
+        #       add it to set
+        # change set to list and return the list
 
-        # res list
-        # set
-        # create an empty set and populate the set with nums1 ele
-        # iterate over the ele in nums2
-        #   check if the ele in set and not in the res list yet
-        #       append the curr ele
-        # return res list
+        hash_set = set()
+        nums2 = set(nums2)
 
-        res = []
-        n = []
-
-        for ele in nums1:
-            if ele not in n:
-                n.append(ele)
+        for n in nums1:
+            if n in nums2:
+                hash_set.add(n)
         
-        for ele in nums2:
-            if ele in n and ele not in res:
-                res.append(ele)
-
-        return res
-
+        return list(hash_set)
