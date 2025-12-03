69a0dafb824701a9c4c0e918b6096545454888ff
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-02 23:46:41 -0800

    Time: 17 ms (38.31%), Space: 31.6 MB (29.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,39 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        
-        # Happy cases
-        # input - [1,2,3,1]
-        # output - true
-        
-        # input - [1,1,3,4,3,2,4,1]
-        # output - true
-        
-        # Edge cases
-        # input - []
-        # output - false
-        # input - [2]
-        # output - false
-        
-        # dictionary approach
-        # create a dict
-        # iterate over the list
-        #   check if the ele is in the dict
-        #       increment the ele by 1
-        #   set the ele in the dict with the val of 1
-        # iterate over the dict
-        #   check if the ele in the dict has more than 1 freq
-        #       return true 
-        # return false
-        
-        freq = {}
-        for ele in nums:
-            if ele not in freq:
-                freq[ele] = 1
-            else:
-                freq[ele] +=1
-                
-        for key, value in freq.items():
-            if value > 1:
+        hash_set = set()
+        for n in nums:
+            if n in hash_set:
                 return True
-            
+
+            hash_set.add(n)
+
         return False
