cc1b9a8ed3a2acb6ed250b05ab339bc3f514f43d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-03 00:33:04 -0800

    Time: 0 ms (100%), Space: 17.8 MB (32.48%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,32 +1,35 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # happy cases
-        # input: [3,2,2,3], val = 3
-        # output: 2,  [2,2]
+        # nums = [3,2,3,2], val = 3
+        # output: [2,2], 2
 
-        # edge cases
-        # input: [], val = 1
-        # output: 0, []
-        # input: [1], val = 1
-        # output: 0, []
+        # nums = [], val = 3
+        # output: [], 0
 
-        # brute force
-        # two pointer 
+        # nums = [1,2,3] val = 4
+        # output: [1,2,3], 3
 
-        # low level steps
-        # initialize two pointers to 0 (i, j)
-        # iterate over the list until i reaches to the end of the list
-        #   check if the i ele is not equal to the val
-        #       increment both i and j by 1
-        #   else:: increment i by 1
-        # return j
+        # two pointers
 
-        i, j = 0, 0
+        # i - maintain the index postion where the ele is not equal to val
+        # j - the iterator 
+        # 
+        # iterate thru the ele
+        #   if curr ele = val
+        #       move j by 1
+        #   else
+        #       assign j ele to i
+        #       move i by 1 
+        #       move j by 1
+        # return i
 
-        while i < len(nums):
-            if nums[i] != val:
-                nums[j] = nums[i]
-                j += 1
-            i += 1
+        i = 0
+        j = 0
 
-        return j
+        while j < len(nums):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+
+        return i
