efebf7ff9bed6709bdb9a5f0428e70c6e39fa3a4
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-04 23:10:08 -0800

    Time: 15 ms (80.93%), Space: 18 MB (33.51%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,50 +1,32 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        # brute force: nested for loop O(n^2)
+        # sliding window
+        # set to maintain the unique ele
+        # i, j pointers
+        # max window
+        # while j is < len(s)
+        #   while the curr ele is in the set
+        #       remove the i ele 
+        #   
+        #   add it to the set
+        #   increment j by 1
+        #   update the curr window size
+        #   update the max window size
 
-        # happy cases
-        # s = "abcabcbb"
-        # output: 3
+        unique = set()
+        i, j = 0, 0
+        max_window = 0
+        curr_window = 0
 
-        # s = "abcecce"
-        # output: 4
+        while j < len(s):
+            while s[j] in unique:
+                unique.remove(s[i])
+                i += 1
 
-        # edge cases
-        # s = "mmmmm"
-        # output: 1
-
-        # s = "pkedng"
-        # output: 6
-
-        # slding window
-        # l, r = 0
-        # max len = 0
-        # duplicate set 
-        # iterate thru the s until r reaches the end
-        #   if r ele is already visited
-        #       until r ele is removed from the set
-        #           remove the l ele from the set
-        #           move l by 1
-        #           
-        #   add r ele to set
-        #   keep track of curr window size and update the max len if needed
-        #   move r by 1
-        # return max len
-        
-        l, r = 0, 0
-        longest = 0
-        visited = set()
-
-        while r < len(s):
-            if s[r] in visited:
-                while s[r] in visited:
-                    visited.remove(s[l])
-                    l += 1
-
-            visited.add(s[r])
-            r += 1
-            curr_window = r - l
-            longest = max(longest, curr_window)
-
-        return longest
-        
-        
+            unique.add(s[j])
+            j += 1
+            curr_window = j - i
+            max_window = max(max_window, curr_window)
+            
+        return max_window        
