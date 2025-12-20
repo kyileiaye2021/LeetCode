263c0a93e23d341b47dab01ec194464eca0c77b8
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-19 23:46:59 -0800

    Time: 7 ms (19.55%), Space: 18.2 MB (98.89%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,38 +3,37 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # set to store the rows and cols to make zeros
-        # iterate thru the rows and cols
-        # spot the zeros in each col and row
-        # if 0 is found
-        #   store the row and col into the set
+        # iterate thru the ele and keep track of the positions where the ele is 0
+        # row set
+        # col set
 
-        # iterate thru the row set
-        #   iterate thru the cols in the row
-        #       make them all 0
+        # iterate thru the rows
+        #   if the curr row is in row set
+        #       iterate thru the cols
+        #           make all col ele 0
 
-        # iterate thru the col set
-        #   iterate thru the rows in the col
-        #       make them all 0
+        # iterate thru the col
+        #   if the curr col is in col set
+        #       iterate thru the row
+        #           make all row ele 0
 
         row_set = set()
         col_set = set()
-        rows = len(matrix)
-        cols = len(matrix[0])
 
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c] == 0:
-                    row_set.add(r)
-                    col_set.add(c)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    row_set.add(i)
+                    col_set.add(j)
 
-        for r in row_set:
-            for c in range(cols):
-                matrix[r][c] = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in row_set:
+                    matrix[i][j] = 0
+        
+        for j in range(len(matrix[0])):
+            for i in range(len(matrix)):
+                if j in col_set:
+                    matrix[i][j] = 0
 
-        for c in col_set:
-            for r in range(rows):
-                matrix[r][c] = 0
-
-            
- 
+        print(matrix)
