a6361c10eacb3d944f81e922bffeb928c9cf79ac
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-21 01:26:17 -0800

    Time: 0 ms (100%), Space: 17.4 MB (97.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -8,25 +8,47 @@ class Solution:
         r = len(matrix[0]) - 1
 
         while l < r:
-
             for i in range(r - l):
                 top = l
                 bottom = r
 
-                top_left = matrix[top][l + i]
+                bottom_right = matrix[bottom][r - i]
 
-                # move bottom left to top left
-                matrix[top][l + i] = matrix[bottom - i][l]
-
-                # move bottom right to bottom left
-                matrix[bottom - i][l] = matrix[bottom][r - i]
-
-                # move top right to bottom right
+                # mv top right to bottom right
                 matrix[bottom][r - i] = matrix[top + i][r]
 
-                # move top left to top right
-                matrix[top + i][r] = top_left
+                # mv top left to top right
+                matrix[top + i][r] = matrix[top][l + i]
+
+                # mv bottom left to top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                # mv bottom right to bottom left
+                matrix[bottom - i][l] = bottom_right
 
             l += 1
             r -= 1
+
+        # while l < r:
+
+        #     for i in range(r - l):
+        #         top = l
+        #         bottom = r
+
+        #         top_left = matrix[top][l + i]
+
+        #         # move bottom left to top left
+        #         matrix[top][l + i] = matrix[bottom - i][l]
+
+        #         # move bottom right to bottom left
+        #         matrix[bottom - i][l] = matrix[bottom][r - i]
+
+        #         # move top right to bottom right
+        #         matrix[bottom][r - i] = matrix[top + i][r]
+
+        #         # move top left to top right
+        #         matrix[top + i][r] = top_left
+
+        #     l += 1
+        #     r -= 1
             
