b6dd158ef7232f3d863b7ae210b1ac736e15b3f9
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-26 17:09:04 -0800

    Time: 679 ms (29.03%), Space: 20 MB (98.36%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,43 +1,56 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sort the list
-        # iterate thru the list
-        #   two pointers l, r
-        #   l = i + 1
-        #   r = end of the list
-        #   while l < r
-        #       check if the l ele + r ele + curr ele > 0
-        #           decrement r
-        #       elif < 0
-        #           increment l
-        #       else:
-        #           add the ele triplet in the list
-        #           decrement r and increment l
+        
+        # nums = [0,1,1]
+        # output: []
 
-        nums.sort()
+        # nums = [0,0,0]
+        # output: [0,0,0]
+
+        # nums = [1,-1,0]
+        # output: [1, -1, 0]
+
+        # nums = [1, -1, 0, -1]
+        # output: [1, -1, 0]
+
+        # nums = [1, -1, 0, -1, 1, 0]
+        # output: [[1, -1, 0], [-1, 1, 0]]
+
+        # nums =[-1, 0, 1, 2, -1, -4]
+        # output: [[-1,-1,2], [-1, 0, 1]]
+
+        # [-4, -1, -1, -1,  0, 2, 2]
+
         res = []
+        # sort the arr
+        nums = sorted(nums)
 
+        # iterate thru the arr
         for i in range(len(nums)):
-
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
+            curr = nums[i]
+        
+
             l = i + 1
             r = len(nums) - 1
-            
+
             while l < r:
-                if (nums[l] + nums[r] + nums[i]) > 0:
+                if nums[l] + nums[r] + curr < 0:
+                    l += 1
+
+                elif nums[l] + nums[r] + curr > 0:
                     r -= 1
 
-                elif (nums[l] + nums[r] + nums[i]) < 0:
-                    l += 1
-
                 else:
-                    curr_list = [nums[i], nums[l], nums[r]]
-                    res.append(curr_list)
+                    res.append([curr, nums[l], nums[r]])
                     l += 1
-
-                    while nums[l] == nums[l - 1] and l < r:
+                    r -= 1
+                    while l < len(nums) and nums[l] == nums[l - 1]:
                         l += 1
 
         return res
+        # l and r pointer for the other ele in the arr to the right of curr
+
+        # apply binary search on the remaining arr
