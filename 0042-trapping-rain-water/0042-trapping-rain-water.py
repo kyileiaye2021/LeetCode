0c2e1babc6c9f8b6049aac79beb8b8f7876da22a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-27 20:16:02 -0800

    Time: 39 ms (7.24%), Space: 19.6 MB (10.65%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,81 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # happy case
-        # input: height = [1,0,1]
-        # output: 1
+        # prefix = [0,0,1,1,2,2,2,2,3,3,3,3]
+        # postfix = [3,3,3,3,3,3,3,2,2,2,1,0]
 
-        # input: height = [0, 2, 1, 0, 2]
-        # output: 3
+        prefix = [0] * len(height)
+        postfix = [0] * len(height)
+        count = 0
 
-        # input: height = [0, 1, 0, 1, 0]
-        # output: 1
+        max_prefix = 0
+        for i in range(len(height)):
+            prefix[i] = max_prefix 
+            max_prefix = max(max_prefix, height[i])
+        print(prefix)
 
-        # input: height = [1, 0, 0, 0, 1]
-        # output: 3
+        max_postfix = 0
+        for i in range(len(height) - 1, -1, -1):
+            postfix[i] = max_postfix
+            max_postfix = max(max_postfix, height[i])
+        print(postfix)
+        
+        for i in range(len(height)):
+            min_height = min(prefix[i], postfix[i])
+            if min_height - height[i] > 0:
+                count += min_height - height[i]
 
-        # edge cases
-        # input: [0,0,0,0]
-        # output; 0
-
-        # input: [0,1,0]
-        # output: 0
-
-        # input: [1]
-        # output: 0
-
-        # on the left and right side, we need to keep track of the longest bar
-        # we have l and r pointers at the end
-        # we get the min of left and right pointers
-        # do min-l_ele and min - r_ele
-        # if min-l_ele > 0: add it to the total
-        # if min-r_ele > 0: add it to the total
-        # update the min
-
-        # first thing - we have to get the max left and max right
-        # second - we have to get the min of max_left and max_right
-        # n = len(height)
-        # max_left = [0] * n
-        # max_right = [0] * n
-        # min_num = [0] * n
-
-        # curr_l = 0
-        # for i, h in enumerate(height):
-        #     max_left[i] = curr_l
-        #     curr_l = max(curr_l, height[i])
-
-        # curr_r = 0
-        # for i in range(n-1, -1, -1):
-        #     max_right[i] = curr_r
-        #     curr_r = max(curr_r, height[i])
-
-        # for i in range(n):
-        #     min_num[i] = min(max_left[i], max_right[i])
-
-        # res = 0
-        # for i, h in enumerate(height):
-        #     if (min_num[i] - h) > 0:
-        #         res += min_num[i] - h
-
-        # return res
-
-        l, r = 0, len(height) - 1
-        max_left = height[l]
-        max_right = height[r]
-
-        res = 0
-        while l < r:
-            if max_left < max_right:
-                l += 1
-                max_left = max(max_left, height[l])
-                res += (max_left - height[l])
-                
-            else:
-                r -= 1
-                max_right = max(max_right, height[r])
-                res += (max_right - height[r])
-
-        return res
-
-
-
-
+        return count
