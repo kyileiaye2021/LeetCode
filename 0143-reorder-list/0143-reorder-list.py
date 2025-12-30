23136a29949d9b2e617c713818620b1db1e8c623
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-29 19:49:50 -0800

    Time: 0 ms (100%), Space: 22.1 MB (89.21%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,50 +8,59 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # fast and slow pointers
-        fast = head
-        slow = head
+        # if the list is just one node return that node
+        # separate the list into two lists
+        # add the nodes alternatively from the two lists
+        # return dummy.next
+
+        # separate the list into two
+        if not head.next:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        slow = dummy 
+        fast = dummy
+
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-
-        prev = None
-        cur = slow.next
+            slow = slow.next
+        
+        l2_head = slow.next
         slow.next = None
-        while cur:
-            next_node = cur.next
-            cur.next = prev
-            prev = cur
-            cur = next_node
 
-        dummy = ListNode()
-        cur = dummy
+        # reverse the second list
+        prev = None
+        curr = l2_head
 
-        while head and prev:
-            cur.next = head
-            cur = cur.next
-            head = head.next
-
-            cur.next = prev
-            cur = cur.next
-            prev = prev.next
-
-        while head:
-            cur.next = head
-            cur = cur.next
-            head = head.next
-
-        while prev:
-            cur.next = prev
-            cur = cur.next
-            prev = prev.next
-
-    
-
-
-
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
 
         
 
+        # recombining the two lists
+        l1 = head
+        l2 = prev
 
-        
+        res = ListNode(0)
+        while l1 and l2:
+            res.next = l1
+            res = res.next
+            l1 = l1.next
+
+            res.next = l2
+            res = res.next
+            l2 = l2.next
+
+        if l1:
+            res.next = l1
+
+        if l2:
+            res.next = l2
+
+        return res.next
+
+
