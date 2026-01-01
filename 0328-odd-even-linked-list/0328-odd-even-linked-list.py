11afe425c09a0c53a19c62da53229688ed90a1c4
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2025-12-31 18:48:24 -0800

    Time: 0 ms (100%), Space: 18.9 MB (83.82%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,72 +5,30 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Happy cases
-        # input: head = [1,2,3,5,8]
-        # output: [1,3,8,2,5]
-
-        # input: head = [3,5,4,7]
-        # output: [3,4,5,7]
-
-        # edge cases
-        # input: head = []
-        # output: []
-
-        # input: head = [3]
-        # output: [3]
-
-        # input: head = [5,8]
-        # output: [5,8]
-
-        # Brute force - 2 pass solution
-        # Two pointers 
-        # if there is no ele in the list or only one ele in the list:
-        #   return head
-        # curr pointer to head
-        # i = 1
-        # 2 dummy nodes
-        # odd dummy node
-        # even dummy node
-        # odd_dummy.next = head
-        # even_dummy.next = head.next
-        # prev_odd = odd_dummy
-        # prev_even = even_dummy
-        # traverse the list until curr is none 
-        #   check if i is odd:
-        #       set the next of prev_odd to curr
-        #       set prev_odd to curr
-    
-        #   else:
-        #       set the next of prev_even to curr
-        #       set prev_even to curr
-        #   set curr to curr.next
-        #   increment i by 1
-        # set the prev_odd.next to even_dummy.next
-        # return odd_dummy.next
-
-        if not head or not head.next:
+        if not head:
             return head
+            
+        sec_list = ListNode(0)
+        temp = sec_list
+
+        first = head
+        second = first.next
+
+        while second and second.next:
+            temp.next = second
+            temp = temp.next
+
+            first.next = second.next
+            second = second.next.next
+            first = first.next
+
+        temp.next = second
+        if second:
+            first.next = None
 
         curr = head
-        i = 1
-        odd_dummy = ListNode(0,head)
-        even_dummy = ListNode(0, head.next)
-        prev_odd = odd_dummy
-        prev_even = even_dummy
-
-        while curr:
-            if i % 2 != 0:
-                prev_odd.next = curr
-                prev_odd = curr
-            
-            else:
-                prev_even.next = curr
-                prev_even = curr
-            
+        while curr.next:
             curr = curr.next
-            i += 1
-        prev_even.next = None
-        prev_odd.next = even_dummy.next
-        return odd_dummy.next
 
-
+        curr.next = sec_list.next
+        return head
