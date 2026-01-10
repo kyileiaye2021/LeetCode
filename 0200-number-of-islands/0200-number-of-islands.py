4541340a9bfe035f19547b332daf5f3874f248ec
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-09 21:32:25 -0800

    Time: 304 ms (10.11%), Space: 26.9 MB (14.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -10,6 +10,21 @@ class Solution:
                 self.dfs(grid, new_r, new_c, visited, dirs)
 
 
+    def bfs(self, grid, r, c, visited, dirs):
+        visited.add((r, c))
+        queue = deque()
+        queue.append((r, c))
+
+        while queue:
+            r, c = queue.pop()
+            for dx, dy in dirs:
+                new_r = r + dx
+                new_c = c + dy
+
+                if new_r in range(len(grid)) and new_c in range(len(grid[0])) and grid[new_r][new_c] == '1' and (new_r, new_c) not in visited:
+                    queue.append((new_r, new_c))
+                    visited.add((new_r, new_c))
+
     def numIslands(self, grid: List[List[str]]) -> int:
         # dfs
         # mark the curr cell as visited
@@ -24,6 +39,27 @@ class Solution:
         #           increment group count
         # return group count
 
+        # group = 0
+        # rows = len(grid)
+        # cols = len(grid[0])
+        # visited = set()
+        # dirs = [[0,1], [1, 0], [-1, 0], [0, -1]]
+
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if (r, c) not in visited and grid[r][c] == '1':
+        #             self.dfs(grid, r, c, visited, dirs)
+        #             group += 1
+
+        # return group
+
+        # time = O(m * n)
+        # space = O(m * n)
+
+
+        # -----------------------------------------------------
+
+        # BFS
         group = 0
         rows = len(grid)
         cols = len(grid[0])
@@ -33,11 +69,7 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if (r, c) not in visited and grid[r][c] == '1':
-                    self.dfs(grid, r, c, visited, dirs)
+                    self.bfs(grid, r, c, visited, dirs)
                     group += 1
-
         return group
-
-        # time = O(m * n)
-        # space = O(m * n)
         
