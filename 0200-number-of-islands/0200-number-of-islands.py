b22463b375b943c8f10fa2271a95d681d40a87c6
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-09 21:12:18 -0800

    Time: 299 ms (11.05%), Space: 26.6 MB (14.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,20 +1,43 @@
 class Solution:
+    def dfs(self, grid, r, c, visited, dirs):
+        visited.add((r, c))
+
+        for dx, dy in dirs:
+            new_r = r + dx
+            new_c = c + dy
+
+            if new_r in range(len(grid)) and new_c in range(len(grid[0])) and grid[new_r][new_c] == '1' and (new_r, new_c) not in visited:
+                self.dfs(grid, new_r, new_c, visited, dirs)
+
+
     def numIslands(self, grid: List[List[str]]) -> int:
-        # [[1,1,0,1]]
-        m, n = len(grid), len(grid[0])
-        visited = [[False for j in range(n)] for i in range(m)]
-        islands = 0
-        def dfs(i, j):
-            if i < 0 or i >= m or j < 0 or j >= n: return
-            if grid[i][j] == '1' and not visited[i][j]:
-                visited[i][j] = True
-                dfs(i - 1, j)
-                dfs(i + 1, j)
-                dfs(i, j - 1)
-                dfs(i, j + 1)
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1' and not visited[i][j]:
-                    islands += 1
-                    dfs(i, j)
-        return islands
+        # dfs
+        # mark the curr cell as visited
+        # go to 4 directions from the curr cell
+        #   if it's not visited and 1
+        #       call dfs on that cell
+
+        # iterate thru the rows 
+        #   iterate thru the cols
+        #       if the curr cell is 1 and not visited
+        #           call dfs 
+        #           increment group count
+        # return group count
+
+        group = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        visited = set()
+        dirs = [[0,1], [1, 0], [-1, 0], [0, -1]]
+
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) not in visited and grid[r][c] == '1':
+                    self.dfs(grid, r, c, visited, dirs)
+                    group += 1
+
+        return group
+
+        # time = O(m * n)
+        # space = O(m * n)
+        
