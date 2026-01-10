480f6b8c18b040ab6e7ec106837cedb49c7307c4
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-09 22:51:06 -0800

    Time: 5 ms (54.18%), Space: 20.6 MB (5.26%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,54 +1,43 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # create a graph for numCOurses
+        processed_courses = 0
+        preq_graph = defaultdict(list)
+        in_deg_graph = defaultdict(list)
 
-        # topological sort
-        # indegree array [0] * n
+        for next_course, preq in prerequisites:
+            preq_graph[preq].append(next_course)
+            in_deg_graph[next_course].append(preq)
+        
+        # keep track of in-degrees for each course
+        in_deg = [0] * numCourses
+        for course in range(numCourses):
+            for _ in in_deg_graph[course]:
+                in_deg[course] += 1
 
-        # {course : next courses}
-        # iterate (next, prereq) thru the pairs in prereq
-        #   indegree[next] += 1
-
-        # bfs
-        # queue
-        # add the courses with no prereq to the queue
-
-        # iterate thru the 0 - n - 1 courses
-        #   check if the curr course is not in the next course values
-        #       add them to the queue
-        #       numCourse -= 1
-
-        # until queue is empty
-        #   pop out those courses
-        #   go to next courses
-        #       decrement the indegree
-        #       check if indegree becomes 0
-        #           add the next course in queue
-        #           numCourses -= 1
-
-        # return true if numCourses == 0 else return false
-
-        course_map = defaultdict(list)
-        indegree = [0] * numCourses
-        for next_course, prereq in prerequisites:
-            course_map[prereq].append(next_course)
-            indegree[next_course] += 1
-
+        # queue to add all courses with no in-deg
+        # if queue is empty -> return false
         queue = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
-                numCourses -= 1
+        for course in range(numCourses):
+            if in_deg[course] == 0:
+                queue.append(course)
+                processed_courses += 1
 
+        if not queue:
+            return False
+
+        # whiile queue is not empty
+        #   pop out the queue
+        #   itreate thru the next coures of that course
+        #       decrement the indeg of the next courese
+        #       if in-deg of next course becomes 0
+        #           add it to the queue
         while queue:
             curr_course = queue.popleft()
-            for next_course in course_map[curr_course]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
+            for next_course in preq_graph[curr_course]:
+                in_deg[next_course] -= 1
+                if in_deg[next_course] == 0:
                     queue.append(next_course)
-                    numCourses -= 1
+                    processed_courses += 1
 
-        return True if numCourses == 0 else False
-        
-
-
-
+        return True if processed_courses == numCourses else False
