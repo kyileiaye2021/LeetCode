2f8ec3330d77be0802f29cb00fbc2b5cbd3c1ec6
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-13 09:39:36 -0800

    Time: 5 ms (34.76%), Space: 20.5 MB (8.75%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,29 +1,39 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        course_graph = defaultdict(list)
+        # topological sort
+        # indeg arr - a list of num of indeg or prereq for each course
+        # res = []
+
+        # graph that maps to the prereq course to next coures
+        # queue
+        # add all courses with no indeg to the queue
+        # until the queue is empty
+        #   pop the queue
+        #   add the course to the res
+        #   iterate thru the next course
+        #       decrement their indeg
+        #       if their indeg is 0, add the course to the queue
+        # return res if len of res == numcourses
+
         indeg = [0] * numCourses
-
-        for (course, prereq) in prerequisites:
-            course_graph[prereq].append(course)
-            indeg[course] += 1
-
+        res = []
+        course_graph = defaultdict(list)
         queue = deque()
-        course_count = 0
-        res_list = []
-        for i in range(numCourses):
-            if indeg[i] == 0:
-                queue.append(i) 
+
+        for next_course, prereq in prerequisites:
+            course_graph[prereq].append(next_course)
+            indeg[next_course] += 1
+
+        for course, preq_count in enumerate(indeg):
+            if preq_count == 0:
+                queue.append(course)
 
         while queue:
             curr_course = queue.popleft()
-            res_list.append(curr_course)
-            course_count += 1
-
+            res.append(curr_course)
             for next_course in course_graph[curr_course]:
                 indeg[next_course] -= 1
-
                 if indeg[next_course] == 0:
                     queue.append(next_course)
 
-        return [] if course_count != numCourses else res_list
-
+        return res if len(res) == numCourses else []
