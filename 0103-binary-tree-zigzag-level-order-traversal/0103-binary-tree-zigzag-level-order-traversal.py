d8d507f7c9f0087ee325093e8055add5cff62d6a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-15 10:58:27 -0800

    Time: 0 ms (100%), Space: 19.4 MB (14.16%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -6,59 +6,33 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-
-        # bfs
-
-        # queue 
-        # add the root node to the queue
-        # left = False
-        # right = False
-        # res = empty
-
-        # while queue is not empty
-        #   curr list
-        #   for the len of queue
-            #   if left is false
-            #       popleft the root node
-            #       set left to true
-            #   else
-            #       pop the node and add the vals to the curr list
-            #       set left to false
-        #   add curr list to the res
-        #   if there is left child, add it to the queue
-        #   if there is right child, add it to queue
-
-        # return res
-
+        if not root:
+            return []
+            
         queue = deque()
         queue.append(root)
-        left = False
+        alternate = False
         res = []
 
-        if not root:
-            return res
-
         while queue:
-            curr_lst = []
             queue_len = len(queue)
-
+            curr_res = []
             for _ in range(queue_len):
                 curr_node = queue.popleft()
-                curr_lst.append(curr_node.val)
+                curr_res.append(curr_node.val)
 
                 if curr_node.left:
                     queue.append(curr_node.left)
                 if curr_node.right:
                     queue.append(curr_node.right)
-
-            if not left:
-                res.append(curr_lst)
-                left = True
+            
+            if not alternate:
+                res.append(curr_res)
+                alternate = True
             else:
-                curr_lst = curr_lst[::-1]
-                res.append(curr_lst)
-                left = False
-        
-        return res
+                res.append(curr_res[::-1])
+                alternate = False
 
+                
+        return res
         
