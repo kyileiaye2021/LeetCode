07a751a7df2c58d986d1b8c158b3a1cdbc5e6005
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-15 10:27:06 -0800

    Time: 4 ms (6.77%), Space: 20.1 MB (9.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,41 +6,29 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # bfs
-        # queue
-        # add the root to the queue
-        # res list
-        # until queue is empty
-        #   pop out the queue
-        #   append it to the res
-        #   go to its children
-        #       if they are not none, add them to the queue
 
-        # return res
-
+        if not root:
+            return []
+            
         queue = deque()
         queue.append(root)
         res = []
 
-        if not root:
-            return res
-
         while queue:
+            curr_len = len(queue)
+            curr_res = []
 
-            length = len(queue)
-            cur_res = []
-
-            for i in range(length):
-
-                cur_node = queue.popleft()
-                cur_res.append(cur_node.val)
-
-            res.append(cur_res)
-
-            if cur_node.left:
-                queue.append(cur_node.left)
-
-            if cur_node.right:
-                queue.append(cur_node.right)
-
+            for _ in range(curr_len):
+                curr_node = queue.popleft()
+                curr_res.append(curr_node.val)
+                
+                if curr_node.left:
+                    queue.append(curr_node.left)
+                if curr_node.right:
+                    queue.append(curr_node.right)
+                
+            res.append(curr_res)
+        
         return res
+            
+        
