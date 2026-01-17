0f158a3542046512cda938485b2db3501a8dac72
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-17 09:14:45 -0800

    Time: 0 ms (100%), Space: 19.5 MB (6.68%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,47 +6,18 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        #BFS
-        # if not root:
-        #     return []
-            
-        # queue = deque()
-        # queue.append(root)
-        # res = []
-
-        # while queue:
-        #     curr_len = len(queue)
-        #     last_node = queue[-1]
-        #     res.append(last_node.val)
-
-        #     for _ in range(curr_len):
-        #         curr_node = queue.popleft()
-                
-        #         if curr_node.left:
-        #             queue.append(curr_node.left)
-        #         if curr_node.right:
-        #             queue.append(curr_node.right)
-                
-        # return res
-
-        #DFS
-        # base case: if node is null
-        # return 
-        # if the first node at that level save the node val. in the res list
-        # go to right subtree first then left
         res = []
-        def dfs(node, depth):
-
-            if not node:
-                return
+        def dfs(root, depth):
+            if not root:
+                return 
 
             if depth == len(res):
-                res.append(node.val)
+                res.append(root.val)
 
-            dfs(node.right, depth + 1)
-            dfs(node.left, depth + 1)
+            dfs(root.right, depth + 1)
+            dfs(root.left, depth + 1)
 
-        dfs(root, 0) 
+        dfs(root, 0)
         return res
-            
+
         
