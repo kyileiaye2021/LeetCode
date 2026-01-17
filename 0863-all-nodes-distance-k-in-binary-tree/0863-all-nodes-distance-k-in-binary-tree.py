2dd981a67948a8dd8484e7fa12745cb3df4c1094
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-17 10:11:41 -0800

    Time: 42 ms (58.64%), Space: 19.7 MB (12.31%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,68 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        # first we have to create a map that maps to children to their parent
+        # queue to iterate all nodes and add their parent-child relationship
+        # visited
+        # queue and add the target node
+        # while k > 0 and queuE isn't empty
+        #   iterate thru the queue len
+        #       check if there is left child of the curr node
+        #           if it's not in the visited
+        #               add it to the queue
+        #       if there is right child
+        #           if it's not in the visited
+        #               add it to the queue
+        #       if there is parent
+        #           if it's not in the visited
+        #               add it to the queue
+        # return list(queue)
+
+
+        queue = deque() #{child node val: parent node}
+        queue.append(root)
+        parent = defaultdict()
+
+        while queue:
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                
+                if curr.left:
+                    parent[curr.left.val] = curr
+                    queue.append(curr.left)
+
+                if curr.right:
+                    parent[curr.right.val] = curr
+                    queue.append(curr.right)
+                
+        
+        queue = deque() 
+        queue.append(target)
+        visited = set()
+
+        while k > 0 and queue:
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                visited.add(curr.val)
+
+                if curr.left and curr.left.val not in visited:
+                    queue.append(curr.left)
+                
+                if curr.right and curr.right.val not in visited:
+                    queue.append(curr.right)
+                
+                if curr.val in parent and parent[curr.val].val not in visited:
+                    queue.append(parent[curr.val])
+
+            k -= 1
+
+        res = []
+        while queue:
+            res.append(queue.popleft().val)
+        return res
