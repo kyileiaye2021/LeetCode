65bef969af50435ff0bd5cb6eead0faecf02d909
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-18 11:23:44 -0800

    Time: 48 ms (23.8%), Space: 20.2 MB (6.92%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -7,62 +7,99 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        # first we have to create a map that maps to children to their parent
-        # queue to iterate all nodes and add their parent-child relationship
-        # visited
-        # queue and add the target node
-        # while k > 0 and queuE isn't empty
-        #   iterate thru the queue len
-        #       check if there is left child of the curr node
-        #           if it's not in the visited
-        #               add it to the queue
-        #       if there is right child
-        #           if it's not in the visited
-        #               add it to the queue
-        #       if there is parent
-        #           if it's not in the visited
-        #               add it to the queue
-        # return list(queue)
+        # DFS
+        # create parent graph with dfs
+        # 
+        if not root:
+            return []
+        parent = {}
+        def create_graph(root, parent_node=None):
+            if not root:
+                return
+
+            parent[root] = parent_node
+            create_graph(root.left, root)
+            create_graph(root.right, root)
+        
+        create_graph(root)
+
+        # print(parent)
+        visited = set()
+        res = []
+
+        def dfs(root, k):
+            # base case
+            if root in visited or not root:
+                return
+            
+            visited.add(root)
+            if k == 0:
+                res.append(root.val)
+
+            # recursive case
+            dfs(root.left, k - 1)
+            dfs(root.right, k - 1)
+            dfs(parent[root], k - 1)
+
+        dfs(target, k)
+        return res
+
+        # # first we have to create a map that maps to children to their parent
+        # # queue to iterate all nodes and add their parent-child relationship
+        # # visited
+        # # queue and add the target node
+        # # while k > 0 and queuE isn't empty
+        # #   iterate thru the queue len
+        # #       check if there is left child of the curr node
+        # #           if it's not in the visited
+        # #               add it to the queue
+        # #       if there is right child
+        # #           if it's not in the visited
+        # #               add it to the queue
+        # #       if there is parent
+        # #           if it's not in the visited
+        # #               add it to the queue
+        # # return list(queue)
 
 
-        queue = deque() #{child node val: parent node}
-        queue.append(root)
-        parent = defaultdict()
+        # queue = deque() #{child node val: parent node}
+        # queue.append(root)
+        # parent = defaultdict()
 
-        while queue:
-            for _ in range(len(queue)):
-                curr = queue.popleft()
+        # while queue:
+        #     for _ in range(len(queue)):
+        #         curr = queue.popleft()
                 
-                if curr.left:
-                    parent[curr.left] = curr
-                    queue.append(curr.left)
+        #         if curr.left:
+        #             parent[curr.left] = curr
+        #             queue.append(curr.left)
 
-                if curr.right:
-                    parent[curr.right] = curr
-                    queue.append(curr.right)
+        #         if curr.right:
+        #             parent[curr.right] = curr
+        #             queue.append(curr.right)
                 
         
-        queue = deque() 
-        queue.append(target)
-        visited = set()
+        # queue = deque() 
+        # queue.append(target)
+        # visited = set()
 
-        while k > 0 and queue:
-            for _ in range(len(queue)):
-                curr = queue.popleft()
-                visited.add(curr)
+        # while k > 0 and queue:
+        #     for _ in range(len(queue)):
+        #         curr = queue.popleft()
+        #         visited.add(curr)
 
-                if curr.left and curr.left not in visited:
-                    queue.append(curr.left)
+        #         if curr.left and curr.left not in visited:
+        #             queue.append(curr.left)
                 
-                if curr.right and curr.right not in visited:
-                    queue.append(curr.right)
+        #         if curr.right and curr.right not in visited:
+        #             queue.append(curr.right)
                 
-                if curr in parent and parent[curr] not in visited:
-                    queue.append(parent[curr])
+        #         if curr in parent and parent[curr] not in visited:
+        #             queue.append(parent[curr])
 
-            k -= 1
+        #     k -= 1
 
-        res = []
-        while queue:
-            res.append(queue.popleft().val)
-        return res
+        # res = []
+        # while queue:
+        #     res.append(queue.popleft().val)
+        # return res
