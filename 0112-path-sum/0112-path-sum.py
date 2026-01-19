f507c6db61899c13718e0c60b04ec3b12ddda266
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-19 10:29:45 -0800

    Time: 0 ms (100%), Space: 20.2 MB (20.2%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -5,46 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root, targetSum):
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
 
-        targetSum = targetSum - root.val
-
-        if not root.left and not root.right and targetSum == 0: # at the leaf node the sum val should be 0
-            return True
-
-        left_subtree = self.dfs(root.left, targetSum)
-        right_subtree = self.dfs(root.right, targetSum)
-
-        return left_subtree or right_subtree
-
-
-    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # input: 1
-        #       / \
-        #      3   5
-        #     /
-        #    5
-        # target = 4
-        # output: false
-
-        # input: 1
-        # target = 1
-        # output: true
-
-        # input: 1
-        # target = 5
-        # output: false
-
         # dfs
-        # preorder traversal
-        # base case
-        # if curr node is none --> return 
-        # check if the curr node is leaf
-        #   check if the sum is equal to the target val
-        #       return True
-        # update sum (curr val + call left subtree )
-        # update sum (curr val + call right subtree )
-        # 
-        return self.dfs(root, targetSum)
+        # the node becomes a leaf and target == sum
+        #   return true
+        # the node becomes none or the node sum value > targetSu
+        #   return false
+        # go to left subtree
+        # go to right subtree
+        # return left or right
+
+        def dfs(root, cur_sum):
+            if not root:
+                return False
+            
+            cur_sum += root.val 
+
+            if not root.left and not root.right and cur_sum == targetSum:
+                return True
+
+            left = dfs(root.left, cur_sum)
+            right = dfs(root.right, cur_sum)
+            return left or right 
+
+        return dfs(root, 0)
+        
+        
