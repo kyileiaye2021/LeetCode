cd45dca24131af5e5433441ceee5788fa073e9af
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-24 19:38:53 -0800

    Time: 3 ms (91.17%), Space: 23.2 MB (20.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,24 +1,45 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        # hashmap
 
-        freq_map = {}
+        # happy cases
+        # nums = [1,1,1,2,2,3], k = 2
+        # output: [1,2]
 
-        for n in nums:
-            freq_map[n] = freq_map.get(n, 0) + 1
+        # nums = [1,1,3,-2,-5], k = 1
+        # output: 1
 
-        #. bucket sort
-        bucket = [[] for _ in range(len(nums) + 1)]
+        # nums = [3,4,-1,3], k = 2
+        # output: [3,4,-1]
 
-        for ele in freq_map:
-            bucket[freq_map[ele]].append(ele)
+        # edge case
+        # nums = [1,2,3,4], k = 3
+        # output: [1,2,3]
 
+        # nums = [2,2,1,1], k = 1
+        # output: 1 or 2
+
+        # nums = [2,2,1,1], k = 2
+        # output: [1,2]
+
+        # max heap (freq count, ele)
+        # [(3,1), (2,2), (1,3)]
+        # for k times
+        # pop out the max heap and store the ele to the res
+        # return res
+
+        max_heap = []
         res = []
-        for i in range(len(bucket) - 1, 0, -1):
-            for ele in bucket[i]:
-                res.append(ele)
-                k -= 1
+        ele_count = Counter(nums) #{1: 3, 2:2, 3: 1}
 
-            if k == 0:
-                return res
+        max_heap = [(-count, ele) for ele, count in ele_count.items()]
+        heapq.heapify(max_heap)
+
+        for _ in range(k):
+            freq, ele = heapq.heappop(max_heap)
+            res.append(ele)
+
+        return res
+
+
+
+
