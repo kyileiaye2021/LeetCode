67376e96108c7e469056d67391d562532914806b
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-01-29 10:23:34 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -18,20 +18,41 @@ class Solution:
         # [ele]
 
         # min heap for dist
-        dist_heap = []
-        for ele in arr:
-            dist = abs(ele - x)
-            heapq.heappush(dist_heap, (dist, ele))
+        # dist_heap = []
+        # for ele in arr:
+        #     dist = abs(ele - x)
+        #     heapq.heappush(dist_heap, (dist, ele))
 
-        # min heap for first k ele
-        closet_heap = []
-        for _ in range(k):
-            dist, ele = heapq.heappop(dist_heap)
-            heapq.heappush(closet_heap, ele)
+        # # min heap for first k ele
+        # closet_heap = []
+        # for _ in range(k):
+        #     dist, ele = heapq.heappop(dist_heap)
+        #     heapq.heappush(closet_heap, ele)
 
-        res = []
-        while closet_heap:
-            res.append(heapq.heappop(closet_heap))
+        # res = []
+        # while closet_heap:
+        #     res.append(heapq.heappop(closet_heap))
         
-        return res
+        # return res
+
+        l = 0
+        r = len(arr) - k
+        
+        while l < r:
+            mid = (l + r) // 2
+
+            diff1 = abs(arr[mid] - x)
+            diff2 = abs(arr[mid + k] - x)
+
+            # if r+1 ele is closer to x than the l ele
+            # move the window to right
+            if diff1 > diff2:
+                l = mid + 1
+
+            # if l ele is closer to x than r+1 ele (or) if the diff is the same, we have to take the smaller ele
+            else:
+                r = mid
+            
+        
+        return arr[l : l + k]
 
