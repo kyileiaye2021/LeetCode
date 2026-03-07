8867796cf3e0a27672c85266324ca4d540b1dd7f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-03-06 23:56:04 -0800

    Time: 301 ms (46.61%), Space: 19.2 MB (90.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,60 +1,58 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # s = babd
+        # output: bab
 
-        # the word is not fully panlindrome
-        # we have to find the longest substr that is panlindrome 
+        # s = abcd
+        # output: ''
 
-        # iterate thru each char in the word
+        # s = "cbbd"
+        # output: bb
 
-        #   # for odd len 
-        #   start from curr i (l, r pointers)
-        #   while l and r are the same
-        #       move l by 1 to left
-        #       move r by 1 to right
-        #   create the curr substr
-        #   check if the substr len is > the longest
-        #       update the longest substr with curr substr
-        #       update the longest len with curr len
-        
-        #   for even len
-        #   start l at curr
-        #   start r at i + 1
-        #   while l and r are the same
-        #       move l by 1 to left
-        #       move r by 1 to right
-        #   create the curr substr
-        #   check if the substr len is > the longest
-        #       update the longest substr with curr substr
-        #       update the longest len with curr len
-        longest = 0
-        longest_substr = ""
+        # s = "cbbc"
+        # output: "cbbc"
+
+        # s = ""
+        # output: ""
+
+        # iterate thru the chars
+        # l and r at the curr char
+        # max window = 0
+        # max sub string = ""
+        # until l and r ele are the same
+        #   l -= 1
+        #   r += 1
+        # curr window size 
+        # update the max window and keep track of curr substr if curr window > max window
+        # return max sub str
+        max_window = 0
+        max_substr = ""
 
         for i in range(len(s)):
-            # for odd len
-            l = i 
-            r = i
+            l = r = i
+
             while l >= 0 and r < len(s) and s[l] == s[r]:
+                curr_window = r - l + 1
+                if curr_window > max_window:
+                    max_window = curr_window
+                    max_substr = s[l : r + 1]
+
                 l -= 1
                 r += 1
 
-            curr_substr = s[l + 1: r]
-            if len(curr_substr) > longest:
-                longest_substr = curr_substr
-                longest = len(curr_substr)
-
-
-            # for even len
+        # for i in range(len(s) - 1):
             l = i
             r = i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
+                curr_window = r - l + 1
+                if curr_window > max_window:
+                    max_window = curr_window
+                    max_substr = s[l : r + 1]
                 l -= 1
                 r += 1
 
-            curr_substr = s[l + 1: r]
-            if len(curr_substr) > longest:
-                longest_substr = curr_substr
-                longest = len(curr_substr)
-        return longest_substr
+        return max_substr
+
 
 
         
