06ec92f0af5b90f7fb78f8d979140bd88b9d80b7
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-03-15 19:47:39 -0700

    Time: 8 ms (33.66%), Space: 26.4 MB (86.11%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,51 +3,59 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # nums = [-1, -100, 3, 99]
-        # k = 2
-        # output: [3, 9, -1, -100]
 
-        # nums = [-1, -100, 3, 99]
-        # k = 15
-        # [99, -1, 100, 3]
-        # [3, 99, -1, 100]
-        # [-100, 3, 99, -1] <--
-        # [-1, -100, 3, 99]
-        # [99, -1, 100, 3]
-        # mod k by len of nums
+        # happy cases
+        # nums = [1,2,3], k = 1
+        # outputs: [3,1,2]
 
-        # [99, 3, -100, -1]
-        # [-100, 3, 99, -1]
+        # nums = [1, 2], k = 1
+        # output: [2,1]
 
-        # k = 3
-        # swapping with 2 pointers
-        # i, j
-        # reverse the list
-        #   swap i and j ele
-        # for the 1st k portion 
-        #   reverse the list
-        # for the rest of the portion
-        #   reverse the list
+        # edge cases
+        # nums = [1], k = 1
+        # output: [1]
 
-        n = len(nums)
-        k = k % n
+        # nums = [1,2,3], k = 3
+        # output: [1,2,3]
 
-        i, j = 0, n - 1
+        # swap the ele in the list
+        # split the list into i to k and k+1 to j
+        # for the first k ele
+        #   swap the ele again to get in order
+        # for the second n - k ele
+        #   swap the ele again to get in order
+
+        if k == 0:
+            return nums
+
+        if k > len(nums):
+            k = k % len(nums)
+
+        i = 0
+        j = len(nums) - 1
+
         while i < j:
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
 
-        i, j = 0, k - 1
+        i = 0
+        j = k - 1
         while i < j:
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
 
-        i, j = k, n - 1
+
+        i = k
+        j = len(nums) - 1
         while i < j:
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
+
+
+
+
 
         
