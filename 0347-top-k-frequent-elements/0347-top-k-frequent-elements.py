f30c015c7cdaf6e0fb92f5faaf51c0349f86e950
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 11:19:56 -0700

    Time: 5 ms (59.96%), Space: 22.8 MB (85.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,33 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         # hashmap + sort + k (O(nlogn))
-        # priority queue with (freq count, ele) heap -> O(n)
+        # priority queue with (freq count, ele) heap -> O(nlogn) in the worst case scenario
+        # bucket sort size - highest freq count
+        
+        freq_map = Counter(nums) #O(n)
+        # hq = []
+        
+        # for ele, freq in freq_map.items(): #O(mlogn) # m = num of unique ele
+        #     heapq.heappush(hq, (-freq, ele))
+        
+        # res = []
+        # for i in range(k): #O(klogn)
+        #     freq, ele = heapq.heappop(hq)
+        #     res.append(ele)
 
-        freq_map = Counter(nums)
-        hq = []
+        # {1:3, 2:2, 3:1}
+        size = max(freq_map.values())
+        bucket = [[] for _ in range(size + 1)] #[[],[3],[2],[1]]
         
         for ele, freq in freq_map.items():
-            heapq.heappush(hq, (-freq, ele))
-        
+            bucket[freq].append(ele)
+
         res = []
-        for i in range(k):
-            freq, ele = heapq.heappop(hq)
-            res.append(ele)
+        for i in range(len(bucket) - 1, 0, -1):
+            for ele in bucket[i]:
+                if k == 0:
+                    break
+                res.append(ele) # [1, 2]
+                k -= 1
 
         return res
