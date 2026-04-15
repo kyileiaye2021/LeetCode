a57833e14929a2e90c392abde6d4b3273de8ae11
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 10:19:32 -0700

    Time: 3 ms (88.44%), Space: 22.9 MB (46.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,45 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        # hashmap + sort + k (O(nlogn))
+        # priority queue with (freq count, ele) heap -> O(n)
 
-        # happy cases
-        # nums = [1,1,1,2,2,3], k = 2
-        # output: [1,2]
-
-        # nums = [1,1,3,-2,-5], k = 1
-        # output: 1
-
-        # nums = [3,4,-1,3], k = 2
-        # output: [3,4,-1]
-
-        # edge case
-        # nums = [1,2,3,4], k = 3
-        # output: [1,2,3]
-
-        # nums = [2,2,1,1], k = 1
-        # output: 1 or 2
-
-        # nums = [2,2,1,1], k = 2
-        # output: [1,2]
-
-        # max heap (freq count, ele)
-        # [(3,1), (2,2), (1,3)]
-        # for k times
-        # pop out the max heap and store the ele to the res
-        # return res
-
-        max_heap = []
+        freq_map = Counter(nums)
+        hq = []
+        
+        for ele, freq in freq_map.items():
+            heapq.heappush(hq, (-freq, ele))
+        
         res = []
-        ele_count = Counter(nums) #{1: 3, 2:2, 3: 1}
-
-        max_heap = [(-count, ele) for ele, count in ele_count.items()]
-        heapq.heapify(max_heap)
-
-        for _ in range(k):
-            freq, ele = heapq.heappop(max_heap)
+        for i in range(k):
+            freq, ele = heapq.heappop(hq)
             res.append(ele)
 
         return res
-
-
-
-
