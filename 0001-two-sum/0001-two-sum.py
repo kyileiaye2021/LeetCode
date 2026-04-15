2ad2d877a3fe37b8193e7609ffb98b31fcfdfb3e
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 10:27:25 -0700

    Time: 0 ms (100%), Space: 20.6 MB (25.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,29 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Happy cases
-        # input - [2,7,11,15], target = 9
-        # output = [0,1]
+        # hashmap
+        # iterate thru the ele 
+        #   diff = subtract the cur ele from target
+        #   check if the diff in hashmap
+        #       return [cur ele idx, diff idx]
+        #   else
+        #       add the [curr ele : curr idx]
 
-        # Edge cases
-        # input - [3,2], target = 5
-        # output = [0, 1]
-
-        # dictionary approach
-        # {ele : index}
-        # iterate over the ele in the list
-        #   do a diff between the target and curr ele
-        #       check if the diff is in dict
-        #           return the curr ele index and diff ele index as a list
-        #       else: add the curr ele with its index in the dict
-        
-        idx_map = {}
-        
-        for i, ele in enumerate(nums):
-            diff = target - ele
-            if diff in idx_map:
-                return [i, idx_map[diff]]
-
+        hashmap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashmap:
+                return [i, hashmap[diff]]
             else:
-                idx_map[ele] = i
+                hashmap[n] = i
 
-        
+            
