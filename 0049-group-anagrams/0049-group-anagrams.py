5ebc05bd079e3b524aa0cb60a075476e3936ec6f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 09:53:04 -0700

    Time: 35 ms (7.44%), Space: 22.7 MB (41.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,24 +1,51 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # {(a-z): ['', '']}
+        # happy cases
+        # input: ["a", "a", "b"]
+        # output: [["a", "a"], ["b"]]
+
+        # edge cases
+        # input: [""]
+        # output: [[""]]
+
+        # nested for + hashmap O(n^2)
+        # hashmap {{'e': 1, 'a': 1, 't': 1}: [eat, ate, tea]
+        #           {'b':1, 'a':1, 't':1}: ["bat"]
+        #           {'t':1, 'a':1, 'n':1}: ["nat", "tan"]}
         
-        anagram = defaultdict(list)
-        res = []
-
+        # a-z chars : 26 chars --> 10001....
+        main_hashmap = {}
         for s in strs:
-            curr_code = [0] * 26
+            # temp list with 26 0s (0-25)
+            temp_lst = [0] * 26
 
+            # iterate thru the s
             for c in s:
-                curr_code[ord(c) - ord('a')] += 1
 
-            curr_code = tuple(curr_code)
-            anagram[curr_code].append(s)
+            #   get the index of cur char in temp list
+                idx = ord(c) - ord('a')
 
-        for group in anagram.values():
+            #   put 1 at that position
+                temp_lst[idx] += 1
+
+            # hashed_str = combine temp list into a str
+            hashed_str = ''.join(str(temp_lst))
+
+            # add the s to main_hashmap with hashed_str  {hashed_str: [s]}
+            if hashed_str not in main_hashmap:
+                main_hashmap[hashed_str] = [s]
+            else:
+                main_hashmap[hashed_str].append(s)
+
+        res = []
+        for hashed_str, group in main_hashmap.items():
             res.append(group)
 
         return res
 
 
 
-            
+
+
+
+       
