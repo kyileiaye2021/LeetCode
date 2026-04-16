360ab251735310c36bc34ce85e7d1197df3f2822
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 22:55:55 -0700

    Time: 67 ms (21.41%), Space: 28.8 MB (15.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,32 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        # i, j - 0, 1
-        # iterate thru the arr until j reaches the end
-        #   if j ele < i ele
-        #       move i to j
-        #       move j by 1
-        #   else
-        #       curr profit = j ele - i ele
-        #       update max profit if curr profit > max profit
-        # return max profit
-
         i = 0
         j = 1
         max_profit = 0
+
         while j < len(prices):
-            if prices[j] <= prices[i]:
+            if prices[i] >= prices[j]:
                 i = j
 
             else:
-                curr_profit = prices[j] - prices[i]
-                max_profit = max(max_profit, curr_profit)
-
-            j += 1
-
-        return max_profit
-
+                profit = prices[j] - prices[i]
+                max_profit = max(max_profit, profit)
                 
+            j += 1
+        
+        return max_profit
 
 
         
