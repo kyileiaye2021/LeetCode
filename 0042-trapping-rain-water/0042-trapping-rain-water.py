e262ba5116775b2f29949354cba7fa8845569994
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 18:21:31 -0700

    Time: 23 ms (7.89%), Space: 21 MB (87.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,27 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # prefix = [0,0,1,1,2,2,2,2,3,3,3,3]
-        # postfix = [3,3,3,3,3,3,3,2,2,2,1,0]
 
         prefix = [0] * len(height)
+        for i in range(1, len(height)):
+            prefix[i] = max(height[i - 1], prefix[i - 1])
+
         postfix = [0] * len(height)
-        count = 0
+        for i in range(len(height) - 2, -1, -1):
+            postfix[i] = max(height[i + 1], postfix[i + 1])
 
-        max_prefix = 0
+        res = 0
         for i in range(len(height)):
-            prefix[i] = max_prefix 
-            max_prefix = max(max_prefix, height[i])
-        print(prefix)
+            boundary = min(prefix[i], postfix[i])
+            water = boundary - height[i]
+            if water > 0:
+                res += water
 
-        max_postfix = 0
-        for i in range(len(height) - 1, -1, -1):
-            postfix[i] = max_postfix
-            max_postfix = max(max_postfix, height[i])
-        print(postfix)
+        return res
+
         
-        for i in range(len(height)):
-            min_height = min(prefix[i], postfix[i])
-            if min_height - height[i] > 0:
-                count += min_height - height[i]
-
-        return count
