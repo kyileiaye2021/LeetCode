019c5870ae000b305443859e6ac9ce2064c150cf
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-16 12:25:12 -0700

    Time: 229 ms (8.98%), Space: 19.7 MB (59.77%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,59 +1,62 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # happy case
-        # s = "ABAB", k = 2
+
+        # "ABAB", k = 2
         # 4
 
-        # s = "BFRGG", k = 2
-        # output: 4
+        # "ABABCDAB", k = 2
+        # 6
 
-        # s = "AAAAA", k = 1
-        # output: 5
+        # "E", k = 0
+        # 1
 
-        # s = "khgtno", k = 3
-        # output: 4
+        # "ADBBC", k = 0
+        # 3
 
-        # s = "t", k = 1
-        # output: 1
-
-        # s = "sty", k = 0
-        # output: 1
-
-        # s = "sssty", k = 0
-        # output: 3
-
-        # sliding window
-        # l = 0, r = 0
-        # hashmap to keep track of the freq of chars in the window
-        # iterate thru the s until r reaches the end
-        #   add the curr ele to the hashmap
-        #   check if window size - the most freq ele num == k:
-        #       update the window size
-        #   while window size - the most freq ele num >= k:
-        #       shrink the window by decrementing l pointer ele freq num
-        #       increment l pointer
-        # increment r
-        # return max window size
+        # sliding window + two pointer
+        # expand from the left
+        #   if the duplicate is found
+        #       check if k > 0
+        #           decrement k
+        #           expand more
+        #       shrink the window size
+        #       remove curr l ele from the set
+        #   add the r ele to the set
+        #   keep track of the window size
+        #   update the longest window size
 
         l = 0
         r = 0
-        map = {}
-        max_window = 0
+        longest = 0
+        cur_len = 0
+        hashmap = {}
+
         while r < len(s):
-            map[s[r]] = map.get(s[r], 0) + 1
-            most_freq_ele_count = max(map.values())
-            curr_window = r - l + 1
+            print(s[r])
+    
+            hashmap[s[r]] = hashmap.get(s[r], 0) + 1
 
-            if curr_window - most_freq_ele_count <= k:
-                max_window = max(curr_window, max_window)
+            window_size = r - l + 1
+            most_freq = max(hashmap.values())
+            while window_size - most_freq > k:
+                    hashmap[s[l]] -= 1
+                    if hashmap[s[l]] == 0:
+                        del hashmap[s[l]]
+                    l += 1
+                    window_size = r - l + 1
+                    most_freq = max(hashmap.values())
 
-            while curr_window - most_freq_ele_count > k:
-                map[s[l]] -= 1
-                l += 1
-                curr_window = r - l + 1
-                most_freq_ele_count = max(map.values())
-
+            curr_len = r - l + 1
+            longest = max(longest, curr_len)
             r += 1
-        
-        return max_window
 
+        return longest
+
+
+
+
+
+
+
+
+        
