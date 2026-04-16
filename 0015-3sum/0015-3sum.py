2ed5a109e0947751c4ac4161b503802decd08314
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 17:23:15 -0700

    Time: 595 ms (73.72%), Space: 22.1 MB (84.17%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,56 +1,31 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        # nums = [0,1,1]
-        # output: []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        # 3 triplets -> distinct
+        # nested loops -> O(n^3)
 
-        # nums = [0,0,0]
-        # output: [0,0,0]
-
-        # nums = [1,-1,0]
-        # output: [1, -1, 0]
-
-        # nums = [1, -1, 0, -1]
-        # output: [1, -1, 0]
-
-        # nums = [1, -1, 0, -1, 1, 0]
-        # output: [[1, -1, 0], [-1, 1, 0]]
-
-        # nums =[-1, 0, 1, 2, -1, -4]
-        # output: [[-1,-1,2], [-1, 0, 1]]
-
-        # [-4, -1, -1, -1,  0, 2, 2]
-
+        nums.sort()
+        # iterate thru the ele 
         res = []
-        # sort the arr
-        nums = sorted(nums)
-
-        # iterate thru the arr
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
-            curr = nums[i]
-        
+            cur_ele = nums[i]
 
             l = i + 1
             r = len(nums) - 1
 
             while l < r:
-                if nums[l] + nums[r] + curr < 0:
-                    l += 1
-
-                elif nums[l] + nums[r] + curr > 0:
+                if cur_ele + nums[l] + nums[r] > 0:
                     r -= 1
+
+                elif cur_ele + nums[l] + nums[r] < 0:
+                    l += 1
 
                 else:
-                    res.append([curr, nums[l], nums[r]])
+                    res.append([cur_ele, nums[l], nums[r]])
                     l += 1
-                    r -= 1
                     while l < len(nums) and nums[l] == nums[l - 1]:
                         l += 1
 
         return res
-        # l and r pointer for the other ele in the arr to the right of curr
 
-        # apply binary search on the remaining arr
