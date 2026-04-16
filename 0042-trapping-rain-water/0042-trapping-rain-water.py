5eb53ae4518eced144aca9014d811481f810fa19
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 18:38:17 -0700

    Time: 11 ms (46.23%), Space: 21.1 MB (30.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,21 +1,39 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
 
-        prefix = [0] * len(height)
+        # prefix = [0] * len(height)
+        # for i in range(1, len(height)):
+        #     prefix[i] = max(height[i - 1], prefix[i - 1])
+
+        # postfix = [0] * len(height)
+        # for i in range(len(height) - 2, -1, -1):
+        #     postfix[i] = max(height[i + 1], postfix[i + 1])
+
+        # res = 0
+        # for i in range(len(height)):
+        #     boundary = min(prefix[i], postfix[i])
+        #     water = boundary - height[i]
+        #     if water > 0:
+        #         res += water
+
+        # return res
+
+        res = [0] * len(height)
         for i in range(1, len(height)):
-            prefix[i] = max(height[i - 1], prefix[i - 1])
+            res[i] = max(height[i - 1], res[i - 1])
 
-        postfix = [0] * len(height)
-        for i in range(len(height) - 2, -1, -1):
-            postfix[i] = max(height[i + 1], postfix[i + 1])
-
-        res = 0
-        for i in range(len(height)):
-            boundary = min(prefix[i], postfix[i])
-            water = boundary - height[i]
+        postfix = 0
+        for i in range(len(height) - 1, -1, -1):
+            boundary = min(res[i], postfix)
+            water =  boundary - height[i]
             if water > 0:
-                res += water
+                res[i] = water
+            else:
+                res[i] = 0
+            
+            postfix = max(height[i], postfix)
 
-        return res
+        return sum(res)
+
 
         
