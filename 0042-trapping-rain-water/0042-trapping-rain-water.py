fc290c4bad24f6602302eb783f2ac157a29795be
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-15 19:16:43 -0700

    Time: 11 ms (46.23%), Space: 21.1 MB (56.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -18,22 +18,43 @@ class Solution:
 
         # return res
 
-        res = [0] * len(height)
-        for i in range(1, len(height)):
-            res[i] = max(height[i - 1], res[i - 1])
+        # res = [0] * len(height)
+        # for i in range(1, len(height)):
+        #     res[i] = max(height[i - 1], res[i - 1])
 
-        postfix = 0
-        for i in range(len(height) - 1, -1, -1):
-            boundary = min(res[i], postfix)
-            water =  boundary - height[i]
-            if water > 0:
-                res[i] = water
-            else:
-                res[i] = 0
+        # postfix = 0
+        # for i in range(len(height) - 1, -1, -1):
+        #     boundary = min(res[i], postfix)
+        #     water =  boundary - height[i]
+        #     if water > 0:
+        #         res[i] = water
+        #     else:
+        #         res[i] = 0
             
-            postfix = max(height[i], postfix)
+        #     postfix = max(height[i], postfix)
 
-        return sum(res)
+        # return sum(res)
 
+        maxL = height[0]
+        maxR = height[-1]
+        res = 0
+
+        l = 0
+        r = len(height) - 1
+
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                water = max(0, maxL - height[l])
+                maxL = max(maxL, height[l])
+
+            else:
+                r -= 1
+                water = max(0, maxR - height[r])
+                maxR = max(maxR, height[r])
+
+            res += water
+
+        return res
 
         
