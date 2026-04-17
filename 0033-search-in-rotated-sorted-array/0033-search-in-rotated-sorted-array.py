b3fdd26e1476352df0c4e42836a3f8117b2ea2d5
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-17 02:50:26 -0700

    Time: 0 ms (100%), Space: 19.4 MB (82.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,69 +1,62 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # happy case
-        # input: [4,5,6,7,0,1,2], target = 0
-        # output: 4
-
-        # input: [4,5,6,7,0,1,2], target = 4
-        # output: 0
-
-        # input: [4,5,6,7,0,1,2], target = 2
-        # output: 6
-
-        # edge case
-        # input: [4,5,6,7,0,1,2], target = 11
-        # output: -1
-
-        # input: [3], target = 3
-        # output: 0
-
-        # input: [4,5], target = 9
-        # output: -1
-
-        # O(n)
-        # binary search
-        # while l < r
-        # find the mid index value
-        #   if mid val is target, return mid index
-        # if mid val is > target
-        #   if the l val is > target
-        #       move mid to the right
+        # if mid val >= l ele
+        #   left side
+        #   if target > mid
+        #       l = mid + 1
+        #   elif target < mid
+        #       if target < l ele
+        #           l = mid + 1
         #   else
-        #       move mid to the left
-        # else
-        #   if target < r val
-        #       move mid to the right
-        #   else
-        #       move mid to the left
+        #       return mid
+        # else:
+        #   right side
+        #   if target > mid
+        #       if target > r ele
+        #           r = mid -1
+        #       else:
+        #           l = mid + 1
+        #   elif target < mid
+        #       r = mid - 1
+        #   else:
+        #       return mid
         # return -1
 
         l = 0
         r = len(nums) - 1
         while l <= r:
-            
             mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
-
-            # first identify on which side the mid val falls into
-            # if mid > l val --> left side
-            # if mid < l val --> right side
 
             if nums[mid] >= nums[l]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
-                else:
-                    # if target < nums[l]:
-                    #     l = mid + 1
-                    # else:
-                    r = mid - 1
+                # left side
 
-            else: 
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
-                else:
+                if nums[mid] > target:
+                    if nums[l] > target: # go the right side
+                        l = mid + 1
+                    
+                    else:
+                        r = mid - 1
+                
+                elif nums[mid] < target:
                     l = mid + 1
                 
-        return -1
+                else:
+                    return mid
+               
+            else:
+                # right side
+                if nums[mid] < target:
+                    if nums[r] < target:
+                        r = mid -1
+                    
+                    else:
+                        l = mid + 1
+                
+                elif nums[mid] > target:
+                    r = mid - 1
+                
+                else:
+                    return mid
 
         
+        return -1
