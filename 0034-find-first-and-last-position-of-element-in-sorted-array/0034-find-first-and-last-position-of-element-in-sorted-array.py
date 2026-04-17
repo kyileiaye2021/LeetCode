6000b45e526c163c1aa993221098040857d4fc6c
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-17 01:39:50 -0700

    Time: 0 ms (100%), Space: 20.5 MB (66.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,41 +1,74 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # Binary Search Algo
-        first = self.search_first_occurence(nums, target)
-        last = self.search_last_occurence(nums, target)
-        return [first, last]
-    
-    def search_first_occurence(self, nums, target):
-        result = -1
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                result = mid
-                right = mid - 1
+        # nums = [1,2,3,4], target = 3
+        # output: [2,2]
+
+        # nums = [1,2,2,3,4], target = 2
+        # output: [1,2]
+
+        # nums = []
+        # output: [-1. -1]
+
+        # nums = [1,2,2,2,3,4], target = 2
+        # output: [1, 3]
+
+        # l index
+        # mid = l + r // 2
+        # if mid ele > target
+        #   r = mid - 1
+        # elif mid ele < target:
+        #   l = mid + 1
+        # else
+        #   left_index = mid
+        #   r = mid - 1
+
+        # r index
+        # mid = l + r // 2
+        # if mid ele > target:
+        #   r = mid -1
+        # elif mid e.e < target:
+        #   l = mid + 1
+        # else:
+        #   right index = mid
+        #   l = mid + 1
+
+        # [2], target = 2
+        # [0,0]
+
+        # return [left, right]
+
+        low = 0
+        high = len(nums) - 1
+        left = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] > target:
+                high = mid - 1
             elif nums[mid] < target:
-                left = mid + 1
+                low = mid + 1
             else:
-                right = mid - 1
-        return result 
-        
-        
-    def search_last_occurence(self, nums, target):
-        result = -1
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                result = mid
-                left = mid + 1
+                left = mid
+                high = mid - 1
+
+        low = 0
+        high = len(nums) - 1
+        right = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] > target:
+                high = mid - 1
             elif nums[mid] < target:
-                left = mid + 1
+                low = mid + 1
             else:
-                right = mid - 1
-        return result 
-            
+                right = mid
+                low = mid + 1
+
+        return [left, right] 
+
         
-        
+
+
+
+
+
         
