14a18d820edac0e495a85bf897035f80f248b7df
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-23 02:59:06 -0700

    Time: 11 ms (55.6%), Space: 22.8 MB (61.51%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,37 +4,48 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def merge(self, curr1, curr2):
-        dummy = temp = ListNode(0)
-        while curr1 and curr2:
-            if curr1.val > curr2.val:
-                temp.next = curr2
-                curr2 = curr2.next
-            else:
-                temp.next = curr1
-                curr1 = curr1.next
-            temp = temp.next
-
-        if curr1:
-            temp.next = curr1
-        if curr2:
-            temp.next = curr2
-
-        return dummy.next
-
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
+        # O(m *n) if we combine 1 list at a time
+        # if we combine 2 list at a time until there is only 1 list left --> (O(nlogm))
+
+        if not lists or len(lists) == 0:
             return None
-            
+        
         while len(lists) > 1:
-            mergedList = []
+            merged_lst = []
+
             for i in range(0, len(lists), 2):
-                curr1 = lists[i]
-                curr2 = lists[i + 1] if i + 1 < len(lists) else None
-                mergedList.append(self.merge(curr1, curr2))
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                merged_lst.append(self.merge(l1, l2))
 
-            lists = mergedList
-
+            lists = merged_lst
+        
         return lists[0]
 
+    def merge(self, l1, l2):
+        
+        dummy = ListNode(0)
+        res = dummy
+        p1 = l1
+        p2 = l2
 
+        while p1 and p2:
+            if p1.val > p2.val:
+                res.next = p2
+                p2 = p2.next
+            
+            else:
+                res.next = p1
+                p1 = p1.next
+            res = res.next
+
+        if p1:
+            res.next = p1
+
+        if p2:
+            res.next = p2
+
+        return dummy.next
+            
+                
