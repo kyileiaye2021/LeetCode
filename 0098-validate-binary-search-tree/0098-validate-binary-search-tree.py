e494768896d5d92fcac5a3d35403ec1674a4a298
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-23 11:58:36 -0700

    Time: 3 ms (34.75%), Space: 20.8 MB (76.99%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,24 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root, min_val, max_val):
-        if not root:
-            return True
-
-        if root.val <= min_val or root.val >= max_val:
-            return False
-    
-        # if there is only one ele --> return True
-        left = self.dfs(root.left, min_val, root.val)
-        right = self.dfs(root.right, root.val, max_val)
-
-        return left and right
-        
-        
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        min = float('-inf')
-        max = float('inf')
-        return self.dfs(root, min, max)
 
+        def dfs(root, min_val, max_val):
+            
+            if not root:
+                return True
+            if root.val <= min_val or root.val >= max_val:
+                return False
 
+            left_child = dfs(root.left, min_val, root.val)
+            right_child = dfs(root.right, root.val, max_val)
+            return left_child and right_child
 
+        return dfs(root, float('-inf'), float('inf'))
+
+        
