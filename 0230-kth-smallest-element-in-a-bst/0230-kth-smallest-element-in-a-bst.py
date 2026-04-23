7502100ccaedacd45f8222cfd3584502a5a92c1f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-23 15:33:41 -0700

    Time: 8 ms (5.66%), Space: 22.2 MB (40.63%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -5,75 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root, k, vals):
-        if not root:
-            return
-
-        self.dfs(root.left, k, vals)
-        vals.append(root.val)
-        self.dfs(root.right, k, vals)
-    # def dfs(self, root, k, n, output):
-        # if not root:
-        #     return 
-
-        # # left subtree
-        # self.dfs(root.left, k, n, output)
-
-        # # current
-        # n[0] += 1
-        # if n[0] == k:
-        #     output[0] = root.val
-
-        # # right subtree
-        # self.dfs(root.right, k, n, output)
-
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        # dfs
-        # root = 5
-        #       / \
-        #      3   6
-        #     / \
-        #    2.  4
-        #   /
-        #  1
-        # output: 3
+        # in order traversal
+        count = 0
+        res = None
 
-        # root = 5, k = 1
-        # output = 5
+        def dfs(root):
+    
+            if not root:
+                return 
 
-        # input: 3, k = 1
-        #       / \
-        #      1.  4
-        #       \
-        #        2
-        # output: 1
+            nonlocal count, res
+            dfs(root.left)
+            count += 1
+            if count == k:
+                res = root.val
+            dfs(root.right)
 
-        # input: 3, k = 2
-        #       / \
-        #      1.  4
-        #       \
-        #        2
-        # output: 2
-
-        # helper func (root, k, n) n = 0
-        # inorder traversal (dfs)
-        # base case
-        # if the root becomes none, return 
-
-         # recursive case
-        # call left subtree
-        # in each recursive call, increment n check if n == k:
-        #   store the root.val
-        # call right subtree
-
-        # n = [0]
-        # output = [0]
-        # self.dfs(root, k, n, output)
-        # return output[0]
-        vals = []
-        self.dfs(root, k, vals)
-        return vals[k-1]
+        dfs(root)
+        return res
 
 
-
-
+        
