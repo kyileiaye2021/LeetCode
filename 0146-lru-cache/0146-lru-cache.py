c51c41984dc622add3465772712b96b6164ef08c
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-23 02:20:24 -0700

    Time: 124 ms (55.76%), Space: 78.8 MB (17.28%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,56 +1,53 @@
-class Node:
-    def __init__(self, val=None, prev=None, next=None):
-        self.val = val
+class ListNode:
+    def __init__(self, key, value, prev=None, next=None):
+        self.key = key
+        self.value = value
         self.prev = prev
         self.next = next
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.data = {}
-        self.capacity = capacity
-        self.tail = Node()
-        self.head = Node()
-        self.tail.prev = self.head
-        self.head.next = self.tail
+        self.cap = capacity
+        self.cache = {} # map key to nodes
+        self.left = ListNode(0,0) #lru
+        self.right = ListNode(0,0) #mru
+        self.left.next, self.right.prev = self.right, self.left
     
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-    
-    def push(self, node):
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
-        return node
-    
-    def promote(self, node):
-        self.remove(node)
-        self.push(node)
-    
-    def pop(self):
-        node = self.head.next
-        self.remove(node)
-        return node.val
-        
+    def remove(self, node): # removing node from the list
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node): # inserting the node at right
+        prev, nxt = self.right.prev, self.right
+        prev.next, node.prev = node, prev
+        nxt.prev, node.next = node, nxt
+
     def get(self, key: int) -> int:
-        if key in self.data:
-            self.promote(self.data[key])
-            return self.data[key].val[1]
+        if key in self.cache:
+            # update the lru
+            # remove from the list
+            # add it at the right
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+
         return -1
-        
+
     def put(self, key: int, value: int) -> None:
-        if key not in self.data:
-            self.data[key] = self.push(Node((key, value)))
-        else:
-            self.data[key].val = (key, value)
-            self.promote(self.data[key])
-        if len(self.data) > self.capacity:
-            key, _ = self.pop()
-            del self.data[key]
-            
-            
-            
+        if key in self.cache:
+            self.remove(self.cache[key])
+        
+        new_node = ListNode(key, value)
+        self.insert(new_node)
+        self.cache[key] = new_node
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
