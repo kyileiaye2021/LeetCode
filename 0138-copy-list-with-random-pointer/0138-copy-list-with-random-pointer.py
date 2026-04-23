34131c11e7efb2ff77dee1c324800a8806aba7b1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-22 21:45:14 -0700

    Time: 46 ms (69.76%), Space: 20 MB (89.42%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -7,28 +7,57 @@ class Node:
         self.random = random
 """
 
-# map the original node to new node
-# traverse the orig node
-#   extract the copy
-#   copy node random should point to the orig node random
-#   copy node next should point to the orig node next
-
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
 
-        map = {None:None}
-        curr= head
+        # new_node hashmap 
+        # original node -> new node
+        # 7 -> 7_new
+        # 13 -> 13_new
 
-        while curr:
-            new_node = Node(curr.val)
-            map[curr] = new_node
-            curr = curr.next
+        # dummy node
 
+        # hashmap 
+        # index -> original node
+        # 0 -> 7
+        # 1 -> 13
+
+        # traverse the orig ll
+        # check if the node is not in new_node hashmap
+        #   create a new node with original val
+        #   add the new node to new_node hashmap
+        # else
+        #   res.next = new node from new_node hashmap
+        #   res = res.next
+        # check if the random node is not in new_node hashmap
+        #   create a new node and add it to new_node hashmap
+        # else
+        #   append the created new node from new_node hashmap
+        #   res.random = new_node
+        # cur = cur.next
+        # return dummy.next
+
+        new_node_map = {None : None}
         curr = head
-        while curr:
-            copy = map[curr]
-            copy.random = map[curr.random]
-            copy.next = map[curr.next]
-            curr = curr.next
+        copied = ListNode(0)
+        res = copied
 
-        return map[head]        
+        while curr:
+            if curr not in new_node_map:
+                new_node = ListNode(curr.val)
+                new_node_map[curr] = new_node
+            
+            res.next = new_node_map[curr]
+            res = res.next
+
+            if curr.random not in new_node_map:
+                new_node = ListNode(curr.random.val)
+                new_node_map[curr.random] = new_node
+            
+            res.random = new_node_map[curr.random]
+            curr = curr.next
+        
+        return copied.next
+                
+
+        
