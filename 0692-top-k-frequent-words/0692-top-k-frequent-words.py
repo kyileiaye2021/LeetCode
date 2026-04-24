831770403b875d8610b90f97e79e84e3db7c7527
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-24 12:18:30 -0700

    Time: 3 ms (55.27%), Space: 19.6 MB (24.04%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,33 +1,16 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        # input: ["is", "is", "the", "the"], k = 2
-        # output: is, the
-
-        # input: ["i", "am", "sweet", "smart", "smart"], k = 1
-        # output: smart
-
-        # input: ["the", "joy", "u", "u", "the"], k = 1
-        # output: the
-
-        # freq of each word
-        # sort the freq according to freq count
-        # consider the str with the most k freq count
-        # sort them in lexicographical order
-
+        # freq map {str -> freq}
         # max heap
-        # str and freq count
-        # add all pairs in a temp list
-        # sort_by_freq = heapify the temp list according to the freq count 
-        # sort_by_lexi = heapify the sort_by_freq according to lexicographical str
-        # extract the first k ele from sort by lexi
-        res = []
-        freq_map = Counter(words)
-        temp_list = [(-f, w) for w, f in freq_map.items()]
-        print(temp_list)
+        # (freq, str)
 
-        heapq.heapify(temp_list)
+        freq_map = Counter(words)
+        max_heap = [(-f, w) for w, f in freq_map.items()]
+        heapq.heapify(max_heap)
+
+        res = []
         for _ in range(k):
-            freq, word = heapq.heappop(temp_list)
-            res.append(word)
+            freq, w = heapq.heappop(max_heap)
+            res.append(w)
+
         return res
-        
