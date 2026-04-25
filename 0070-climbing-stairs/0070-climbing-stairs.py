96973b8c5fa2c1802c3ef4740859dd3a394afc2a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-25 16:50:34 -0700

    Time: 0 ms (100%), Space: 19.3 MB (19.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,60 +1,18 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n < 3: 
+            return n
+        ways = [0] * (n + 1)
+        ways[1] = 1
+        ways[2] = 2
+        def dfs(n):
+            if n == 1 or n == 2:
+                return ways[n]
 
-        # tabulation approoach
+            if ways[n] > 0:
+                return ways[n]
 
-        # two pointers to store the num of ways at step 1 and 2, ptr1 = 1 and ptr2 = 2
-        # iterate thru from 3 to n
-        #   add the two pointers and store in the curr
-        #   update ptr1 to ptr2 and ptr2 to curr
+            ways[n] = dfs(n - 1) + dfs(n - 2)      
+            return ways[n]  
 
-        # return curr
-
-        ptr1 = 1
-        ptr2 = 2
-        if n == 1: 
-            return ptr1
-
-        if n == 2:
-            return ptr2
-            
-        curr = 0
-
-        for i in range(3, n + 1):
-            curr = ptr1 + ptr2
-            ptr1 = ptr2
-            ptr2 = curr
-
-        return curr
-
-
-
-        # base case
-        # if n is 1 or 2 return n
-        # call recursive func on the n - 1 and n -2
-
-        # def recur(n):
-        #     if n == 1 or n == 2:
-        #         return n
-
-        #     return recur(n - 1) + recur(n - 2)
-        
-        # return recur(n)
-
-        # memoization
-        # def recur(n, memo):
-        #     # base case
-        #     if n == 1 or n == 2:
-        #         return n
-
-        #     if memo[n] != -1:
-        #         return memo[n]
-
-        #     memo[n] = recur(n - 1, memo) + recur(n - 2, memo)
-        #     return memo[n]
-
-        # memo = [-1] * (n + 1)
-        # return recur(n, memo)
-
-        
-        
+        return dfs(n)
