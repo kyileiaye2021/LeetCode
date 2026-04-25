13d9865e7662ea759110a0fd06d6b36a35318c44
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-24 21:11:08 -0700

    Time: 0 ms (100%), Space: 19.8 MB (32.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,28 +1,22 @@
 class Solution:
-    def dfs(self, sr, sc, image, dirs, original, color):
-        # change the color in the curr cell
-        image[sr][sc] = color
-
-        # go to the nei 
-        #   check if they are in boundary and equal to original:
-        #       call dfs on that 
-
-        for dr, dc in dirs:
-            new_r = dr + sr
-            new_c = dc + sc
-
-            if new_r in range(len(image)) and new_c in range(len(image[0])) and image[new_r][new_c] == original:
-                self.dfs(new_r, new_c, image, dirs, original, color)
-
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
-        # dirs = 4 directions
-        # call dfs on the start sr, sc
-        # return matrix
-
-        if image[sr][sc] == color:
+        # dfs
+        visited = set()
+        orig_color = image[sr][sc]
+        if orig_color == color:
             return image
-        dirs = [[0,1], [0, -1], [1,0], [-1,0]]
-        self.dfs(sr, sc, image, dirs, image[sr][sc], color)
+        dir = [[1,0], [-1,0], [0,1], [0,-1]]
+        def dfs(r,c):
+            image[r][c] = color
+
+            for dir_r, dir_c in dir:
+                new_r = dir_r + r
+                new_c = dir_c + c
+
+                if new_r in range(len(image)) and new_c in range(len(image[0])) and image[new_r][new_c] == orig_color:
+                    dfs(new_r, new_c)
+    
+        dfs(sr, sc)
         return image
-        
+
+
