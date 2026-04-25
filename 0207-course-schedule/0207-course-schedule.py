154b5d7142dc9ed39f45a003b4db93cd6b6cc16d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-25 00:54:01 -0700

    Time: 10 ms (14.48%), Space: 20.6 MB (57.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,81 +1,47 @@
 class Solution:
-    def dfs(self, curr_course, preq_graph, visited, curr_visited):
-        if curr_course in curr_visited:
-            return False
-
-        if curr_course in visited:
-            return True
-
-        curr_visited.add(curr_course)
-
-        for next_course in preq_graph[curr_course]:
-            if not self.dfs(next_course, preq_graph, visited, curr_visited):
-                return False
-    
-        curr_visited.remove(curr_course)
-        # mark the node as visited
-        visited.add(curr_course)
-        return True
-        
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #DFS
-        # graph {preq: next courses}
-        # visted to keep track of which nodes are already visited
-        # curr visited to keep track of visited nodes in each curr path
-        preq_graph = defaultdict(list)
-        visited = set()
-        curr_visited = set()
+        # preq_map {preq -> course}
+        # indeg {course -> num of preq}
 
-        for next_course, preq in prerequisites:
-            preq_graph[preq].append(next_course)
+        # queue
+        # add the course with no preq in the queue
+        # until queue is empty
+        #   pop out the cur course
+        #   go to courses that need cur course as prereq
+        #       decrement the indeg of nei course
+        #       if nei course indeg == 0
+        #           add nei course to queue
+        # check every indeg of courses becomes 0 --> return true else false
 
-        for i in range(numCourses):
-            if i not in visited:
-                if not self.dfs(i, preq_graph, visited, curr_visited):
-                    return False
+        preq_map = defaultdict(set)
+        indeg = [0] * numCourses
+
+        for c, p in prerequisites:
+            preq_map[p].add(c)
+            indeg[c] += 1
+
+        queue = deque()
+        for i, ind in enumerate(indeg):
+            if ind == 0:
+                queue.append(i)
+
+        while queue:
+            cur_course = queue.popleft()
+            numCourses -= 1
+            if numCourses == 0:
+                return True
+
+            for nei in preq_map[cur_course]:
+                indeg[nei] -= 1
+                if indeg[nei] == 0:
+                    queue.append(nei)
+
         
-        return True
+        return True if numCourses == 0 else False
+
+            
 
 
-        # create a graph for numCOurses
-        # processed_courses = 0
-        # preq_graph = defaultdict(list)
-        # in_deg_graph = defaultdict(list)
 
-        # for next_course, preq in prerequisites:
-        #     preq_graph[preq].append(next_course)
-        #     in_deg_graph[next_course].append(preq)
-        
-        # # keep track of in-degrees for each course
-        # in_deg = [0] * numCourses
-        # for course in range(numCourses):
-        #     for _ in in_deg_graph[course]:
-        #         in_deg[course] += 1
 
-        # # queue to add all courses with no in-deg
-        # # if queue is empty -> return false
-        # queue = deque()
-        # for course in range(numCourses):
-        #     if in_deg[course] == 0:
-        #         queue.append(course)
-        #         processed_courses += 1
-
-        # if not queue:
-        #     return False
-
-        # # whiile queue is not empty
-        # #   pop out the queue
-        # #   itreate thru the next coures of that course
-        # #       decrement the indeg of the next courese
-        # #       if in-deg of next course becomes 0
-        # #           add it to the queue
-        # while queue:
-        #     curr_course = queue.popleft()
-        #     for next_course in preq_graph[curr_course]:
-        #         in_deg[next_course] -= 1
-        #         if in_deg[next_course] == 0:
-        #             queue.append(next_course)
-        #             processed_courses += 1
-
-        # return True if processed_courses == numCourses else False
 
