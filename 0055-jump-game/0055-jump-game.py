bf6155895fc7732ef586cda0efd39f20b23ccd1d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-25 23:54:02 -0700

    Time: 17 ms (68.03%), Space: 20.5 MB (33.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,23 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
-        # total = first ele in nums
-        # iterate thru the nums upto last index
-        #   decrement 1 step from total
-        #   if total < 0:
-        #       return False
-        #   if total == 0
-        #       add curr jump len to total
-        # if total >= 0 -> we can reach to the end
+        goal = len(nums) - 1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return True if goal == 0 else False
+
+
         
-        total = nums[0]
-        for i in range(0, len(nums) - 1):
-            if total < 0:
-                return False
-            if nums[i] > total:
-                total = nums[i]
-            total -= 1
-
-        return True if total >= 0 else False
-
-       
