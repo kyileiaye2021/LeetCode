69cf78a7e338dd640538be56535dab5faaf80051
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-26 12:55:07 -0700

    Time: 18 ms (81.49%), Space: 25.8 MB (92.7%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,28 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # find the total of the gas amount and cost
-        # check if it cannnot cover the cost of whole journey
-        #   return -1
-
-        # itereate thru the ele in the gas
-        #   check if the gas is greater than equal to the cost
-        #       return the pos
-
-        res = 0
-        total = 0
         if sum(gas) < sum(cost):
-            return -1
+            return -1 
+
+        total = 0
+        start = 0
 
         for i in range(len(gas)):
-            
-            total += (gas[i] - cost[i])
-
+            total += gas[i] - cost[i]
             if total < 0:
                 total = 0
-                res = i + 1
-
-        return res
-
-
-
-
+                start = i + 1
+        
+        return start
