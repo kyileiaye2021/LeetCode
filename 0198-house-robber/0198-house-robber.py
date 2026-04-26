11ed53ca9846b2e68c74f88c564265ab6c6cab52
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-25 17:44:41 -0700

    Time: 0 ms (100%), Space: 19.3 MB (63.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,18 +3,16 @@ class Solution:
 
         if len(nums) == 1:
             return nums[-1]
-        total_money = [0] * len(nums)
-        total_money[0] = nums[0]
-        total_money[1] = max(total_money[0], nums[1])
+        first = nums[0]
+        second = max(first, nums[1])
 
 
         for i in range(2, len(nums)):
-            if nums[i] + total_money[i - 2] > total_money[i - 1]:
-                total_money[i] = nums[i] + total_money[i - 2]
+            temp = first
+            first = second
+            if nums[i] + temp > second:
+                second = nums[i] + temp
             
-            else:
-                total_money[i] = total_money[i - 1]
         
-        print(total_money)
-        return total_money[-1]
+        return second
         
