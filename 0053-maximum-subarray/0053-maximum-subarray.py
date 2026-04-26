4c176475bc7cd1b2e8af5154c4bbe24e0c2f699b
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-25 21:04:06 -0700

    Time: 23 ms (92.18%), Space: 31.5 MB (46.16%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,26 +1,18 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # when sub array sum becomes neg, set it 0
-        # max sum = min num
-        # iterate thru the nums
-        #   add the curr ele to curr sum 
-        #   update the max sum with curr sum if curr sum > max sum
-        #   check if max sum < 0
-        #       reset max sum to 0
-        # return max sum
-
-        max_sum = float('-inf')
-        curr_sum = 0
-        for i in range(len(nums)):
-            curr_sum += nums[i]
-            max_sum = max(max_sum, curr_sum)
-            print(max_sum)
-            if curr_sum < 0:
-                curr_sum = 0
-
-        return max_sum
-
-            
+        res = float('-inf')
+        total = 0
+        # total = 0
+        for n in nums:
+            total += n
+            res = max(total, res)
+            if total < 0:
+                total = 0
+        
+        return res
 
 
-      
+
+
+
+        
