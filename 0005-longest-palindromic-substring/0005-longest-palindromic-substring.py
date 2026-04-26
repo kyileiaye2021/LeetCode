b00c5313c1567011a57e268c63d3b63e57d719e2
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-25 20:15:04 -0700

    Time: 307 ms (43.84%), Space: 19.3 MB (72.26%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,58 +1,31 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # s = babd
-        # output: bab
-
-        # s = abcd
-        # output: ''
-
-        # s = "cbbd"
-        # output: bb
-
-        # s = "cbbc"
-        # output: "cbbc"
-
-        # s = ""
-        # output: ""
-
-        # iterate thru the chars
-        # l and r at the curr char
-        # max window = 0
-        # max sub string = ""
-        # until l and r ele are the same
-        #   l -= 1
-        #   r += 1
-        # curr window size 
-        # update the max window and keep track of curr substr if curr window > max window
-        # return max sub str
-        max_window = 0
         max_substr = ""
-
+        max_window = 0
         for i in range(len(s)):
             l = r = i
 
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                curr_window = r - l + 1
-                if curr_window > max_window:
-                    max_window = curr_window
-                    max_substr = s[l : r + 1]
-
+    
+                cur_window = r - l + 1
+                if cur_window > max_window:
+                    max_substr = s[l:r + 1]
+                    max_window = cur_window
+            
                 l -= 1
                 r += 1
 
-        for i in range(len(s) - 1):
             l = i
             r = i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                curr_window = r - l + 1
-                if curr_window > max_window:
-                    max_window = curr_window
-                    max_substr = s[l : r + 1]
+                cur_window = r - l + 1
+                if cur_window > max_window:
+                    max_substr = s[l:r + 1]
+                    max_window = cur_window
+
                 l -= 1
                 r += 1
-
+        
         return max_substr
 
-
-
-        
+    
