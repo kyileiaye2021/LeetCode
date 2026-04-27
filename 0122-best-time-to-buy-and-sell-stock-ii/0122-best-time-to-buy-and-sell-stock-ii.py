89ad33a6b3714ad4b5ad1ca9a309005dbe80aada
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-04-27 00:55:02 -0700

    Time: 3 ms (61.56%), Space: 20.3 MB (53.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,25 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # twopointers
-        # i and j
-        # total profit
-        # itereate thru the list
-        #   diff 
-        #   if diff > 0:
-        #       add the diff to total profit
-        #       move i to j
-        #   else:
-        #       move i to j
-        #   move j by 1
-        # return total profit
 
-        i, j = 0, 1
-        total_profit = 0
+        i =0
+        j = 1
+        total = 0
         while j < len(prices):
-            curr_profit = prices[j] - prices[i]
-            if curr_profit > 0:
-                total_profit += curr_profit
-
+            profit = prices[j] - prices[i]
+            if profit > 0:
+                total += profit
+            
             i = j
             j += 1
-        return total_profit
+
+        return total
+                    
+
+
+            
+        
