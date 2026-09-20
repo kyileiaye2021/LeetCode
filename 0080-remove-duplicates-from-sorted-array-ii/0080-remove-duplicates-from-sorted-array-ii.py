6df3a221b65d2e071732594b7712b0d565255dd4
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-19 23:11:49 -0700

    Time: 4 ms (97.09%), Space: 21.2 MB (96.88%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,36 +1,38 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        # two pointer
-        # bool var to keep track of the ele appears twice or not
-        # found = bool var false
-        # i , j 
-        # iterate the nums with j 
-        #   check if j ele == j - 1 ele
-        #       if found is false
-        #           set found to true
-        #           move i by 1
-        #          
-        #  else:
-        #      assign j ele to i
-        #      move i by 1
-    
-        #  move j by 1
-        # return i 
+    def removeDuplicates(self, nums: list[int]) -> int:
+        
+        # i, j = 1, 1
+        # k = 0
+        # while j < len(nums)
+        #   if j ele == j - 1 ele
+        #       if k <= 1:
+        #           i += 1
+        #           j += 1
+        #           k += 1
+        #       else: j += 1
+        #   else
+        #       i ele = j ele
+        #       i +=1
+        #       j += 1
+        # return i
 
-        i, j = 1, 1
-        found = False
+        i = 1
+        j = 1
+        k = 1
+
         while j < len(nums):
             if nums[j] == nums[j - 1]:
-                if not found:
-                    found = True
+                if k <= 1:
                     nums[i] = nums[j]
                     i += 1
+                    k += 1
 
             else:
-                found = False
                 nums[i] = nums[j]
                 i += 1
+                k = 1
 
             j += 1
 
         return i
+                
