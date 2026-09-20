5a62e088a152d40da647f314d53ff4a9383dbb60
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 02:54:31 -0700

    Time: 2 ms (65.1%), Space: 21 MB (70.61%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,23 +1,17 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(self, nums: list[int]) -> int:
 
-        # [4,4,2]
-        # output: 2
+        # sort and iteration pass
+        # O(nlogn)
 
-        # [1,2,2]
-        # output: 1
+        # set
+        # O(n) time and O(n) space
 
-        # [7,6,6,1,1]
-        # output: 7
-        # hashmap : O(n) space
-        # sort and traverse : O(nlogn) time
-        # bucket sort like storing the count at each index : O(n) space
+        # XOR
+        # O(n) time and O(1) space
 
         res = 0
-
         for n in nums:
             res ^= n
-
         return res
-
         
