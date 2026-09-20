02ba0efbccd0f1f957c6e91ffca58c3042718422
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 01:50:26 -0700

    Time: 0 ms (100%), Space: 20.5 MB (19.4%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,29 +1,10 @@
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        # happy cases
-        # nums = [1,0,3]
-        # output: 2
+    def missingNumber(self, nums: list[int]) -> int:
+        # sorting
+        # O(nlogn) time 
 
-        # nums = [1]
-        # output: 0
-
-        # nums = [0]
-        # output: 1
-
-        # n = len(nums)
-        # num_list = [i for i in range(n+1)]
-        # num_set = set(num_list)
-
-        # for i in range(len(nums)):
-        #     if nums[i] in num_set:
-        #         num_set.remove(nums[i])
+        # sum of 0 to 3 - sum of nums = missing num
+        n = len(nums)
+        total = sum(i for i in range(n + 1))
+        return total - sum(nums)
         
-        # remaining = num_set.pop()
-        # return remaining
-
-        res = len(nums)
-        for i in range(len(nums)):
-            res += (i - nums[i])
-
-        return res
-
