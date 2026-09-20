8f23a84bec1d328700f593afa6de1dd0549116dd
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 02:28:33 -0700

    Time: 3 ms (60.67%), Space: 20.4 MB (76.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,7 +4,16 @@ class Solution:
         # O(nlogn) time 
 
         # sum of 0 to 3 - sum of nums = missing num
-        n = len(nums)
-        total = sum(i for i in range(n + 1))
-        return total - sum(nums)
+        # n = len(nums)
+        # total = sum(i for i in range(n + 1))
+        # return total - sum(nums)
+
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res ^= (i ^ nums[i])
+
+        return res
+
+
         
