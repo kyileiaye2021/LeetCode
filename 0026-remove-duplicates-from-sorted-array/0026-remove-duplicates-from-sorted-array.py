513e872cfca695d0c60ed372b447a9068168c118
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-19 21:19:40 -0700

    Time: 3 ms (44.59%), Space: 20.6 MB (43.57%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,61 +1,17 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        
-        # happy cases
-        # [1,1,2]
-        # [1,2, _]
-
-        # [1,2,3]
-        # [1,2,3]
-
-        # [1,2,3,3]
-        # [1,2,3,_]
-
-        # [1,1,1,2,2,2,3,3,3]
-        # [1,2,3,_,_,_,_,_,_]
-
-        # edge cases
-        # [1,1,1,1]
-        # [1,_,_,_]
-
-        # [1]
-        # [1]
-
-        # []
-        # []
-
+    def removeDuplicates(self, nums: list[int]) -> int:
         # 2 pointers
-        # l,r
-        # l = 0
-        # r = l + 1
-        # until r reaches the end
-        # keep moving r until the l and r ele are the same
-        # if l and r ele not same
-        #   move r pointer ele to l + 1 place
-        #   move l to r
-        #   move r to l + 1
-        # return first k ele of the list
-
-        if len(nums) == 0:
-            return []
-
-        l = 0
-        r = l + 1
-        k = l + 1
-
-        while r < len(nums):
-            if nums[r] == nums[l]:
-                r += 1
-            
-            else:
-                nums[k] = nums[r]
-                k += 1
-                l = r
-                r = l + 1
-
-        return k
-
-
         
+        # i , j
+        
+        i, j = 1, 1
 
+        while j < len(nums):
+
+            if nums[j] != nums[j - 1]:
+                nums[i] = nums[j]
+                i += 1
+            j += 1
+        
+        return i
         
