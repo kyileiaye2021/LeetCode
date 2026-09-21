bf89e66fe2ba39b5445ae0c374372bc40709ef2b
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 21:42:51 -0700

    Time: 79 ms (8.86%), Space: 28.8 MB (11.92%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,42 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
+
+        # prices = [7, 1,2,5]
+        # profit = 4
+
+        # prices = [1,6,3,2]
+        # profit = 5
+
+        # prices = [5,3,1]
+        # profit = 0
+
+        # prices = [4,4,4]
+        # profit = 0
+
+        # prices = [3]
+        # profit = 0
+
+        # 2 pointers
+
+        # i, j 
+
+        max_profit = 0
         i = 0
         j = 1
-        max_profit = 0
 
         while j < len(prices):
-            if prices[i] >= prices[j]:
-                i = j
+            profit = prices[j] - prices[i]
+            max_profit = max(max_profit, profit)
 
-            else:
-                profit = prices[j] - prices[i]
-                max_profit = max(max_profit, profit)
-                
-            j += 1
+            if profit < 0:
+                i = j
         
+            j += 1
         return max_profit
+
+
+
+
 
 
         
