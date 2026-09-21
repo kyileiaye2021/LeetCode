bf4495921b8e5b775c15a8d706976a5f3dc67e4a
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 19:53:49 -0700

    Time: 7 ms (39.85%), Space: 19.3 MB (48.7%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,52 +1,74 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
 
-        # s = "egg", t = "add"
+        # happy cases
+        # egg, add
         # true
 
-        # s = "foo", t = "bar"
+        # tap, pit
+        # true
+
+        # add, gap
         # false
 
-        # s = "bar", t = "foo"
-        # false   
+        # f3, g1
+        # true
 
-        # s = "bad", t = "felt"
+        # ss4, tte
+        # true
+
+        # edge cases
+        # ab, cde
         # false
 
-        # s = "felt", t = "bad"
-        # false
+        # '', ''
+        # true
 
-        # hashmap
-        # s_map = {e:a, g:d}, t_map = {a:e, d:g}     
-        # check if the size of the two inputs are not the same -> return false
-        # iterate thru the s
-        #   check if the ele is in s_map
-        #       check if the value of the curr key is not the same
+        # map
+        # itereate thru s and t 
+        #   if curr s char not in map
+        #       curr s char : curr t char
+        #       add pair to map
+        #   else:
+        #       if curr t char == t char in the map
+        #           move s and t ptrs
+        #       else
         #           return False
-        #   else
-        #       check if the curr t is already in the t map
-        #           return false
-        # return true
 
-        s_map = {}
-        t_map = {}
+        # return True
 
-        s_len = len(s)
-        t_len = len(t)
+        s_to_t_pairs = {}
+        t_to_s_pairs = {}
+        if len(s) != len(t):
+            return False
 
-        # if s_len != t_len:
-        #     return False
+        s_ptr = 0
+        t_ptr = 0
 
-        for i in range(s_len):
-            if s[i] in s_map:
-                if s_map[s[i]] != t[i]:
-                    return False
+        while s_ptr < len(s):
+            if s[s_ptr] not in s_to_t_pairs:
+                s_to_t_pairs[s[s_ptr]] = t[t_ptr]
 
             else:
-                if t[i] in t_map:
+
+                if t[t_ptr] != s_to_t_pairs[s[s_ptr]]:
                     return False
 
-                s_map[s[i]] = t[i]
-                t_map[t[i]] = s[i]
+            if t[t_ptr] not in t_to_s_pairs:
+                t_to_s_pairs[t[t_ptr]] = s[s_ptr]
+
+            else:
+                if s[s_ptr] != t_to_s_pairs[t[t_ptr]]:
+                    return False
+
+            s_ptr += 1
+            t_ptr += 1
 
         return True
+
+
+
+
+
+    
+        
