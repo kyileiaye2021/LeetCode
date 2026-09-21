bd232e0a1441258f9c5f4c3c8eea0be0a641b157
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 18:00:36 -0700

    Time: 55 ms (30.64%), Space: 39.3 MB (47.64%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,27 +1,16 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        
+        hashmap = {}
 
-        # hashmap
-        # {ele: index}
-        # iterate thru the ele
-        #   if ele not in hashmap
-        #       add with the curr index
-        #   else
-        #       check if the diff between curr index and prev index <= k
-        #           return true
-        #       else:
-        #           assign the curr index to the prev index
-        # return false
-
-        map = {}
-        for i in range(len(nums)):
-            
-            if nums[i] in map:
-                diff = i - map[nums[i]]
+        for i, n in enumerate(nums):
+            if n in hashmap:
+                diff = i - hashmap[n]
                 if diff <= k:
                     return True
 
-            map[nums[i]] = i
-
-        return False
+            hashmap[n] = i
         
+        return False
+
+           
