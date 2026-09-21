0fb7887f866891264e3e0ee98dc7993fbcde4272
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 21:23:54 -0700

    Time: 7 ms (41.23%), Space: 19.3 MB (59.48%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,25 +1,44 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        # total = 0
+        # roman_num = {}
+        # iterate thru the the chars
+        #   if curr == 'V' or curr == 'X'
+        #       if i > 0 and prev one == 'I' 
+        #           total -= 1
+        #   if curr == 'L' or curr == 'C'
+        #       if i > 0 and prev == 'X'
+        #           total -= 10
+        #   if curr == 'D' or curr == 'M'
+        #       if i > 0 and prev = C
+        #           total -= 100
+        #   total += roman_num[curr]
 
-        symbols = {'I': 1,
-                    'V': 5,
-                    'X': 10,
-                    'L': 50,
-                    'C': 100,
-                    'D': 500,
-                    'M': 1000}
-        
+        # return total
         total = 0
+        roman_num_map = {'I': 1,
+                        'V': 5,
+                        'X': 10,
+                        'L': 50,
+                        'C': 100,
+                        'D': 500,
+                        'M': 1000}
+        for i in range(len(s)):
 
-        i = 0
+            if s[i] == 'V' or s[i] == 'X':
+                if i > 0 and s[i - 1] == 'I':
+                    total -= 2
 
-        while i < len(s)-1:
-            if symbols[s[i]] < symbols[s[i+1]]:
-                total -= symbols[s[i]]
-            else:
-                total += symbols[s[i]]
-            i += 1
+            if s[i] == 'L' or s[i] == 'C':
+                if i > 0 and s[i - 1] == 'X':
+                    total -= 20
 
-        return total + symbols[s[i]]
+            if s[i] == 'D' or s[i] == 'M':
+                if i > 0 and s[i - 1] == 'C':
+                    total -= 200
 
+            total += roman_num_map[s[i]]
+
+        return total
         
+
