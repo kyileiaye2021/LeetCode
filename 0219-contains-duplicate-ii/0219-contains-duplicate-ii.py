741e7330b8d0d326e13589b58c4b28471b073f23
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-20 18:16:20 -0700

    Time: 66 ms (13.68%), Space: 36.4 MB (71.42%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,16 +1,48 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
         
-        hashmap = {}
+        # hashmap = {}
 
-        for i, n in enumerate(nums):
-            if n in hashmap:
-                diff = i - hashmap[n]
-                if diff <= k:
-                    return True
+        # for i, n in enumerate(nums):
+        #     if n in hashmap:
+        #         diff = i - hashmap[n]
+        #         if diff <= k:
+        #             return True
 
-            hashmap[n] = i
+        #     hashmap[n] = i
+        
+        # return False
+
+        # sliding window
+        # set 
+        # iterate thru the ele
+        #   if the size of set becomes > k
+        #       remove the ith ele 
+        #       increment the i by 1
+        #   if the curr jth ele in the set
+        #       return True
+        #   add the curr jth ele to the set
+        # return False
+        
+        i, j = 0, 0
+        hashSet = set()
+
+        while j < len(nums):
+            if len(hashSet) > k:
+                hashSet.remove(nums[i])
+                i += 1
+
+            if nums[j] in hashSet:
+                return True
+
+            hashSet.add(nums[j])
+
+            j += 1
         
         return False
+
+
+
+
 
            
