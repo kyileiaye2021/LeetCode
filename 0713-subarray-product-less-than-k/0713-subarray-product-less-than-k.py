07952fb339e6cf38c4312f6f3647952829d409d1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-21 23:36:18 -0700

    Time: 47 ms (10.37%), Space: 21.4 MB (86.76%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,37 +1,32 @@
 class Solution:
-    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # two pointer sliding window
-        # total_product
-        # count 
-        # l , r
-        # iterate thru the ele until r reach the end
-        #   if curr < k 
-        #       increment count by 1
-        #       multiply total with curr
-        #       check if total < k
-        #           increment count by 1
-        #       else
-        #           divide the total by l ele
-        #           increment l 
+    def numSubarrayProductLessThanK(self, nums: list[int], k: int) -> int:
+        # count = 0
+        # total = 1
+        # l = 0
+        # r = 0
+        # iterate thru the ele with r
+        #     total *= n
+        #     
+        #     while total >= k:
+        #              total = total / l ele 
+        #              l += 1
+        #     count += r -l + 1
+        #   r += 1
         # return count
 
-        # [10, 50, 100, 600]
-        # [10, 60, 12, 6]
-
-        total_product = 1
         count = 0
+        total = 1
         l = 0
         r = 0
-        while r < len(nums):
-            total_product *= nums[r]
 
-            while l <= r and total_product >= k:
-                total_product = total_product // nums[l]
+        while r < len(nums):
+            total *= nums[r]
+
+            while l <= r and total >= k:
+                total = total / nums[l]
                 l += 1
-            count += r - l + 1 
+
+            count += (r - l + 1)
+
             r += 1
         return count
-
-        
-            
-
