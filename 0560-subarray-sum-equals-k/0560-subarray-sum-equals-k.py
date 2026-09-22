9d94eb93d90a1a55fa459f776ed687f13855bdbb
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-22 14:13:26 -0700

    Time: 31 ms (72.26%), Space: 22.2 MB (21.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,55 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
-        prefix_count = {0:1}
+        # happy cases
+        # nums = [0,1,2], k = 1
+        # 2
+
+        # nums = [2,1,3], k = 3
+        # 2
+
+        # nums = [-1, 0, 1], k = 0
+        # 2
+
+        # [-1, 1, - 3, 3], k = 0
+        # 3
+
+        # edge cases
+        # [0], k = 9
+        # 0
+
+        # [], k = 2
+        # 0
+
+        # prefix sum + hashmap
+        # prefix = [nums[0]]
+        # for n in nums from index 1
+        #   curr_sum = prefix[-1] + nums[i]
+        #   prefix.append(curr_sum)
+        #   add curr sum in hashmap with the frequency count
+
+        # total = 0
+        # iterate thru the prefix sum 
+        #   curr sum - k is in hashmap
+        #       add freq count of that diff to total 
+        # return total 
+
+        curr_total = 0
         res = 0
-        prefix_sum = 0
+        prefix_count = {0: 1}
 
-        for n in nums:
-            prefix_sum += n
-
-            if prefix_sum - k in prefix_count:
-                res += prefix_count[prefix_sum - k]
+        for i in range(len(nums)):
+            curr_total += nums[i]
+            if curr_total - k in prefix_count:
+                res += prefix_count[curr_total - k]
             
-            prefix_count[prefix_sum] = prefix_count.get(prefix_sum, 0) + 1
+            prefix_count[curr_total] = 1 + prefix_count.get(curr_total, 0)
 
         return res
+                
 
-            
+
+
+
+
+        
