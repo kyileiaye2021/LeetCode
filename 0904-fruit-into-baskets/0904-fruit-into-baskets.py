897b8b6c367e754d1a29f4e292c902cb8469b953
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-22 00:45:37 -0700

    Time: 222 ms (12.11%), Space: 26 MB (35.01%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,64 +1,44 @@
 class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        # hashmap {ele : freq}
-        # sliding window
+    def totalFruit(self, fruits: list[int]) -> int:
+        # findng  k unique ele in the window
+        # k = 2
 
-        # fruits = [1,1,1,2]
-        # output: 4
+        # hashmap 
+        # l, r = 0, 0
 
-        # fruit = [1,2,3,2,2]
-        # output: 4
-
-        # fruit = [1,2,3,1,1]
-        # output: 3
-
-        # fruit = [0,1,0,0]
-        # output: 4
-
-        # fruit = [0,0,0,0]
-        # output: 4
-
-        # fruit = [1,2,4,3,4]
-        # output: 3
-
-        # l and r at index 0
-        # hashmap to store the ele and their freq
-        # iterate thru the ele until r reaches the end
-        #   if curr ele is in the hashmap
-        #       increment the freq of the ele in hashmap
-        #       keep track of the max ele
-        #       increment the r 
-        #   else
-        #       check if hashmap size is already 2
-        #           decrement freq of the l ele 
-        #           if l ele becomes 0, remove the ele from hashmap
-        #           decrement l
-        #       else: 
-        #           add the ele to the hashmap with freq of 1
-
+        # iterate thru ele wiht r
+        #   if r ele not in hashmap
+        #       while count size >= k
+        #           decrement count[l ele]
+        #           if count[l ele] == 0: del count[l ele]
+        #           l += 1
+        #  
+        #    add curr r ele to count or increment count val in count 
+        # 
+        #    track curr window
+        #    update max window
+        #    r += 1
         # return max window
-
-        l = 0
-        r = 0
-        map = {}
+        k = 2
+        count = {}
+        l, r = 0, 0
         max_window = 0
-        while r < len(fruits):
-            if fruits[r] in map:
-                map[fruits[r]] += 1
-            
-            else:
-                while len(map) >= 2:
-                    map[fruits[l]] -= 1
-                    if map[fruits[l]] == 0:
-                        del map[fruits[l]]
-                    l += 1
-                
-                map[fruits[r]] = 1
 
+        while r < len(fruits):
+            if fruits[r] not in count:
+                while len(count) >= k:
+                    count[fruits[l]] -= 1
+                    if count[fruits[l]] == 0:
+                        del count[fruits[l]]
+                    l += 1
+
+            count[fruits[r]] = 1 + count.get(fruits[r], 0)
             curr_window = r - l + 1
             max_window = max(max_window, curr_window)
-            r += 1 
+            r += 1
 
         return max_window
+
+
 
         
