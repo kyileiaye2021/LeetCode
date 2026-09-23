8950ba5e6ae21a581ffe965dd02530689c257e25
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-23 16:21:06 -0700

    Time: 291 ms (82.04%), Space: 32.4 MB (12.67%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,46 +1,69 @@
 class Solution:
-    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        # num of flowers = m * k
-        # if len of bloomDay < nun of flowers
-        #   return -1
+    def minDays(self, bloomDay: list[int], m: int, k: int) -> int:
+        # res = -1
+        # if m *k > len(bloomDay)
+        #   return res
 
-        # iterate i from 1 to max day
-        #   iterate j thru the arr from 0 to n -k
-        #       from j to j + k
-        #           check all ele are <= curr i
-        #           m -= 1
-        #       if m == 0:
-        #           res = i
-        #           break
-        # return res if m = 0 else -1
+        # happy cases
+        # bloomDay = [1,10,3,10,2], m = 3, k = 1
+        # 3
 
+        # edge cases
+        # bloomDay = [1,10,3,10,2], m = 3, k = 2
+        # -1
 
-        l = 1
-        r = max(bloomDay)
+        # bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
+        # 12
+
+        # l = min(bloomDay)
+        # r = max(bloomDay)
+        # while l < = r:
+        #   mid = (l + r) // 2
+        #   bouquet = 0
+        #   streak = 0 (to keep track of the num of continuous flower)
+        #   for b in bloomDay
+        #       if b <= mid 
+        #           streak += 1
+        #           if streak == k:
+        #               bouquet += 1
+        #               streak = 0
+        #       else:
+        #           streak = 0
+        #   if bouquet >= m:
+        #       res = mid
+        #       r = mid - 1
+        #   eles:
+        #       l = mid + 1
+        # return res
+
         res = -1
+        if m * k > len(bloomDay):
+            return res
+
+        l = min(bloomDay)
+        r = max(bloomDay)
 
         while l <= r:
+            mid = (l + r) // 2
             bouquet = 0
             streak = 0
-            mid = (l + r) // 2
 
-            for i in range(len(bloomDay)):
-                if bloomDay[i] <= mid:
+            for b in bloomDay:
+                if b <= mid:
                     streak += 1
+
                     if streak == k:
                         bouquet += 1
                         streak = 0
+                    
                 else:
                     streak = 0
 
             if bouquet >= m:
-                res = mid 
+                res = mid
                 r = mid - 1
+
             else:
                 l = mid + 1
-        print(bouquet)
-        return res 
-
-
-
-
+            
+        return res
