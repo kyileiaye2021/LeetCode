eca24e3b502248d7369ea191a8a394f5a72fcca1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-24 08:53:46 -0700

    Time: 3 ms (65.27%), Space: 19.8 MB (12.25%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -17,42 +17,80 @@ class Solution:
         # 0, 1
         # 1, 1
 
+        # recur(i, j, carry)
+        # base case
+        # if not i and not j and carry = 0:
+        #   return None
+
+        # recursive step
+        # if not i:
+        #   i val = 0 else i val = i.val
+        # if not j
+        #   j val = 0 else j val = j.val
+        # total = i val + jval + carry
+        # new node = ListNode(total % 10)
+        # new carry = total // 10
+        # new_node.next = recur(i.next, j.next, new carry)
+        # return new node
+
+        def recur(i, j, carry):
+            if not i and not j and carry == 0:
+                return None
+
+            i_val = i.val if i else 0
+            j_val = j.val if j else 0
+
+            total = i_val + j_val + carry
+            new_node = ListNode(total % 10)
+            new_carry = total // 10
+
+            next_i = i.next if i else None
+            next_j = j.next if j else None
+
+            new_node.next = recur(next_i, next_j, new_carry)
+            return new_node
+
         i = l1
         j = l2
         carry = 0
-        dummy = ListNode()
-        curr = dummy
+        return recur(i, j, carry)
 
-        while i and j:
-            new_node_val = (i.val + j.val + carry) % 10
-            carry = (i.val + j.val + carry) // 10
-            new_node = ListNode(new_node_val)
-            curr.next = new_node
-            curr = curr.next
-            i = i.next
-            j = j.next
+        # i = l1
+        # j = l2
+        # carry = 0
+        # dummy = ListNode()
+        # curr = dummy
 
-        while i:
-            new_node_val = (i.val + carry) % 10
-            carry = (i.val + carry) // 10
-            new_node = ListNode(new_node_val)
-            curr.next = new_node
-            curr = curr.next
-            i = i.next
+        # while i and j:
+        #     new_node_val = (i.val + j.val + carry) % 10
+        #     carry = (i.val + j.val + carry) // 10
+        #     new_node = ListNode(new_node_val)
+        #     curr.next = new_node
+        #     curr = curr.next
+        #     i = i.next
+        #     j = j.next
 
-        while j:
-            new_node_val = (j.val + carry) % 10
-            carry = (j.val + carry) // 10
-            new_node = ListNode(new_node_val)
-            curr.next = new_node
-            curr = curr.next
-            j = j.next
+        # while i:
+        #     new_node_val = (i.val + carry) % 10
+        #     carry = (i.val + carry) // 10
+        #     new_node = ListNode(new_node_val)
+        #     curr.next = new_node
+        #     curr = curr.next
+        #     i = i.next
 
-        if carry > 0:
-            new_node = ListNode(carry)
-            curr.next = new_node
+        # while j:
+        #     new_node_val = (j.val + carry) % 10
+        #     carry = (j.val + carry) // 10
+        #     new_node = ListNode(new_node_val)
+        #     curr.next = new_node
+        #     curr = curr.next
+        #     j = j.next
+
+        # if carry > 0:
+        #     new_node = ListNode(carry)
+        #     curr.next = new_node
 
 
-        return dummy.next
+        # return dummy.next
 
 
