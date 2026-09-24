ebfd30d3003896ea06ef4270850bd438b1b75e3d
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-24 00:12:37 -0700

    Time: 11 ms (6.67%), Space: 19.3 MB (43.29%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,77 +4,55 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # l1 = [2,3,4]
-        # l2 = [4,3,1]
-        # output: [6,6,5]
+    def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
+        # 2, 4, 3
+        # 5, 6, 4
+        # 7, 0, 8
 
-        # l1 = [5,6,7]
-        # l2 = [5,6,7]
-        # output: [0,3,5,1]
+        # 9, 9, 9
+        # 9, 9, 9, 9
+        # 8, 9, 9, 0, 1
 
-        # l1 = [0]
-        # l2 = [0]
-        # output: [0]
+        # 1
+        # 0, 1
+        # 1, 1
 
-        # l1 = [9,9,9]
-        # l2 = [9,9]
-        # output: [8, 9, 0, 1]
-
-        # l1 = [0]
-        # l2 = [1]
-        # output: [1]
-
-        # 5 + 5 = 10 % 10 = 0
-        # carry = 10// 10 = 1
-
-        # traverse l1 and l2 together
-        
-        dummy = ListNode(0)
-        res = dummy
-
-        p1 = l1
-        p2 = l2
-        cur_sum = 0
+        i = l1
+        j = l2
         carry = 0
+        dummy = ListNode()
+        curr = dummy
 
-        while p1 and p2:
-            cur_sum = p1.val + p2.val + carry
-            carry = 0
-            if cur_sum > 9:
-                carry = cur_sum // 10
-                cur_sum = cur_sum % 10
-            
-            new_node = ListNode(cur_sum)
-            res.next = new_node
-            res = res.next
-            p1 = p1.next
-            p2 = p2.next
+        while i and j:
+            new_node_val = (i.val + j.val + carry) % 10
+            carry = (i.val + j.val + carry) // 10
+            new_node = ListNode(new_node_val)
+            curr.next = new_node
+            curr = curr.next
+            i = i.next
+            j = j.next
 
-        while p1:
-            cur_sum = p1.val + carry
-            carry = 0
-            if cur_sum > 0:
-                carry = cur_sum // 10
-                cur_sum = cur_sum % 10
-            new_node = ListNode(cur_sum)
-            res.next = new_node
-            res = res.next
-            p1 = p1.next
+        while i:
+            new_node_val = (i.val + carry) % 10
+            carry = (i.val + carry) // 10
+            new_node = ListNode(new_node_val)
+            curr.next = new_node
+            curr = curr.next
+            i = i.next
 
-        while p2:
-            cur_sum = p2.val + carry
-            carry = 0
-            if cur_sum > 0:
-                carry = cur_sum // 10
-                cur_sum = cur_sum % 10
-            new_node = ListNode(cur_sum)
-            res.next = new_node
-            res = res.next
-            p2 = p2.next
+        while j:
+            new_node_val = (j.val + carry) % 10
+            carry = (j.val + carry) // 10
+            new_node = ListNode(new_node_val)
+            curr.next = new_node
+            curr = curr.next
+            j = j.next
 
         if carry > 0:
             new_node = ListNode(carry)
-            res.next = new_node
+            curr.next = new_node
+
 
         return dummy.next
+
+
