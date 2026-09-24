f828f8c247f766a1ccadc4813dba1bfdfbf49579
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-23 17:48:58 -0700

    Time: 0 ms (100%), Space: 19.6 MB (60.54%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.py
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.py
@@ -1,37 +1,44 @@
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
+    def findMin(self, nums: list[int]) -> int:
+        # binary search
 
-        # if l val <= m val (on the left side)
-        #   if l val < r val
-        #       r = mid
-        #
-        #   if l val > r val
+        # l = 0
+        # r = len(nums) - 1
+        # while l < = r
+        #   
+        #   if l ele <= r ele
+        #       return l ele
+        #   mid = l + ((r - l) // 2)
+        #   if mid ele >= l ele
         #       l = mid + 1
-        # 
-        #   if l val == r val
-        #       l += 1
-
-        # else ( on the right side)
-        #   r = mid
+        #   else
+        #       res = mid
+        #       r = mid - 1
+        
+        # return res
 
         l = 0
         r = len(nums) - 1
+        res = float('inf')
+        while l <= r:
 
-        while l < r:
-            mid = (l + r) // 2
-            
-            if nums[l] <= nums[mid]: #  mid on the left side
+            mid = l + ((r - l) // 2)
 
-                if nums[l] < nums[r]:
-                    r = mid
+            if nums[l] == nums[mid]:
+                res = min(res, nums[mid])
+                l += 1
 
-                elif nums[l] > nums[r]:
-                    l = mid + 1
+            elif nums[l] > nums[mid]:
+                res = min(res, nums[mid])
+                r = mid - 1
+
+            else:
+                if nums[r] > nums[l]:
+                    # return nums[l]
+                    return min(res, nums[l])
 
                 else:
-                    l += 1
+                    l = mid + 1
 
-            else: # mid on the right side
-                r = mid
+        return res
 
-        return nums[r]
