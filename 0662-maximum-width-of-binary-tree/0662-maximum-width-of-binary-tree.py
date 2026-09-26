0d1fc821c593cffbc59a905a00761fe68251a2c8
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-26 03:02:54 -0700

    Time: 0 ms (100%), Space: 20 MB (88.49%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -5,60 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # happy cases
-        # input: 1
-        #       / \
-        #      3.  2
-        #     /     \
-        #    4       5
-        # output: 4
-
-        # input: 1
-        #       / \
-        #      3   2
-        #     / \   
-        #    4   5
-        # output: 2   
-
-        # input: 1
-        #       / \
-        #      3   2
-        #     /   
-        #    4
-        # output: 2  
-
-        # edge cases
-        # input: 1
-        # output: 1
-
-        # input: []
-        # output: 0
-        
-        # BFS
-        # for non-null ending nodes, we can maintain their positions by assigning numbers
-        # for tracking the first node in each level, we can maintain prevLevel and currLevel 
-        # then if currlevel > prevLevel --> update prevLevel and prevNode
+    def widthOfBinaryTree(self, root: TreeNode | None) -> int:
+        # bfs
+        # deque 
+        if not root:
+            return 0
+        dq = collections.deque()
+        dq.append((root, 0))
         max_len = float('-inf')
-        queue = collections.deque()
-        queue.append([root, 1, 0]) # [node, num, level]
-        prevNum = 1
-        prevLevel = 0
+
+        while dq:
+            cur_len = len(dq)
+            first_index = dq[0][1]
+            last_index = dq[-1][1]
+            max_len = max(max_len, last_index - first_index + 1)
+
+            for _ in range(cur_len):
         
-        while queue:
-            node, num, level = queue.popleft()
+                curr_node, index = dq.popleft()
+                
+                if curr_node.left:
+                    dq.append((curr_node.left, index * 2))
 
-            if level > prevLevel:
-                prevLevel = level
-                prevNum = num
-
-            first_node = prevNum # here, prevNum is the first node in each level
-            curr_len = (num - first_node) + 1
-            max_len = max(max_len, curr_len)
-
-            if node.left:
-                queue.append([node.left, num * 2, level + 1])
-            if node.right:
-                queue.append([node.right, (num * 2) + 1, level + 1])
+                if curr_node.right:
+                    dq.append((curr_node.right, index * 2 + 1))
 
         return max_len
+            
+            
